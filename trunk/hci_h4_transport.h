76a09ef37a7459958c32469aaa48810c0d91fe33
matthias.ringwald <matthias.ringwald@1a0a8af8-31b5-11de-8e0c-53a27eea117e>
2009-04-29 21:53:15 +0000

    prototyping

---
+++ trunk/hci_h4_transport.h
@@ -0,0 +1,11 @@
+/*
+ *  hci_h4_transport.h
+ *
+ *  Created by Matthias Ringwald on 4/29/09.
+ *
+ */
+#pragma once
+
+#include "hci_transport.h"
+
+extern hci_transport_t hci_h4_transport;
---
+++ trunk/hci_transport.h
@@ -0,0 +1,31 @@
+/*
+ *  hci_transport.h
+ *
+ *  HCI Transport API -- allows hcid to use different transport protcols 
+ *
+ *  Created by Matthias Ringwald on 4/29/09.
+ *
+ */
+#pragma once
+
+typedef struct {
+    int    (*open)(void *transport_config);
+    int    (*close)();
+    int    (*send_cmd_packet)(void *packet, int size);
+    int    (*send_acl_packet)(void *packet, int size);
+    void   (*register_event_packet_handler)(void (*handler)(void *packet, int size));
+    void   (*register_acl_packet_handler)  (void (*handler)(void *packet, int size));
+    int    (*get_fd)();      // <-- only used for select(..) call
+    int    (*handle_data)(); // -- to be called when select( .. ) returns for the fd
+    const char * (*get_transport_name)();
+} hci_transport_t;
+
+typedef struct {
+    const char *device_name;
+    int   baudrate;
+    int   flowcontrol; // 
+} hci_uart_config_t;
+
+typedef struct {
+    // unique usb device identifier
+} hci_libusb_config_t;
