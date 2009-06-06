68855539e708ba592e2baf546a8445d3fe93189a
matthias.ringwald <matthias.ringwald@1a0a8af8-31b5-11de-8e0c-53a27eea117e>
2009-06-06 19:00:32 +0000

    use new run_loop to handle BT stack

--- trunk/src/run_loop.c
+++ trunk/src/run_loop.c
@@ -5,9 +5,72 @@
  */
 
 #include "run_loop.h"
+#include <sys/select.h>
+#include <stdlib.h>
 
 // the run loop
-static data_source_t the_run_loop;
+static data_source_t * the_run_loop = NULL;
+
+/**
+ * Add data_source to run_loop
+ */
+void run_loop_add(data_source_t *ds){
+    // check if already in list
+    data_source_t *it;
+    for (it = the_run_loop; it ; it = it->next){
+        if (it == ds) {
+            return;
+        }
+    }
+    // add first
+    ds->next = the_run_loop;
+    the_run_loop = ds;
+}
+
+/**
+ * Remove data_source from run loop
+ *
+ * @note: assumes that data_source_t.next is first element in data_source
+ */
+int run_loop_remove(data_source_t *ds){
+    data_source_t *it;
+    for (it = (data_source_t *) &the_run_loop; it ; it = it->next){
+        if (it->next == ds){
+            it->next =  ds->next;
+            return 0;
+        }
+    }
+    return -1;
+}
+
+/**
+ * Execute run_loop
+ */
+void run_loop_execute() {
+    fd_set descriptors;
+    data_source_t *ds;
+    int highest;
+    while (1) {
+        // collect FDs
+        FD_ZERO(&descriptors);
+        int highest_fd = 0;
+        for (ds = the_run_loop; ds ; ds = ds->next){
+            if (ds->fd) {
+                FD_SET(ds->fd, &descriptors);
+                if (ds->fd > highest_fd) {
+                    highest_fd = ds->fd;
+                }
+            }
+        }
+        // wait for ready FDs
+        select( highest+1, &descriptors, NULL, NULL, NULL);
+
+        // process input
+        for (ds = the_run_loop; ds != NULL ; ds = ds->next){
+            ds->process(ds, FD_ISSET(ds->fd, &descriptors));
+        }
+    }
+}
 
 
 
