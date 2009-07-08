9320a67b01d03652bf86821e26179fe9e8b03454
matthias.ringwald <matthias.ringwald@1a0a8af8-31b5-11de-8e0c-53a27eea117e>
2009-07-08 22:21:02 +0000

    adapting usb test code into HCI transport abstraction

--- src/hci_transport.h
+++ src/hci_transport.h
@@ -11,6 +11,27 @@
 #include <stdint.h>
 #include "run_loop.h"
 
+/**
+ * Indicates (first byte) that this pkt to the bluetooth module is a command pkt. The
+ * module will send a #HCI_EVENT_PACKET for response.
+ */
+#define HCI_COMMAND_DATA_PACKET	0x01
+
+/**
+ * Indicates (first byte) that this pkt is a acl pkt. Will be sent to and from the module.
+ */
+#define HCI_ACL_DATA_PACKET	    0x02
+
+/**
+ * Indicates (first byte) that this pkt is a sco pkt. Will be sent to and from the module.
+ */
+#define HCI_SCO_DATA_PACKET	    0x03
+
+/**
+ * Indicates (first byte) that this pkt is an event pkt. Only sent by the bluetooth module.
+ */
+#define HCI_EVENT_PACKET	    0x04
+
 typedef struct {
     data_source_t ds;
     int    (*open)(void *transport_config);
---
+++ src/hci_transport_usb.h
@@ -0,0 +1,7 @@
+/*
+ *  hci_transport_usb.h
+ *
+ *  Created by Matthias Ringwald on 7/8/09.
+ */
+
+extern hci_transport_t * hci_transport_usb_instance();
