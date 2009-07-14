0a6e8820523eca5392288b05dcf86d6a911c46b1
matthias.ringwald <matthias.ringwald@1a0a8af8-31b5-11de-8e0c-53a27eea117e>
2009-07-14 18:29:29 +0000

    remove test code from daemon and put into example/test.c - not working yet

--- trunk/src/btstack.h
+++ trunk/src/btstack.h
@@ -7,3 +7,19 @@
  *  
  */
 
+// init BTstack library
+int bt_open();
+
+// stop using BTstack library
+int bt_close();
+
+// send hci cmd packet
+int hci_send_cmd(hci_cmd_t *cmd, ...);
+
+// send hci acl packet
+int hci_send_acl_packet(uint8_t *packet, int size);
+
+// register packet and event handler
+void bt_register_event_packet_handler(void (*handler)(uint8_t *packet, int size));
+void bt_register_acl_packet_handler  (void (*handler)(uint8_t *packet, int size));
+
