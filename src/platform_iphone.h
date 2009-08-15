ac3013478d4298f51503d6c031b104a98145fa32
matthias.ringwald <matthias.ringwald@1a0a8af8-31b5-11de-8e0c-53a27eea117e>
2009-08-15 21:32:06 +0000

    start collecting iPhone specific stuff in platform_iphone.m

---
+++ src/platform_iphone.h
@@ -0,0 +1,13 @@
+//
+//  platform_iphone.h
+//
+//  support for the iPhone platform
+//
+//  Created by Matthias Ringwald on 8/15/09.
+//
+
+#pragma once
+
+#include "hci.h"
+
+void platform_iphone_status_handler(BLUETOOTH_STATE state);
