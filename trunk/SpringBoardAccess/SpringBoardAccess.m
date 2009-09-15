5e7f69693e2a02790ba0cd98435afc9018a47a4a
matthias.ringwald <matthias.ringwald@1a0a8af8-31b5-11de-8e0c-53a27eea117e>
2009-09-15 21:13:32 +0000

    create dummy SpringBoardAccess MobileSubstrate extension when configured for iPhone

---
+++ trunk/SpringBoardAccess/SpringBoardAccess.m
@@ -0,0 +1,14 @@
+//
+//  SpringBoardAccess.m
+//
+//  Created by Matthias Ringwald on 9/15/09.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreFoundation/CoreFoundation.h>
+
+#import "../3rdparty/substrate.h"
+
+void SpringBoardAccessInitialize(){
+	NSLog(@"SpringBoardAccessInitialize called!");
+}
