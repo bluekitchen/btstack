9ec7547d132313365845227ff62ad22837aa5a1d
matthias.ringwald <matthias.ringwald@1a0a8af8-31b5-11de-8e0c-53a27eea117e>
2009-09-28 21:23:31 +0000

    use ldid already in makefile

--- trunk/SpringBoardAccess/package.sh
+++ trunk/SpringBoardAccess/package.sh
@@ -10,8 +10,6 @@ mkdir -p SpringBoardAccess/usr/local/bin
 cp SpringBoardAccess-test SpringBoardAccess/usr/local/bin
 
 mkdir -p SpringBoardAccess/Library/MobileSubstrate/DynamicLibraries
-export CODESIGN_ALLOCATE=/Developer/Platforms/iPhoneOS.platform/Developer/usr/bin/codesign_allocate ; ldid -S SpringBoardAccess.dylib
-export CODESIGN_ALLOCATE=/Developer/Platforms/iPhoneOS.platform/Developer/usr/bin/codesign_allocate ; ldid -S SpringBoardAccess-test
 cp SpringBoardAccess.dylib SpringBoardAccess.plist SpringBoardAccess/Library/MobileSubstrate/DynamicLibraries
 
 echo Packaging SpringBoardAccess
