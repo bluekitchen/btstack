1f86f1bbfd68900bd9c56c5f723739f8fe16033f
matthias.ringwald <matthias.ringwald@1a0a8af8-31b5-11de-8e0c-53a27eea117e>
2009-10-05 22:06:12 +0000

    merge event and data handler into general packet handler

--- example/rfcomm.c
+++ example/rfcomm.c
@@ -26,19 +26,6 @@ bd_addr_t addr = {0x00,0x1c,0x4d,0x02,0x1a,0x77};  // WiiMote
 hci_con_handle_t con_handle;
 uint16_t source_cid;
 
-void data_handler(uint8_t *packet, uint16_t size){
-	// just dump data for now
-	hexdump( packet, size );
-
-    // 	received 1. message BT_RF_COMM_UA
-    if (size == 12 && packet[9] == BT_RFCOMM_UA){
-        printf("Received RFCOMM unnumbered acknowledgement - mutliplexer working.. sorry, this hacks stop here\n");
-        // send SABM command on dlci 0
-        _bt_rfcomm_send_sabm(source_cid, 1, 1);
-        // exit(0);
-    }
-}
-
 void _bt_rfcomm_send_sabm(uint16_t source_cid, uint8_t initiator, uint8_t dlci)
 {
 	uint8_t payload[4];
@@ -48,60 +35,94 @@ void _bt_rfcomm_send_sabm(uint16_t source_cid, uint8_t initiator, uint8_t dlci)
 	payload[1] = BT_RFCOMM_SABM;        // control field
 	payload[2] = 1;                     // set EA bit to 1 to indicate 7 bit length field
 	payload[3] = crc8_calc(payload, BT_RFCOMM_CRC_CHECK_LEN);   // calc fcs, 3 bytes to be send...
-    l2cap_send( source_cid, payload, sizeof(payload));
+    bt_send_l2cap( source_cid, payload, sizeof(payload));
 }
 
-void event_handler(uint8_t *packet, uint16_t size){
-	// handle HCI init failure
-	if (packet[0] == BTSTACK_EVENT_POWERON_FAILED){
-		printf("HCI Init failed - make sure you have turned off Bluetooth in the System Settings\n");
-		exit(1);
-	}
 
-    // bt stack activated, get started - set local name
-    if (packet[0] == BTSTACK_EVENT_STATE && packet[2] == HCI_STATE_WORKING) {
-        bt_send_cmd(&hci_write_local_name, "BTstack-Test");
-    }
-    
-	// use pairing yes/no
-    if ( COMMAND_COMPLETE_EVENT(packet, hci_write_local_name) ) {
-        bt_send_cmd(&hci_write_authentication_enable, 1);
-    }
-	
-	// connect to RFCOMM device (PSM 0x03) at addr
-    if ( COMMAND_COMPLETE_EVENT(packet, hci_write_authentication_enable) ) {
-		bt_send_cmd(&l2cap_create_channel, addr, 0x03);
-		printf("Turn on the Zeemote\n");
-	}
+void packet_handler(uint8_t packet_type, uint8_t *packet, uint16_t size){
+	bd_addr_t event_addr;
 
-	
-	// inform about pin code request
-	if (packet[0] == HCI_EVENT_PIN_CODE_REQUEST){
-		bt_flip_addr(addr, &packet[2]); 
-		bt_send_cmd(&hci_pin_code_request_reply, &addr, 4, "0000");
-		printf("Please enter PIN 0000 on remote device\n");
-	}
-	
-	// inform about new l2cap connection
-	if (packet[0] == L2CAP_EVENT_CHANNEL_OPENED){
-		bd_addr_t addr;
-		bt_flip_addr(addr, &packet[2]);
-		uint16_t psm = READ_BT_16(packet, 10); 
-		source_cid = READ_BT_16(packet, 12); 
-		con_handle = READ_BT_16(packet, 8);
-		printf("Channel successfully opened: ");
-		print_bd_addr(addr);
-		printf(", handle 0x%02x, psm 0x%02x, source cid 0x%02x, dest cid 0x%02x\n",
-			   con_handle, psm, source_cid,  READ_BT_16(packet, 14));
-        
-        // send SABM command on dlci 0
-        _bt_rfcomm_send_sabm(source_cid, 1, 0);
-	}
-	
-	// connection closed -> quit tes app
-	if (packet[0] == HCI_EVENT_DISCONNECTION_COMPLETE) {
-		printf("Basebank connection closed, exit.\n");
-		exit(0);
+	switch (packet_type) {
+			
+		case L2CAP_DATA_PACKET:
+			
+			// just dump data for now
+			hexdump( packet, size );
+			
+			// 	received 1. message BT_RF_COMM_UA
+			if (size == 12 && packet[9] == BT_RFCOMM_UA){
+				printf("Received RFCOMM unnumbered acknowledgement - mutliplexer working.. sorry, this hacks stop here\n");
+				// maybe send SABM command on dlci 0
+				//_bt_rfcomm_send_sabm(source_cid, 1, 1);
+				exit(0);
+			}
+			break;
+			
+		case HCI_EVENT_PACKET:
+			
+			switch (packet[0]) {
+					
+				case BTSTACK_EVENT_POWERON_FAILED:
+					// handle HCI init failure
+					printf("HCI Init failed - make sure you have turned off Bluetooth in the System Settings\n");
+					exit(1);
+					break;		
+					
+				case BTSTACK_EVENT_STATE:
+					// bt stack activated, get started - set local name
+					if (packet[2] == HCI_STATE_WORKING) {
+						bt_send_cmd(&hci_write_local_name, "BTstack-Test");
+					}
+					break;
+					
+				case HCI_EVENT_PIN_CODE_REQUEST:
+					// inform about pin code request
+					bt_flip_addr(event_addr, &packet[2]); 
+					bt_send_cmd(&hci_pin_code_request_reply, &event_addr, 4, "0000");
+					printf("Please enter PIN 0000 on remote device\n");
+					break;
+			
+				case L2CAP_EVENT_CHANNEL_OPENED:
+					// inform about new l2cap connection
+					bt_flip_addr(event_addr, &packet[2]);
+					uint16_t psm = READ_BT_16(packet, 10); 
+					source_cid = READ_BT_16(packet, 12); 
+					con_handle = READ_BT_16(packet, 8);
+					printf("Channel successfully opened: ");
+					print_bd_addr(event_addr);
+					printf(", handle 0x%02x, psm 0x%02x, source cid 0x%02x, dest cid 0x%02x\n",
+						   con_handle, psm, source_cid,  READ_BT_16(packet, 14));
+					
+					// send SABM command on dlci 0
+					_bt_rfcomm_send_sabm(source_cid, 1, 0);
+					break;
+			
+				case HCI_EVENT_DISCONNECTION_COMPLETE:
+					// connection closed -> quit test app
+					printf("Basebank connection closed, exit.\n");
+					exit(0);
+					break;
+				
+				case HCI_EVENT_COMMAND_COMPLETE:
+					// use pairing yes/no
+					if ( COMMAND_COMPLETE_EVENT(packet, hci_write_local_name) ) {
+						bt_send_cmd(&hci_write_authentication_enable, 1);
+					}
+			
+					// connect to RFCOMM device (PSM 0x03) at addr
+					if ( COMMAND_COMPLETE_EVENT(packet, hci_write_authentication_enable) ) {
+						bt_send_cmd(&l2cap_create_channel, addr, 0x03);
+						printf("Turn on the Zeemote\n");
+					}
+					break;
+					
+				default:
+					// unhandled event
+					break;
+			}
+		default:
+			// unhandled packet type
+			break;
 	}
 }
 
@@ -111,8 +132,7 @@ int main (int argc, const char * argv[]){
 		printf("Failed to open connection to BTdaemon\n");
 		return err;
 	}
-	bt_register_event_packet_handler(event_handler);
-	bt_register_data_packet_handler(data_handler);
+	bt_register_packet_handler(packet_handler);
 	bt_send_cmd(&btstack_set_power_mode, HCI_POWER_ON );
 	run_loop_execute();
 	bt_close();
--- example/test.c
+++ example/test.c
@@ -18,78 +18,101 @@ hci_con_handle_t con_handle;
 uint16_t source_cid_interrupt;
 uint16_t source_cid_control;
 
-void data_handler(uint8_t *packet, uint16_t size){
-	// just dump data for now
-	hexdump( packet, size );
+void packet_handler(uint8_t packet_type, uint8_t *packet, uint16_t size){
+	bd_addr_t event_addr;
+
+	switch (packet_type) {
+			
+		case L2CAP_DATA_PACKET:
+			// just dump data for now
+			hexdump( packet, size );
 	
-	// HOME => disconnect
-	if (packet[8] == 0xA1) {							// Status report
-		if (packet[9] == 0x30 || packet[9] == 0x31) {   // type 0x30 or 0x31
-			if (packet[11] & 0x080) {                   // homne button pressed
-				printf("Disconnect baseband\n");
-				bt_send_cmd(&hci_disconnect, con_handle, 0x13); // remote closed connection
+			// HOME => disconnect
+			if (packet[8] == 0xA1) {							// Status report
+				if (packet[9] == 0x30 || packet[9] == 0x31) {   // type 0x30 or 0x31
+					if (packet[11] & 0x080) {                   // homne button pressed
+						printf("Disconnect baseband\n");
+						bt_send_cmd(&hci_disconnect, con_handle, 0x13); // remote closed connection
+					}
+				}
 			}
-		}
-	}
-}
+			break;
+			
+		case HCI_EVENT_PACKET:
+			
+			switch (packet[0]) {
 
-void event_handler(uint8_t *packet, uint16_t size){
-	// handle HCI init failure
-	if (packet[0] == BTSTACK_EVENT_POWERON_FAILED){
-		printf("HCI Init failed - make sure you have turned off Bluetooth in the System Settings\n");
-		exit(1);
-	}
+				case BTSTACK_EVENT_POWERON_FAILED:
+					printf("HCI Init failed - make sure you have turned off Bluetooth in the System Settings\n");
+					exit(1);
+					break;
+					
+				case BTSTACK_EVENT_STATE:
+					// bt stack activated, get started - set local name
+					if (packet[2] == HCI_STATE_WORKING) {
+						bt_send_cmd(&hci_write_local_name, "BTstack-Test");
+					}
+					break;
+					
+				case HCI_EVENT_PIN_CODE_REQUEST:
+					// inform about pin code request
+					printf("Please enter PIN 1234 on remote device\n");
+					break;
 
-    // bt stack activated, get started - set local name
-    if (packet[0] == BTSTACK_EVENT_STATE && packet[2] == HCI_STATE_WORKING) {
-        bt_send_cmd(&hci_write_local_name, "BTstack-Test");
-    }
-    
-	// use pairing yes/no
-    if ( COMMAND_COMPLETE_EVENT(packet, hci_write_local_name) ) {
-        bt_send_cmd(&hci_write_authentication_enable, 0);
-    }
-	
-	// connect to HID device (PSM 0x13) at addr
-    if ( COMMAND_COMPLETE_EVENT(packet, hci_write_authentication_enable) ) {
-		bt_send_cmd(&l2cap_create_channel, addr, 0x13);
-		printf("Press 1+2 on WiiMote to make it discoverable - Press HOME to disconnect later :)\n");
-	}
-
-	
-	// inform about pin code request
-	if (packet[0] == HCI_EVENT_PIN_CODE_REQUEST){
-		printf("Please enter PIN 1234 on remote device\n");
-	}
-	
-	// inform about new l2cap connection
-	if (packet[0] == L2CAP_EVENT_CHANNEL_OPENED){
-		bd_addr_t addr;
-		bt_flip_addr(addr, &packet[2]);
-		uint16_t psm = READ_BT_16(packet, 10); 
-		uint16_t source_cid = READ_BT_16(packet, 12); 
-		con_handle = READ_BT_16(packet, 8);
-		printf("Channel successfully opened: ");
-		print_bd_addr(addr);
-		printf(", handle 0x%02x, psm 0x%02x, source cid 0x%02x, dest cid 0x%02x\n",
-			   con_handle, psm, source_cid,  READ_BT_16(packet, 14));
-
-		if (psm == 0x13) {
-			source_cid_interrupt = source_cid;
-			// interupt channel openedn succesfully, now open control channel, too.
-			bt_send_cmd(&l2cap_create_channel, addr, 0x11);
-		} else {
-			source_cid_control = source_cid;
-			// request acceleration data..
-			uint8_t setMode31[] = { 0x52, 0x12, 0x00, 0x31 };
-			l2cap_send( source_cid, setMode31, sizeof(setMode31));
-		}
-	}
-	
-	// connection closed -> quit tes app
-	if (packet[0] == HCI_EVENT_DISCONNECTION_COMPLETE) {
-		printf("Basebank connection closed, exit.\n");
-		exit(0);
+				case L2CAP_EVENT_CHANNEL_OPENED:
+					// inform about new l2cap connection
+					bt_flip_addr(event_addr, &packet[2]);
+					uint16_t psm = READ_BT_16(packet, 10); 
+					uint16_t source_cid = READ_BT_16(packet, 12); 
+					con_handle = READ_BT_16(packet, 8);
+					printf("Channel successfully opened: ");
+					print_bd_addr(event_addr);
+					printf(", handle 0x%02x, psm 0x%02x, source cid 0x%02x, dest cid 0x%02x\n",
+						   con_handle, psm, source_cid,  READ_BT_16(packet, 14));
+					
+					if (psm == 0x13) {
+						source_cid_interrupt = source_cid;
+						// interupt channel openedn succesfully, now open control channel, too.
+						bt_send_cmd(&l2cap_create_channel, event_addr, 0x11);
+					} else {
+						source_cid_control = source_cid;
+						// request acceleration data..
+						uint8_t setMode31[] = { 0x52, 0x12, 0x00, 0x31 };
+						bt_send_l2cap( source_cid, setMode31, sizeof(setMode31));
+						// stop blinking
+						uint8_t setLEDs[] = { 0x52, 0x11, 0x10 };
+                        bt_send_l2cap( source_cid, setLEDs, sizeof(setLEDs));
+					}
+					break;
+					
+				case HCI_EVENT_DISCONNECTION_COMPLETE:
+					// connection closed -> quit tes app
+					printf("Basebank connection closed, exit.\n");
+					exit(0);
+					break;
+					
+				case HCI_EVENT_COMMAND_COMPLETE:
+					// use pairing yes/no
+					if ( COMMAND_COMPLETE_EVENT(packet, hci_write_local_name) ) {
+						bt_send_cmd(&hci_write_authentication_enable, 0);
+					}
+					
+					// connect to HID device (PSM 0x13) at addr
+					if ( COMMAND_COMPLETE_EVENT(packet, hci_write_authentication_enable) ) {
+						bt_send_cmd(&l2cap_create_channel, addr, 0x13);
+						printf("Press 1+2 on WiiMote to make it discoverable - Press HOME to disconnect later :)\n");
+					}
+					break;
+					
+				default:
+					// other event
+					break;
+			}
+			break;
+			
+		default:
+			// other packet type
+			break;
 	}
 }
 
@@ -99,8 +122,7 @@ int main (int argc, const char * argv[]){
 		printf("Failed to open connection to BTdaemon\n");
 		return err;
 	}
-	bt_register_event_packet_handler(event_handler);
-	bt_register_data_packet_handler(data_handler);
+	bt_register_packet_handler(packet_handler);
 	bt_send_cmd(&btstack_set_power_mode, HCI_POWER_ON );
 	run_loop_execute();
 	bt_close();
