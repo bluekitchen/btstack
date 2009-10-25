3d11c683772146791a35709933131fa1db2bda94
matthias.ringwald <matthias.ringwald@1a0a8af8-31b5-11de-8e0c-53a27eea117e>
2009-10-25 20:00:31 +0000

    add linebreak after debug statement

--- trunk/src/platform_iphone.c
+++ trunk/src/platform_iphone.c
@@ -19,17 +19,17 @@ void platform_iphone_status_handler(BLUETOOTH_STATE state){
         case BLUETOOTH_OFF:
             SBA_removeStatusBarImage("BTstack");
             SBA_removeStatusBarImage("BTstackActive");
-            printf("Bluetooth status: OFF");
+            printf("Bluetooth status: OFF\n");
             break;
         case BLUETOOTH_ON:
             SBA_removeStatusBarImage("BTstackActive");
             SBA_addStatusBarImage("BTstack");
-            printf("Bluetooth status: ON");
+            printf("Bluetooth status: ON\n");
             break;
         case BLUETOOTH_ACTIVE:
             SBA_removeStatusBarImage("BTstack");
             SBA_addStatusBarImage("BTstackActive");
-            printf("Bluetooth status: ACTIVE");
+            printf("Bluetooth status: ACTIVE\n");
             break;
         default:
             break;
