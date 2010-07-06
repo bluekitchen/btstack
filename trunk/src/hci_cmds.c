0cd96aa7023f984318a1e5d21fd0dab6fbdd8d21
matthias.ringwald <matthias.ringwald@1a0a8af8-31b5-11de-8e0c-53a27eea117e>
2010-07-06 18:13:54 +0000

    prevent segfault by only copying strlen(ptr) for Bluetooth name - thanks to akolhlsmith for detecting this

--- trunk/src/hci_cmds.c
+++ trunk/src/hci_cmds.c
@@ -110,7 +110,7 @@ uint16_t hci_create_cmd_internal(uint8_t *hci_cmd_buffer, hci_cmd_t *cmd, va_lis
                 break;
             case 'N': { // UTF-8 string, null terminated
                 ptr = va_arg(argptr, uint8_t *);
-                uint16_t len = strlen(ptr);
+                uint16_t len = strlen((const char*) ptr);
                 if (len > 248) {
                     len = 248;
                 }
