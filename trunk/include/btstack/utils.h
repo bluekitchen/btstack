70fd2733f177473d1f4da7eccbb1d3509dfa3af1
matthias.ringwald <matthias.ringwald@1a0a8af8-31b5-11de-8e0c-53a27eea117e>
2011-08-01 11:55:41 +0000

    re-add prototype or print_bd_addr to not break existing code

--- trunk/include/btstack/utils.h
+++ trunk/include/btstack/utils.h
@@ -106,9 +106,12 @@ void net_store_32(uint8_t *buffer, uint16_t pos, uint32_t value);
 
 void hexdump(void *data, int size);
 void printUUID(uint8_t *uuid);
+
+// @deprecated please use more convenient bd_addr_to_str
+void print_bd_addr( bd_addr_t addr);
 char * bd_addr_to_str(bd_addr_t addr);
+
 int sscan_bd_addr(uint8_t * addr_string, bd_addr_t addr);
-char * bd_addr_to_str(bd_addr_t addr);
     
 uint8_t crc8_check(uint8_t *data, uint16_t len, uint8_t check_sum);
 uint8_t crc8_calc(uint8_t *data, uint16_t len);
