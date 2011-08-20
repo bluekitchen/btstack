bcbb77fff8be4a2c811f44d9227ee86b040a167c
matthias.ringwald <matthias.ringwald@1a0a8af8-31b5-11de-8e0c-53a27eea117e>
2011-08-20 12:44:32 +0000

    mark function tables and controls structs as const

--- src/hci.h
+++ src/hci.h
@@ -295,7 +295,7 @@ uint16_t hci_create_cmd(uint8_t *hci_cmd_buffer, hci_cmd_t *cmd, ...);
 uint16_t hci_create_cmd_internal(uint8_t *hci_cmd_buffer, const hci_cmd_t *cmd, va_list argptr);
 
 // set up HCI
-void hci_init(hci_transport_t *transport, void *config, bt_control_t *control, remote_device_db_t * remote_device_db);
+void hci_init(const hci_transport_t *transport, void *config, const bt_control_t *control, const remote_device_db_t * remote_device_db);
 void hci_register_packet_handler(void (*handler)(uint8_t packet_type, uint8_t *packet, uint16_t size));
 void hci_close(void);
 
