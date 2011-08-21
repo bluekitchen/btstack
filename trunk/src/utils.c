fc66eb0227417dcbe6fb3a97c03fe68ad5c7a537
matthias.ringwald <matthias.ringwald@1a0a8af8-31b5-11de-8e0c-53a27eea117e>
2011-08-21 13:25:11 +0000

    return addr to bd_addr_to_str_buffer instead of addr of function

--- trunk/src/utils.c
+++ trunk/src/utils.c
@@ -94,7 +94,7 @@ static char uuid_to_str_buffer[];
 static char bd_addr_to_str_buffer[6*3];  // 12:45:78:01:34:67\0
 char * bd_addr_to_str(bd_addr_t addr){
     sprintf(bd_addr_to_str_buffer, "%02x:%02x:%02x:%02x:%02x:%02x", addr[0], addr[1], addr[2], addr[3], addr[4], addr[5]);
-    return (char *) bd_addr_to_str;
+    return (char *) bd_addr_to_str_buffer;
 }
 
 void print_bd_addr( bd_addr_t addr){
