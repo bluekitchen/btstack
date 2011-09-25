04e42ad0d407383e6dd127996098e6f41d738209
matthias.ringwald <matthias.ringwald@1a0a8af8-31b5-11de-8e0c-53a27eea117e>
2011-09-25 14:03:40 +0000

    don't send test data in echo example

--- example/rfcomm-echo.c
+++ example/rfcomm-echo.c
@@ -182,11 +182,6 @@ void packet_handler(uint8_t packet_type, uint16_t channel, uint8_t *packet, uint
                     bt_send_cmd(&btstack_set_discoverable, 1);
                     break;
                 
-                case RFCOMM_EVENT_CREDITS:
-                    sprintf((char*)test_data, "-> %09u <- ", counter++);
-                    bt_send_rfcomm(rfcomm_channel_id, test_data, mtu);
-                    break;
-                    
 				case HCI_EVENT_PIN_CODE_REQUEST:
 					// inform about pin code request
 					printf("Using PIN 0000\n");
