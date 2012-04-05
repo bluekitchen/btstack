00b8de360cfee59d9de34c9d8e054d179f3094a6
matthias.ringwald <matthias.ringwald@1a0a8af8-31b5-11de-8e0c-53a27eea117e>
2012-04-05 15:33:30 +0000

    assume we're using iOS SDK 5

--- trunk/config-cydia.sh
+++ trunk/config-cydia.sh
@@ -1,1 +1,1 @@
-./configure --target=iphone --enable-launchd --enable-ldid --with-prefsbundle --with-uart-speed=921600 --with-gcc-version=4.2
+./configure --target=iphone --enable-launchd --enable-ldid --with-prefsbundle --with-uart-speed=921600 --with-sdk-version=5.0 --with-gcc-version=4.2
