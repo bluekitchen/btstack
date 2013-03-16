3f3f45c7227d1539435babd7d943915ace280810
matthias.ringwald <matthias.ringwald@1a0a8af8-31b5-11de-8e0c-53a27eea117e>
2013-03-16 22:46:02 +0000

    forgot to add config-iphone.h

---
+++ trunk/config-iphone.h
@@ -0,0 +1,20 @@
+// config.h created by configure for BTstack  Thu Feb 28 23:50:03 CET 2013
+#define HAVE_TRANSPORT_H4
+#define UART_DEVICE "/dev/tty.bluetooth"
+#define UART_SPEED 921600
+#define USE_BLUETOOL
+#define USE_POWERMANAGEMENT
+#define USE_SPRINGBOARD
+#define USE_LAUNCHD
+#define USE_COCOA_RUN_LOOP
+#define USE_POSIX_RUN_LOOP
+#define HAVE_SDP
+#define HAVE_RFCOMM
+#define REMOTE_DEVICE_DB remote_device_db_iphone
+#define HAVE_SO_NOSIGPIPE
+#define HAVE_TIME
+#define HAVE_MALLOC
+#define HAVE_BZERO
+#define ENABLE_LOG_INFO 
+#define ENABLE_LOG_ERROR
+#define HCI_ACL_PAYLOAD_SIZE 1021
