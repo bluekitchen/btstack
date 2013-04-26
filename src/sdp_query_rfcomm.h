d3d614de4918ba94509def7e85740b3b8a988848
matthias.ringwald@gmail.com <matthias.ringwald@gmail.com@1a0a8af8-31b5-11de-8e0c-53a27eea117e>
2013-04-26 20:18:25 +0000

    new files for SDP queries

---
+++ src/sdp_query_rfcomm.h
@@ -0,0 +1,31 @@
+
+//*****************************************************************************
+//
+// minimal setup for SDP client over USB or UART
+//
+//*****************************************************************************
+
+#include <btstack/utils.h>
+#include "sdp_parser.h"
+
+
+typedef struct sdp_query_rfcomm_event {
+    uint8_t type;
+} sdp_query_rfcomm_event_t;
+
+typedef struct sdp_query_rfcomm_service_event {
+    uint8_t type;
+    uint8_t rfcomm_channel_nr;
+    uint8_t * service_name;
+} sdp_query_rfcomm_service_event_t;
+
+typedef struct sdp_query_rfcomm_complete_event {
+    uint8_t type;
+    uint8_t status; // 0 == OK
+} sdp_query_rfcomm_complete_event_t;
+
+
+void sdp_query_rfcomm_init(void);
+void sdp_query_rfcomm_channel_and_name_for_service_with_uuid(bd_addr_t remote, uint16_t uuid);
+void sdp_query_rfcomm_channel_and_name_for_service_with_service_search_pattern(bd_addr_t remote, uint8_t * des_serviceSearchPattern);
+void sdp_query_rfcomm_register_callback(void(*sdp_app_callback)(sdp_query_rfcomm_event_t * event, void * context), void * context);
