dd280e25fa56ecc05f38bae9e59b6d61adcdda0b
matthias.ringwald@gmail.com <matthias.ringwald@gmail.com@1a0a8af8-31b5-11de-8e0c-53a27eea117e>
2013-05-24 20:29:02 +0000

    unit test for sdp rfcomm queries and sdp parser

---
+++ test/sdp_client/config.h
@@ -0,0 +1,13 @@
+// config.h created by configure for BTstack  Sun Sep 25 16:06:59 CEST 2011
+#pragma once
+#define HAVE_TRANSPORT_H4
+#define UART_DEVICE "/dev/tty.usbserial-A600eIDu"
+#define UART_SPEED 115200
+#define USE_POSIX_RUN_LOOP
+#define HAVE_TIME
+#define HAVE_MALLOC
+#define HAVE_BZERO
+#define ENABLE_LOG_INFO 
+#define ENABLE_LOG_ERROR
+#define HCI_ACL_PAYLOAD_SIZE 1021
+// #define HCI_ACL_PAYLOAD_SIZE 52
