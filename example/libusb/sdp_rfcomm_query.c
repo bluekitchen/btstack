e076f1242fa5d38a0c1200f7a827c2142adfbdc2
matthias.ringwald@gmail.com <matthias.ringwald@gmail.com@1a0a8af8-31b5-11de-8e0c-53a27eea117e>
2013-06-10 10:43:53 +0000

    examples for POSIX/libusb platform, sdp rfcomm query

---
+++ example/libusb/sdp_rfcomm_query.c
@@ -0,0 +1,119 @@
+
+//*****************************************************************************
+//
+// minimal setup for SDP client over USB or UART
+//
+//*****************************************************************************
+
+#include "config.h"
+
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "sdp_query_rfcomm.h"
+
+#include <btstack/hci_cmds.h>
+#include <btstack/run_loop.h>
+
+#include "hci.h"
+#include "btstack_memory.h"
+#include "hci_dump.h"
+#include "l2cap.h"
+
+static bd_addr_t remote = {0x04,0x0C,0xCE,0xE4,0x85,0xD3};
+
+static uint8_t  service_index = 0;
+static uint8_t  channel_nr[10];
+static char*    service_name[10];
+
+
+static void packet_handler (void * connection, uint8_t packet_type, uint16_t channel, uint8_t *packet, uint16_t size){
+    printf("packet_handler type %u, packet[0] %x\n", packet_type, packet[0]);
+
+    if (packet_type != HCI_EVENT_PACKET) return;
+    uint8_t event = packet[0];
+
+    switch (event) {
+        case BTSTACK_EVENT_STATE:
+            // bt stack activated, get started 
+            if (packet[2] == HCI_STATE_WORKING){
+                sdp_query_rfcomm_channel_and_name_for_uuid(remote, 0x1002);
+            }
+            break;
+        default:
+            break;
+    }
+}
+
+static void btstack_setup(){
+    /// GET STARTED ///
+    btstack_memory_init();
+    run_loop_init(RUN_LOOP_POSIX);
+    
+    // hci_dump_open("/tmp/hci_dump_sdp_client.pklg", HCI_DUMP_PACKETLOGGER);
+   
+    hci_transport_t    * transport = hci_transport_usb_instance();
+    hci_uart_config_t  * config = NULL;
+    bt_control_t       * control   = NULL;    
+
+    remote_device_db_t * remote_db = (remote_device_db_t *) &remote_device_db_memory;
+    hci_init(transport, config, control, remote_db);
+    printf("Client HCI init done\r\n");
+    
+    // init L2CAP
+    l2cap_init();
+    l2cap_register_packet_handler(packet_handler);
+
+    // turn on!
+    hci_power_control(HCI_POWER_ON);
+}
+
+void store_found_service(uint8_t * name, uint8_t port){
+    printf("APP: Service name: '%s', RFCOMM port %u\n", name, port);
+    channel_nr[service_index] = port;
+    service_name[service_index] = (char*) malloc(SDP_SERVICE_NAME_LEN+1);
+    strncpy(service_name[service_index], (char*) name, SDP_SERVICE_NAME_LEN);
+    service_name[service_index][SDP_SERVICE_NAME_LEN] = 0;
+    service_index++;
+}
+
+void report_found_services(){
+    printf("\n *** Client query response done. ");
+    if (service_index == 0){
+        printf("No service found.\n\n");
+    } else {
+        printf("Found following %d services:\n", service_index);
+    }
+    int i;
+    for (i=0; i<service_index; i++){
+        printf("     Service name %s, RFCOMM port %u\n", service_name[i], channel_nr[i]);
+    }    
+    printf(" ***\n\n");
+}
+
+void handle_query_rfcomm_event(sdp_query_event_t * event, void * context){
+    sdp_query_rfcomm_service_event_t * ve;
+    sdp_query_complete_event_t * ce;
+            
+    switch (event->type){
+        case SDP_QUERY_RFCOMM_SERVICE:
+            ve = (sdp_query_rfcomm_service_event_t*) event;
+            store_found_service(ve->service_name, ve->channel_nr);
+            break;
+        case SDP_QUERY_COMPLETE:
+            ce = (sdp_query_complete_event_t*) event;
+            report_found_services();
+            break;
+    }
+}
+
+int main(void){
+    sdp_query_rfcomm_register_callback(handle_query_rfcomm_event, NULL);
+
+    btstack_setup();
+    // go!
+    run_loop_execute(); 
+    return 0;
+}
