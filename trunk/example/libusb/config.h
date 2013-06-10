11b211336b298b067c9368ed297073d017018fe0
matthias.ringwald@gmail.com <matthias.ringwald@gmail.com@1a0a8af8-31b5-11de-8e0c-53a27eea117e>
2013-06-10 10:43:53 +0000

    examples for POSIX/libusb platform, sdp rfcomm query

---
+++ trunk/example/libusb/config.h
@@ -0,0 +1,15 @@
+// config.h created by configure for BTstack  Tue Jun 4 23:10:20 CEST 2013
+#define HAVE_TRANSPORT_USB
+#define USB_PRODUCT_ID 0
+#define USB_VENDOR_ID 0
+#define USE_POSIX_RUN_LOOP
+#define HAVE_SDP
+#define HAVE_RFCOMM
+#define REMOTE_DEVICE_DB remote_device_db_iphone
+#define HAVE_SO_NOSIGPIPE
+#define HAVE_TIME
+#define HAVE_MALLOC
+#define HAVE_BZERO
+#define ENABLE_LOG_INFO 
+#define ENABLE_LOG_ERROR
+#define HCI_ACL_PAYLOAD_SIZE 1021
