1ac0d173007f9db557781cdcfc21a020a27fe3ba
matthias.ringwald@gmail.com <matthias.ringwald@gmail.com@1a0a8af8-31b5-11de-8e0c-53a27eea117e>
2013-11-01 14:07:32 +0000

    added public domain AES-128 implementation for testing/devlopment

---
+++ trunk/example/libusb/rijndael.h
@@ -0,0 +1,27 @@
+// =============================== RIJNDAEL.H   ===============================
+// from http://www.efgh.com/software/rijndael.htm,
+// License: Public Domain, 
+// Author:  Philip J. Erdelsky
+
+#ifndef H__RIJNDAEL
+#define H__RIJNDAEL
+
+int rijndaelStartOfCode();
+int rijndaelSetupEncrypt(unsigned long *rk, const unsigned char *key,
+  int keybits);
+int rijndaelSetupDecrypt(unsigned long *rk, const unsigned char *key,
+  int keybits);
+void rijndaelEncrypt(const unsigned long *rk, int nrounds,
+  const unsigned char plaintext[16], unsigned char ciphertext[16]);
+void rijndaelDecrypt(const unsigned long *rk, int nrounds,
+  const unsigned char ciphertext[16], unsigned char plaintext[16]);
+int rijndaelEndOfCode();
+	
+#define KEYBITS 128
+
+#define KEYLENGTH(keybits) ((keybits)/8)
+#define RKLENGTH(keybits)  ((keybits)/8+28)
+#define NROUNDS(keybits)   ((keybits)/32+6)
+
+#endif
+
