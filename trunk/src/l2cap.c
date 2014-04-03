f4f82e4f9157ff2a6398e8b71709e9b53ae63d22
matthias.ringwald@gmail.com <matthias.ringwald@gmail.com@1a0a8af8-31b5-11de-8e0c-53a27eea117e>
2014-04-03 15:16:45 +0000

    reserve l2cap packet buffer before using it

--- trunk/src/l2cap.c
+++ trunk/src/l2cap.c
@@ -235,6 +235,13 @@ int  l2cap_can_send_packet_now(uint16_t local_cid){
     return hci_can_send_packet_now(HCI_ACL_DATA_PACKET);
 }
 
+int l2cap_can_send_packet_now_using_buffer(uint16_t local_cid){
+    l2cap_channel_t *channel = l2cap_get_channel_for_local_cid(local_cid);
+    if (!channel) return 0;
+    if (!channel->packets_granted) return 0;
+    return hci_can_send_packet_now_using_packet_buffer(HCI_ACL_DATA_PACKET);
+}
+
 uint16_t l2cap_get_remote_mtu_for_local_cid(uint16_t local_cid){
     l2cap_channel_t * channel = l2cap_get_channel_for_local_cid(local_cid);
     if (channel) {
