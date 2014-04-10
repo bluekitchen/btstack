e5ac1806249ce6598d735e103db2347db89b5d31
mila@ringwald.ch <mila@ringwald.ch@1a0a8af8-31b5-11de-8e0c-53a27eea117e>
2014-04-10 15:18:55 +0000

    ble client: test advertisement skeleton

--- trunk/test/gatt_client/gatt_client.c
+++ trunk/test/gatt_client/gatt_client.c
@@ -18,21 +18,40 @@
 
 #include "btstack_memory.h"
 #include "hci.h"
+#include "ble_client.h"
 
-// #include "hci_dump.h"
-// #include "l2cap.h"
-// #include "sdp_parser.h"
 
-TEST_GROUP(ADParser){
+static void handle_scan_and_connect(le_central_event_t * event){
+    if (event->type != GATT_ADVERTISEMENT) return;
+    printf("test client - SCAN ACTIVE\n");
+    ad_event_t * e = (ad_event_t*) event;
+    printf("    *** adv. event *** evt-type %u, addr-type %u, addr %s, rssi %u, length adv %u, data: ", e->event_type, 
+                            e->address_type, bd_addr_to_str(e->address), e->rssi, e->length); 
+    // hexdump2( e->data, e->length);   
+
+    // dump_ad_event(ad_event);
+}
+
+static void handle_le_central_event(le_central_event_t * event){
+	handle_scan_and_connect(event);
+}
+
+TEST_GROUP(GATTClient){
+	uint8_t advertisement_received;
+
+	void setup(){
+		advertisement_received = 0;
+		le_central_init();
+		le_central_register_handler(handle_le_central_event);
+	}
 };
 
 
-TEST(ADParser, TestDataParsing){
+TEST(GATTClient, TestScanning){
+	le_central_start_scan();
+	CHECK(advertisement_received);
 }
 
-TEST(ADParser, TestHasUUID){
-
-}
 
 int main (int argc, const char * argv[]){
     return CommandLineTestRunner::RunAllTests(argc, argv);
