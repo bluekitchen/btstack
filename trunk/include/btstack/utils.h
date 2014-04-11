83161f1c36c8639ede80e4e5d598a202c076ac11
matthias.ringwald@gmail.com <matthias.ringwald@gmail.com@1a0a8af8-31b5-11de-8e0c-53a27eea117e>
2014-04-11 10:25:05 +0000

    mark source of swap functions as const

--- trunk/include/btstack/utils.h
+++ trunk/include/btstack/utils.h
@@ -124,11 +124,11 @@ void bt_flip_addr(bd_addr_t dest, bd_addr_t src);
 void net_store_16(uint8_t *buffer, uint16_t pos, uint16_t value);
 void net_store_32(uint8_t *buffer, uint16_t pos, uint32_t value);
 
-void swapX(uint8_t *src, uint8_t *dst, int len);
-void swap24(uint8_t  src[3],  uint8_t dst[3]);
-void swap56(uint8_t  src[7],  uint8_t dst[7]);
-void swap64(uint8_t  src[8],  uint8_t dst[8]);
-void swap128(uint8_t src[16], uint8_t dst[16]);
+void swapX(const uint8_t *src, uint8_t *dst, int len);
+void swap24(const uint8_t  src[3],  uint8_t dst[3]);
+void swap56(const uint8_t  src[7],  uint8_t dst[7]);
+void swap64(const uint8_t  src[8],  uint8_t dst[8]);
+void swap128(const uint8_t src[16], uint8_t dst[16]);
 
 void hexdump(void *data, int size);
 void printUUID128(uint8_t *uuid);
