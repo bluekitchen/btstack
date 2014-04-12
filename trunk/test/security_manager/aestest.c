c6fa85342c110ca322162741fe745c05d4e7543c
matthias.ringwald@gmail.com <matthias.ringwald@gmail.com@1a0a8af8-31b5-11de-8e0c-53a27eea117e>
2014-04-12 21:21:46 +0000

    debug pairing test case

---
+++ trunk/test/security_manager/aestest.c
@@ -0,0 +1,30 @@
+
+#include "rijndael.h"
+#include <stdio.h>
+#include <strings.h>
+
+void aes128_calc_cyphertext(uint8_t key[16], uint8_t plaintext[16], uint8_t cyphertext[16]){
+	uint32_t rk[RKLENGTH(KEYBITS)];
+	int nrounds = rijndaelSetupEncrypt(rk, &key[0], KEYBITS);
+	rijndaelEncrypt(rk, nrounds, plaintext, cyphertext);
+}
+
+
+static void hexdump2(void *data, int size){
+    if (size <= 0) return;
+    int i;
+    for (i=0; i<size;i++){
+        printf("%02X ", ((uint8_t *)data)[i]);
+    }
+    printf("\n");
+}
+
+int main(){
+	uint8_t key[16];
+	uint8_t plaintext[16];
+	bzero(key, 16);
+	bzero(plaintext, 16);
+	uint8_t cyphertext[16];
+	aes128_calc_cyphertext(key, plaintext, cyphertext);
+	hexdump2(cyphertext, 16);
+}
--- trunk/test/security_manager/rijndael.h
+++ trunk/test/security_manager/rijndael.h
@@ -6,26 +6,18 @@
 #ifndef H__RIJNDAEL
 #define H__RIJNDAEL
 
-#if defined __cplusplus
-extern "C" {
-#endif
-
 #include <stdint.h>
 
-int  rijndaelSetupEncrypt(uint32_t *rk, const unsigned char *key, int keybits);
-int  rijndaelSetupDecrypt(uint32_t *rk, const unsigned char *key, int keybits);
-void rijndaelEncrypt(const uint32_t *rk, int nrounds, const unsigned char plaintext[16], unsigned char ciphertext[16]);
-void rijndaelDecrypt(const uint32_t *rk, int nrounds, const unsigned char ciphertext[16], unsigned char plaintext[16]);
+int rijndaelSetupEncrypt(uint32_t *rk, const uint8_t *key, int keybits);
+int rijndaelSetupDecrypt(uint32_t *rk, const uint8_t *key, int keybits);
+void rijndaelEncrypt(const uint32_t *rk, int nrounds, const uint8_t plaintext[16], uint8_t ciphertext[16]);
+void rijndaelDecrypt(const uint32_t *rk, int nrounds, const uint8_t ciphertext[16], uint8_t plaintext[16]);
 	
 #define KEYBITS 128
 
 #define KEYLENGTH(keybits) ((keybits)/8)
 #define RKLENGTH(keybits)  ((keybits)/8+28)
 #define NROUNDS(keybits)   ((keybits)/32+6)
-
-#if defined __cplusplus
-}
-#endif
 
 #endif
 
