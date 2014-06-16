c242c5e3ca120478e71b8c32894531a0ed521f95
matthias.ringwald <matthias.ringwald@1a0a8af8-31b5-11de-8e0c-53a27eea117e>
2014-06-16 20:55:54 +0000

    added linked list iterator implementation and unit tests

--- trunk/include/btstack/linked_list.h
+++ trunk/include/btstack/linked_list.h
@@ -49,13 +49,28 @@ typedef struct linked_item {
 
 typedef linked_item_t * linked_list_t;
 
-void linked_item_set_user(linked_item_t *item, void *user_data);        // <-- set user data
-void * linked_item_get_user(linked_item_t *item);                       // <-- get user data
-int  linked_list_empty(linked_list_t * list);
-void linked_list_add(linked_list_t * list, linked_item_t *item);        // <-- add item to list as first element
-void linked_list_add_tail(linked_list_t * list, linked_item_t *item);   // <-- add item to list as last element
-int  linked_list_remove(linked_list_t * list, linked_item_t *item);     // <-- remove item from list
-linked_item_t * linked_list_get_last_item(linked_list_t * list);        // <-- find the last item in the list
+typedef struct {
+	int advance_on_next;
+    linked_item_t * prev;	// points to the item before the current one
+    linked_item_t * curr;	// points to the current item (to detect item removal)
+} linked_list_iterator_t;
+
+
+void            linked_item_set_user(linked_item_t *item, void *user_data);        // <-- set user data
+void *          linked_item_get_user(linked_item_t *item);                         // <-- get user data
+int             linked_list_empty(linked_list_t * list);
+void            linked_list_add(linked_list_t * list, linked_item_t *item);        // <-- add item to list as first element
+void            linked_list_add_tail(linked_list_t * list, linked_item_t *item);   // <-- add item to list as last element
+int             linked_list_remove(linked_list_t * list, linked_item_t *item);     // <-- remove item from list
+linked_item_t * linked_list_get_last_item(linked_list_t * list);                   // <-- find the last item in the list
+
+//
+// iterator for linked lists. alloes to remove current element. also robust against removal of current element by linked_list_remove
+//
+void            linked_list_iterator_init(linked_list_iterator_t * it, linked_list_t * list);
+int             linked_list_iterator_has_next(linked_list_iterator_t * it);
+linked_item_t * linked_list_iterator_next(linked_list_iterator_t * it);
+void            linked_list_iterator_remove(linked_list_iterator_t * it);
 
 void test_linked_list(void);
 
