08660634ba0bcc2ada01cc92ffdc696f908616c4
matthias.ringwald@gmail.com <matthias.ringwald@gmail.com@1a0a8af8-31b5-11de-8e0c-53a27eea117e>
2014-08-11 22:07:10 +0000

    remove unused function

--- trunk/ble/att_server.c
+++ trunk/ble/att_server.c
@@ -86,11 +86,6 @@ static timer_source_t att_handle_value_indication_timer;
 
 static btstack_packet_handler_t att_client_packet_handler = NULL;
 
-// should that go into l2cap[_le].c? Should it actually use the handle?
-static uint16_t l2cap_max_mtu_for_handle(uint16_t handle){
-    return l2cap_max_mtu();
-}
-
 static void att_handle_value_indication_notify_client(uint8_t status, uint16_t client_handle, uint16_t attribute_handle){
     uint8_t event[7];
     int pos = 0;
