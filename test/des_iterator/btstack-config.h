7ebf954ce989664d562264b51208f06c9c6d33ae
mila@ringwald.ch <mila@ringwald.ch@1a0a8af8-31b5-11de-8e0c-53a27eea117e>
2014-08-29 12:01:39 +0000

    started des iterator

---
+++ test/des_iterator/btstack-config.h
@@ -0,0 +1,18 @@
+// config.h created by configure for BTstack  Sun Sep 25 16:06:59 CEST 2011
+#ifndef __CONFIG_H
+#define __CONFIG_H
+#define HAVE_TRANSPORT_H4
+#define UART_DEVICE "/dev/tty.usbserial-A600eIDu"
+#define UART_SPEED 115200
+#define USE_POSIX_RUN_LOOP
+#define HAVE_TIME
+#define HAVE_MALLOC
+#define HAVE_BZERO
+#define ENABLE_LOG_INFO 
+#define ENABLE_LOG_ERROR
+#define HAVE_SDP_EXTRA_QUERIES
+#define SDP_DES_DUMP
+#define HCI_ACL_PAYLOAD_SIZE 1021
+// #define HCI_ACL_PAYLOAD_SIZE 52
+
+#endif // __CONFIG_H
