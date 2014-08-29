85742615a805d8b3a2057e9fe3b5a7d36c08e272
mila@ringwald.ch <mila@ringwald.ch@1a0a8af8-31b5-11de-8e0c-53a27eea117e>
2014-08-29 15:20:27 +0000

    des iterator done

--- test/des_iterator/des_iterator.c
+++ test/des_iterator/des_iterator.c
@@ -1,5 +1,5 @@
 #include "des_iterator.h"
- 
+
 int des_iterator_init(des_iterator_t * it, uint8_t * element){
 	de_type_t type = de_get_element_type(element);
     if (type != DE_DES) return 0;
@@ -7,6 +7,7 @@ int des_iterator_init(des_iterator_t * it, uint8_t * element){
 	it->element = element;
     it->pos = de_get_header_size(element);
     it->length = de_get_len(element);
+    // printf("des_iterator_init current pos %d, total len %d\n", it->pos, it->length);
     return 1;
 }
 
@@ -20,10 +21,37 @@ uint16_t des_iterator_get_size (des_iterator_t * it){
 	return length - header_size;
 }
 
-int  des_iterator_has_more(des_iterator_t * it){
-	return 0;
+int des_iterator_has_more(des_iterator_t * it){
+	return it->pos < it->length;
 }
 
-uint8_t * des_iterator_get_value(des_iterator_t * it){
-	return NULL;
+uint8_t * des_iterator_get_element(des_iterator_t * it){
+	if (!des_iterator_has_more(it)) return NULL;
+	return &it->element[it->pos];
 }
+
+void des_iterator_next(des_iterator_t * it){
+	int element_len = de_get_len(&it->element[it->pos]);
+	// printf("des_iterator_next element size %d, current pos %d, total len %d\n", element_len, it->pos, it->length);
+	it->pos += element_len;
+}
+
+
+// move to sdp_util.c
+
+// @returns OK, if UINT16 value was read
+int de_element_get_uint16(uint8_t * element, uint16_t * value){
+	if (de_get_size_type(element) != DE_SIZE_16) return 0;
+	*value = READ_NET_16(element, de_get_header_size(element));
+    return 1;
+}
+
+// @returns 0 if no UUID16 was present, and UUID otherwise
+uint16_t de_element_get_uuid16(uint8_t * element){
+	if (de_get_element_type(element) != DE_UUID) return 0;
+    uint16_t value = 0;
+    de_element_get_uint16(element, &value);
+    return value;
+}
+
+
--- test/des_iterator/des_iterator.h
+++ test/des_iterator/des_iterator.h
@@ -26,7 +26,13 @@ int des_iterator_init(des_iterator_t * it, uint8_t * element);
 int  des_iterator_has_more(des_iterator_t * it);
 de_type_t des_iterator_get_type (des_iterator_t * it);
 uint16_t des_iterator_get_size (des_iterator_t * it);
-uint8_t * des_iterator_get_value(des_iterator_t * it);
+uint8_t * des_iterator_get_element(des_iterator_t * it);
+void des_iterator_next(des_iterator_t * it);
+
+
+// move to sdp util
+int de_element_get_uint16(uint8_t * element, uint16_t * value);
+uint16_t de_element_get_uuid16(uint8_t * element);
 
 #if defined __cplusplus
 }
