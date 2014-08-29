795d1c99928e4f041b980b577d8c3ee03f98a210
mila@ringwald.ch <mila@ringwald.ch@1a0a8af8-31b5-11de-8e0c-53a27eea117e>
2014-08-29 21:22:55 +0000

    added new test

--- trunk/test/des_iterator/des_iterator_test.c
+++ trunk/test/des_iterator/des_iterator_test.c
@@ -16,7 +16,7 @@
 #include "CppUTest/CommandLineTestRunner.h"
 
 
-static uint8_t  prot_des_list_des[] = { 
+static uint8_t  des_list[] = { 
     0x35, 0x1E, 0x35, 0x06, 0x19, 0x01, 0x00, 0x09, 0x00, 0x0F, 0x35, 0x14, 0x19, 0x00, 0x0F, 0x09, 
     0x01, 0x00, 0x35, 0x0C, 0x09, 0x08, 0x00, 0x09, 0x08, 0x06, 0x09, 0x86, 0xDD, 0x09, 0x88, 0x0B
 };
@@ -29,6 +29,7 @@ static uint16_t expected_values[] = {
 
 TEST_GROUP(DESParser){
     int value_index;
+    des_iterator_t des_list_it;
 
     void setup(){
         value_index = 0;
@@ -46,48 +47,67 @@ TEST_GROUP(DESParser){
         CHECK_EQUAL(expected_values[value_index], value);
         value_index++;
     }
+
+    void iter(des_iterator_t * des_list_it){
+        CHECK_EQUAL(des_iterator_get_type(des_list_it), DE_DES);
+        des_iterator_t prot_it;
+        des_iterator_t packet_it;
+        uint8_t * des_element;
+        uint8_t * packet_des;
+        des_element = des_iterator_get_element(des_list_it);
+        for (des_iterator_init(&prot_it, des_element) ; des_iterator_has_more(&prot_it) ; des_iterator_next(&prot_it) ){
+            if (des_iterator_get_type(&prot_it) != DE_DES){
+                CHECK_EQUAL_UINT16(&prot_it);
+                continue;
+            }
+            packet_des = des_iterator_get_element(&prot_it);
+            for (des_iterator_init(&packet_it, packet_des) ; des_iterator_has_more(&packet_it) ; des_iterator_next(&packet_it)){
+                CHECK_EQUAL_UINT16(&packet_it);
+            }
+        }
+    }
 };
 
 // DES { DES{0x0100, 0xf}, DES{0x000f, 0x0100, DES{0x800, 0x806, 0x86dd, 0x880b}}}
 TEST(DESParser, DESIterator){
-    des_iterator_t des_protocol_list_it;
-    des_iterator_t prot_it;
-    des_iterator_t packet_it;
-    int iterator_initialized;
-    uint8_t * prot_des;
-    uint8_t * packet_des;
-    // printf("\n *** protocol list\n");
-    iterator_initialized = des_iterator_init(&des_protocol_list_it, prot_des_list_des);
-    CHECK_EQUAL(iterator_initialized, 1);
-    CHECK_EQUAL(des_iterator_get_type(&des_protocol_list_it), DE_DES);
-    
-    // printf("\n *** l2cap uuid, psm\n");
-    prot_des = des_iterator_get_element(&des_protocol_list_it);
-    for (des_iterator_init(&prot_it, prot_des) ; des_iterator_has_more(&prot_it) ; des_iterator_next(&prot_it) ){
-        CHECK_EQUAL_UINT16(&prot_it);
+    for (des_iterator_init(&des_list_it, des_list); des_iterator_has_more(&des_list_it); des_iterator_next(&des_list_it)){
+        iter(&des_list_it);
     }
-    
-    des_iterator_next(&des_protocol_list_it);
-    CHECK_EQUAL(des_iterator_get_type(&des_protocol_list_it), DE_DES);
-
-    // printf("\n *** bnep uuid, value\n");
-    prot_des = des_iterator_get_element(&des_protocol_list_it);
-
-    for (des_iterator_init(&prot_it, prot_des) ; des_iterator_has_more(&prot_it) ; des_iterator_next(&prot_it)){
-        if (des_iterator_get_type(&prot_it) != DE_DES){
-            CHECK_EQUAL_UINT16(&prot_it);
-            continue;
-        }
-
-        packet_des = des_iterator_get_element(&prot_it);
-        for (des_iterator_init(&packet_it, packet_des) ; des_iterator_has_more(&packet_it) ; des_iterator_next(&packet_it)){
-            CHECK_EQUAL_UINT16(&packet_it);
-        }
-    }
-    des_iterator_next(&des_protocol_list_it);
+    CHECK_EQUAL(des_iterator_has_more(&des_list_it), 0);
     CHECK_EQUAL(value_index, 8);
 }
 
+TEST(DESParser, DESIterator2){
+    uint16_t l2cap_psm = 0;
+    uint16_t bnep_version = 0;
+    for (des_iterator_init(&des_list_it, des_list); des_iterator_has_more(&des_list_it); des_iterator_next(&des_list_it)){
+        CHECK_EQUAL(des_iterator_get_type(&des_list_it), DE_DES);
+        des_iterator_t prot_it;
+        uint8_t * des_element = des_iterator_get_element(&des_list_it);
+        des_iterator_init(&prot_it, des_element);
+        uint8_t * element = des_iterator_get_element(&prot_it);
+        CHECK_EQUAL(de_get_element_type(element), DE_UUID);
+        uint16_t uuid = de_element_get_uuid16(element);
+        switch (uuid){
+            case 0x100:
+                CHECK_EQUAL(des_iterator_has_more(&prot_it), 1);
+                des_iterator_next(&prot_it);
+                de_element_get_uint16(des_iterator_get_element(&prot_it), &l2cap_psm);
+                break;
+            case 0x000f:
+                CHECK_EQUAL(des_iterator_has_more(&prot_it), 1);
+                des_iterator_next(&prot_it);
+                de_element_get_uint16(des_iterator_get_element(&prot_it), &bnep_version);
+                break;
+            default:
+                break;
+        }
+    }
+    printf("l2cap_psm 0x%04x, bnep_version 0x%04x\n", l2cap_psm, bnep_version);
+    CHECK_EQUAL(l2cap_psm, expected_values[1]);
+    CHECK_EQUAL(bnep_version, expected_values[3]);
+    CHECK_EQUAL(des_iterator_has_more(&des_list_it), 0);
+}
 
 int main (int argc, const char * argv[]){
     return CommandLineTestRunner::RunAllTests(argc, argv);
