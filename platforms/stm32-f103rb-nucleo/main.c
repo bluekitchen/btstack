d901374d9acb401abe6ba95613590dcedc06a6ca
matthias.ringwald@gmail.com <matthias.ringwald@gmail.com@1a0a8af8-31b5-11de-8e0c-53a27eea117e>
2014-09-29 21:08:37 +0000

    add in alpha Bluetooth code

--- platforms/stm32-f103rb-nucleo/main.c
+++ platforms/stm32-f103rb-nucleo/main.c
@@ -8,6 +8,8 @@
 #include <errno.h>
 #include <unistd.h>
 
+#include <btstack/run_loop.h>
+
 // Configuration
 // LED2 on PA5
 // Debug: USART2, TX on PA2
@@ -18,6 +20,8 @@
 
 // btstack code starts there
 void btstack_main(void);
+
+static void bluetooth_power_cycle(void);
 
 // hal_tick.h inmplementation
 #include <btstack/hal_tick.h>
@@ -72,6 +76,59 @@ void hal_led_toggle(void){
 	gpio_toggle(GPIOA, GPIO_LED2);
 }
 
+// hal_uart_dma.c implementation
+#include <btstack/hal_uart_dma.h>
+
+// handlers
+static void (*rx_done_handler)(void) = dummy_handler;
+static void (*tx_done_handler)(void) = dummy_handler;
+static void (*cts_irq_handler)(void) = dummy_handler;
+
+void hal_uart_dma_init(void){
+	bluetooth_power_cycle();
+}
+void hal_uart_dma_set_block_received( void (*the_block_handler)(void)){
+    rx_done_handler = the_block_handler;
+}
+
+void hal_uart_dma_set_block_sent( void (*the_block_handler)(void)){
+    tx_done_handler = the_block_handler;
+}
+
+void hal_uart_dma_set_csr_irq_handler( void (*the_irq_handler)(void)){
+	// TODO: enable/disable interrupt
+    cts_irq_handler = the_irq_handler;
+}
+
+int  hal_uart_dma_set_baud(uint32_t baud){
+	usart_disable(USART3);
+	usart_set_baudrate(USART3, baud);
+	usart_enable(USART3);
+}
+
+void hal_uart_dma_send_block(const uint8_t *buffer, uint16_t length){
+	// TODO: use DMA for this
+	printf("TX: ");
+	while (length){
+		printf("%02x ", *buffer);
+		usart_send_blocking(USART3, *buffer);
+		buffer++;
+		length--;
+	}
+	printf("\n");
+	(*tx_done_handler)();
+}
+void hal_uart_dma_receive_block(uint8_t *buffer, uint16_t len){
+	// TODO: use DMA for this
+	(void) buffer;
+	(void) len;
+}
+
+void hal_uart_dma_set_sleep(uint8_t sleep){
+	// TODO:
+	(void) sleep;
+}
+
 /**
  * Use USART_CONSOLE as a console.
  * This is a syscall for newlib
@@ -81,8 +138,7 @@ void hal_led_toggle(void){
  * @return
  */
 int _write(int file, char *ptr, int len);
-int _write(int file, char *ptr, int len)
-{
+int _write(int file, char *ptr, int len){
 	int i;
 
 	if (file == STDOUT_FILENO || file == STDERR_FILENO) {
@@ -98,8 +154,7 @@ int _write(int file, char *ptr, int len)
 	return -1;
 }
 
-static void clock_setup(void)
-{
+static void clock_setup(void){
 	/* Enable clocks for GPIO port A (for GPIO_USART1_TX) and USART1 + USART2. */
 	/* needs to be done before initializing other peripherals */
 	rcc_periph_clock_enable(RCC_GPIOA);
@@ -108,15 +163,12 @@ static void clock_setup(void)
 	rcc_periph_clock_enable(RCC_USART3);
 }
 
-static void gpio_setup(void)
-{
+static void gpio_setup(void){
 	/* Set GPIO5 (in GPIO port A) to 'output push-pull'. [LED] */
 	gpio_set_mode(GPIOA, GPIO_MODE_OUTPUT_2_MHZ, GPIO_CNF_OUTPUT_PUSHPULL, GPIO_LED2);
 }
 
-void usart_setup(void);
-void usart_setup(void)
-{
+static void usart_setup(void){
 	/* Setup GPIO pin GPIO_USART2_TX/GPIO2 on GPIO port A for transmit. */
 	gpio_set_mode(GPIOA, GPIO_MODE_OUTPUT_50_MHZ, GPIO_CNF_OUTPUT_ALTFN_PUSHPULL, GPIO_USART2_TX);
 
@@ -132,12 +184,60 @@ void usart_setup(void)
 	usart_enable(USART2);
 }
 
+static void bluetooth_setup(void){
+	printf("\nBluetooth starting...\n");
+
+	// n_shutdown as output
+	gpio_set_mode(GPIOB, GPIO_MODE_OUTPUT_50_MHZ, GPIO_CNF_OUTPUT_PUSHPULL,GPIO_BT_N_SHUTDOWN);
+
+	// tx output
+	gpio_set_mode(GPIOB, GPIO_MODE_OUTPUT_50_MHZ, GPIO_CNF_OUTPUT_ALTFN_PUSHPULL, GPIO_USART3_TX);
+	// rts output
+	gpio_set_mode(GPIOB, GPIO_MODE_OUTPUT_50_MHZ, GPIO_CNF_OUTPUT_ALTFN_PUSHPULL, GPIO_USART3_RTS);
+	// rx input
+	gpio_set_mode(GPIOB, GPIO_MODE_INPUT, GPIO_CNF_INPUT_FLOAT, GPIO_USART3_RX);
+	// cts as input
+	gpio_set_mode(GPIOB, GPIO_MODE_INPUT, GPIO_CNF_INPUT_FLOAT, GPIO_USART3_CTS);
+
+	/* Setup UART parameters. */
+	usart_set_baudrate(USART3, 115200);
+	usart_set_databits(USART3, 8);
+	usart_set_stopbits(USART3, USART_STOPBITS_1);
+	usart_set_mode(USART3, USART_MODE_TX_RX);
+	usart_set_parity(USART3, USART_PARITY_NONE);
+	usart_set_flow_control(USART3, USART_FLOWCONTROL_RTS_CTS);
+
+	/* Finally enable the USART. */
+	usart_enable(USART3);
+}
+
+// reset Bluetooth using n_shutdown
+
+static void msleep(uint32_t delay)
+{
+	uint32_t wake = embedded_get_ticks() + delay;
+	while (wake > embedded_get_ticks());
+}
+
+static void bluetooth_power_cycle(void){
+	printf("n_shutdown low\n");
+	gpio_clear(GPIOA, GPIO_LED2);
+	gpio_clear(GPIOB, GPIO_BT_N_SHUTDOWN);
+	msleep(1000);
+	printf("n_shutdown high\n");
+	gpio_set(GPIOA, GPIO_LED2);
+	gpio_set(GPIOB, GPIO_BT_N_SHUTDOWN);
+	msleep(1000);
+	printf("n_shutdown toggling down\n");	
+}
+
 int main(void)
 {
 	clock_setup();
 	gpio_setup();
-	usart_setup();
 	hal_tick_init();
+	usart_setup();
+	bluetooth_setup();
 
 	// hand over to btstack embedded code 
 	btstack_main();
