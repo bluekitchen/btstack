1a1b52738c9f7b0a8783ce92172c754e17d01617
matthias.ringwald@gmail.com <matthias.ringwald@gmail.com@1a0a8af8-31b5-11de-8e0c-53a27eea117e>
2014-10-02 12:28:55 +0000

    revert accidental commit

--- example/libusb/gap_inquiry.c
+++ example/libusb/gap_inquiry.c
@@ -10,68 +10,168 @@
 #include <string.h>
 
 #include "btstack-config.h"
+
 #include <btstack/run_loop.h>
 #include <btstack/sdp_util.h>
-#include <btstack/utils.h>
-#include <btstack/utils.h>
 
 #include "debug.h"
 #include "btstack_memory.h"
 #include "hci.h"
 #include "hci_dump.h"
 
-#define OPCODE(ogf, ocf) (ocf | ogf << 10)
+#define MAX_DEVICES 10
+enum DEVICE_STATE { REMOTE_NAME_REQUEST, REMOTE_NAME_INQUIRED, REMOTE_NAME_FETCHED };
+struct device {
+    bd_addr_t  address;
+    uint16_t   clockOffset;
+    uint32_t   classOfDevice;
+    uint8_t    pageScanRepetitionMode;
+    uint8_t    rssi;
+    enum DEVICE_STATE  state; 
+};
 
-enum STATE {INIT, W4_1, W4_2, W4_3, DONE} ;
+#define INQUIRY_INTERVAL 5
+struct device devices[MAX_DEVICES];
+int deviceCount = 0;
+
+
+enum STATE {INIT, W4_INQUIRY_MODE_COMPLETE, ACTIVE} ;
 enum STATE state = INIT;
 
-static const hci_cmd_t hci_write_linkkey = {
-    OPCODE(OGF_CONTROLLER_BASEBAND, 0x11), "1BP"
-};
 
-static const hci_cmd_t hci_read_linkkeys = {
-OPCODE(OGF_CONTROLLER_BASEBAND, 0x0D), "B1"
-};
+int getDeviceIndexForAddress( bd_addr_t addr){
+    int j;
+    for (j=0; j< deviceCount; j++){
+        if (BD_ADDR_CMP(addr, devices[j].address) == 0){
+            return j;
+        }
+    }
+    return -1;
+}
+
+void start_scan(void){
+    printf("Starting inquiry scan..\n");
+    hci_send_cmd(&hci_inquiry, HCI_INQUIRY_LAP, INQUIRY_INTERVAL, 0);
+}
+
+int has_more_remote_name_requests(void){
+    int i;
+    for (i=0;i<deviceCount;i++) {
+        if (devices[i].state == REMOTE_NAME_REQUEST) return 1;
+    }
+    return 0;
+}
+
+void do_next_remote_name_request(void){
+    int i;
+    for (i=0;i<deviceCount;i++) {
+        // remote name request
+        if (devices[i].state == REMOTE_NAME_REQUEST){
+            devices[i].state = REMOTE_NAME_INQUIRED;
+            printf("Get remote name of %s...\n", bd_addr_to_str(devices[i].address));
+            hci_send_cmd(&hci_remote_name_request, devices[i].address,
+                        devices[i].pageScanRepetitionMode, 0, devices[i].clockOffset | 0x8000);
+            return;
+        }
+    }
+}
+
+static void continue_remote_names(){
+    if (has_more_remote_name_requests()){
+        do_next_remote_name_request();
+        return;
+    } 
+    start_scan();
+}
 
 static void packet_handler (uint8_t packet_type, uint8_t *packet, uint16_t size){
-    bd_addr_t a = {1,2,3,4,5,6};
-    uint8_t linkkey[16];
-                
+    bd_addr_t addr;
+    int i;
+    int numResponses;
+    
     // printf("packet_handler: pt: 0x%02x, packet[0]: 0x%02x\n", packet_type, packet[0]);
     if (packet_type != HCI_EVENT_PACKET) return;
+
+    uint8_t event = packet[0];
 
     switch(state){
 
         case INIT: 
             if (packet[2] == HCI_STATE_WORKING) {
-                // hci_send_cmd(&hci_write_inquiry_mode, 0x01); // with RSSI
-                hci_send_cmd(&hci_write_linkkey, 1, a, linkkey);
-                state = W4_1;
+                hci_send_cmd(&hci_write_inquiry_mode, 0x01); // with RSSI
+                state = W4_INQUIRY_MODE_COMPLETE;
             }
             break;
 
-        case W4_1:
-            if ( COMMAND_COMPLETE_EVENT(packet, hci_write_linkkey) ) {
-                a[0] = 2;
-                hci_send_cmd(&hci_write_linkkey, 1, a, linkkey);
-                state = W4_2;
+        case W4_INQUIRY_MODE_COMPLETE:
+            if ( COMMAND_COMPLETE_EVENT(packet, hci_write_inquiry_mode) ) {
+                start_scan();
+                state = ACTIVE;
             }
             break;
-        case W4_2:
-            if ( COMMAND_COMPLETE_EVENT(packet, hci_write_linkkey) ) {
-                a[0] = 4;
-                hci_send_cmd(&hci_write_linkkey, 1, a, linkkey);
-                state = W4_3;
+            
+        case ACTIVE:
+            switch(event){
+                case HCI_EVENT_INQUIRY_RESULT:
+                case HCI_EVENT_INQUIRY_RESULT_WITH_RSSI:
+                    numResponses = packet[2];
+                    for (i=0; i<numResponses && deviceCount < MAX_DEVICES;i++){
+                        bt_flip_addr(addr, &packet[3+i*6]);
+                        int index = getDeviceIndexForAddress(addr);
+                        if (index >= 0) continue;   // already in our list
+
+                        memcpy(devices[deviceCount].address, addr, 6);
+                        devices[deviceCount].pageScanRepetitionMode =   packet [3 + numResponses*(6)         + i*1];
+                        if (event == HCI_EVENT_INQUIRY_RESULT){
+                            devices[deviceCount].classOfDevice = READ_BT_24(packet, 3 + numResponses*(6+1+1+1)   + i*3);
+                            devices[deviceCount].clockOffset =   READ_BT_16(packet, 3 + numResponses*(6+1+1+1+3) + i*2) & 0x7fff;
+                            devices[deviceCount].rssi  = 0;
+                        } else {
+                            devices[deviceCount].classOfDevice = READ_BT_24(packet, 3 + numResponses*(6+1+1)     + i*3);
+                            devices[deviceCount].clockOffset =   READ_BT_16(packet, 3 + numResponses*(6+1+1+3)   + i*2) & 0x7fff;
+                            devices[deviceCount].rssi  =                    packet [3 + numResponses*(6+1+1+3+2) + i*1];
+                        }
+                        devices[deviceCount].state = REMOTE_NAME_REQUEST;
+                        printf("Device found: %s with COD: 0x%06x, pageScan %d, clock offset 0x%04x, rssi 0x%02x\n", bd_addr_to_str(addr),
+                                devices[deviceCount].classOfDevice, devices[deviceCount].pageScanRepetitionMode,
+                                devices[deviceCount].clockOffset, devices[deviceCount].rssi);
+                        deviceCount++;
+                    }
+                    break;
+                    
+                case HCI_EVENT_INQUIRY_COMPLETE:
+                    for (i=0;i<deviceCount;i++) {
+                        // retry remote name request
+                        if (devices[i].state == REMOTE_NAME_INQUIRED)
+                            devices[i].state = REMOTE_NAME_REQUEST;
+                    }
+                    continue_remote_names();
+                    break;
+
+                case BTSTACK_EVENT_REMOTE_NAME_CACHED:
+                    bt_flip_addr(addr, &packet[3]);
+                    printf("Cached remote name for %s: '%s'\n", bd_addr_to_str(addr), &packet[9]);
+                    break;
+
+                case HCI_EVENT_REMOTE_NAME_REQUEST_COMPLETE:
+                    bt_flip_addr(addr, &packet[3]);
+                    int index = getDeviceIndexForAddress(addr);
+                    if (index >= 0) {
+                        if (packet[2] == 0) {
+                            printf("Name: '%s'\n", &packet[9]);
+                            devices[index].state = REMOTE_NAME_FETCHED;
+                        } else {
+                            printf("Failed to get name: page timeout\n");
+                        }
+                    }
+                    continue_remote_names();
+                    break;
+
+                default:
+                    break;
             }
             break;
-        case W4_3:
-            if ( COMMAND_COMPLETE_EVENT(packet, hci_write_linkkey) ) {
-                hci_send_cmd(&hci_read_linkkeys, a, 1);
-                state = DONE;
-            }
-            break;
-        case DONE:
-            break;
+            
         default:
             break;
     }
