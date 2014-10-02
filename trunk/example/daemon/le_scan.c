e2d78d63831814cc23d34b0b55f356be1265aa30
mila@ringwald.ch <mila@ringwald.ch@1a0a8af8-31b5-11de-8e0c-53a27eea117e>
2014-10-02 20:22:04 +0000

    added le_scan example

---
+++ trunk/example/daemon/le_scan.c
@@ -0,0 +1,91 @@
+/*
+ * Copyright (C) 2009 by Matthias Ringwald
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the copyright holders nor the names of
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY MATTHIAS RINGWALD AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL MATTHIAS
+ * RINGWALD OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
+ * THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ */
+
+/*
+ *  le_scan.c
+ */
+
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <btstack/btstack.h>
+
+static void packet_handler(uint8_t packet_type, uint16_t channel, uint8_t *packet, uint16_t size){
+	
+	// printf("- packet_handler: pt: 0x%02x, packet[0]: 0x%02x\n", packet_type, packet[0]);
+	if (packet_type != HCI_EVENT_PACKET) return;
+
+	uint8_t event = packet[0];
+
+	switch (event){
+		case BTSTACK_EVENT_STATE:
+			// bt stack activated, get started
+			printf("- btstack state %u\n", packet[2]);
+            if (packet[2] == HCI_STATE_WORKING) {
+            	bt_send_cmd(&gap_le_set_scan_parameters, 1, 0x30, 0x30);
+	            bt_send_cmd(&gap_le_scan_start);
+			}
+			break;
+		case HCI_EVENT_LE_META:
+            switch (packet[2]) {
+                case HCI_SUBEVENT_LE_ADVERTISING_REPORT: 
+                	printf("\n- ADV: ");
+                	hexdumpf(packet, size);
+                    break;
+                }
+                break;
+		default:
+			break;
+	}
+}
+	
+int main (int argc, const char * argv[]){
+	printf("le_scan started\n");
+	printf("- connecting to BTstack Daemon\n");
+	// start stack
+	run_loop_init(RUN_LOOP_POSIX);
+	int err = bt_open();
+	if (err) {
+		printf("-> Failed to open connection to BTstack Daemon\n");
+		return err;
+	}
+	
+	printf("- connected\n");
+	printf("- send power on\n");
+
+	bt_register_packet_handler(packet_handler);
+	bt_send_cmd(&btstack_set_power_mode, HCI_POWER_ON );
+	run_loop_execute();
+	bt_close();
+	return 0;
+}
