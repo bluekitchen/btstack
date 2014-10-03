cd0110e455c3b4c61d1dff82d11597ba3857a789
matthias.ringwald@gmail.com <matthias.ringwald@gmail.com@1a0a8af8-31b5-11de-8e0c-53a27eea117e>
2014-10-03 10:36:43 +0000

    fix generated GAP_LE_ADVERTISING_REPORT

--- trunk/src/hci.c
+++ trunk/src/hci.c
@@ -628,21 +628,15 @@ void le_handle_advertisement_report(uint8_t *packet, int size){
     offset += 1;
 
     int i;
-    int total_data_length = 0;
-
-    for (i=0; i<num_reports;i++){
-        total_data_length += packet[4+num_reports*8+i];
-    }
-
     log_info("HCI: handle adv report with num reports: %d", num_reports);
     for (i=0; i<num_reports;i++){
-        uint8_t data_length = packet[offset + 9];
+        uint8_t data_length = packet[offset + 8];
         uint8_t event_size = 10 + data_length;
         uint8_t event[2 + event_size ];
         int pos = 0;
         event[pos++] = GAP_LE_ADVERTISING_REPORT;
         event[pos++] = event_size;
-        memcpy(&event[pos], &packet[offset], 1+1+6); // bt address
+        memcpy(&event[pos], &packet[offset], 1+1+6); // event type + address type + address
         offset += 8;
         pos += 8;
         event[pos++] = packet[offset + 1 + data_length]; // rssi
