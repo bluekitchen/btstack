1d796bc40333b9ca411f1a99714a7af98dd75756
matthias.ringwald@gmail.com <matthias.ringwald@gmail.com@1a0a8af8-31b5-11de-8e0c-53a27eea117e>
2014-11-14 16:23:33 +0000

    created libusb-main and started splitting code

---
+++ trunk/example/libusb/main.c
@@ -0,0 +1,42 @@
+// *****************************************************************************
+//
+// minimal setup for HCI code
+//
+// *****************************************************************************
+
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "btstack-config.h"
+
+#include <btstack/run_loop.h>
+
+#include "debug.h"
+#include "btstack_memory.h"
+#include "hci.h"
+#include "hci_dump.h"
+
+void btstack_main();
+
+int main(void){
+	/// GET STARTED with BTstack ///
+	btstack_memory_init();
+    run_loop_init(RUN_LOOP_POSIX);
+	    
+    // use logger: format HCI_DUMP_PACKETLOGGER, HCI_DUMP_BLUEZ or HCI_DUMP_STDOUT
+    hci_dump_open("/tmp/hci_dump.pklg", HCI_DUMP_PACKETLOGGER);
+
+    // init HCI
+	hci_transport_t    * transport = hci_transport_usb_instance();
+    hci_uart_config_t * config = NULL;
+	bt_control_t       * control   = NULL;
+    remote_device_db_t * remote_db = (remote_device_db_t *) &remote_device_db_memory;
+        
+	hci_init(transport, config, control, remote_db);
+
+    btstack_main();
+
+    return 0;
+}
