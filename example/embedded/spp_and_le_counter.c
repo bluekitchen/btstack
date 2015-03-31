bluekitchen/btstack

commit c13e0a893ed9deeb6c04732580794c54b8a89d3c

removed run_loop_execute from embedded examples, call after btstack_main in main.c

example/embedded/spp_and_le_counter.c
@@ -298,11 +298,7 @@ int btstack_main(void)
 
     // turn on!
 	hci_power_control(HCI_POWER_ON);
-	
-    // go!
-    run_loop_execute();	
-    
-    // happy compiler!
+	    
     return 0;
 }
 

example/embedded/spp_counter.c
@@ -208,10 +208,6 @@ int btstack_main(int argc, const char * argv[]){
     // turn on!
 	hci_power_control(HCI_POWER_ON);
 	
-    // go!
-    run_loop_execute();	
-    
-    // happy compiler!
     return 0;
 }
 

example/embedded/spp_flowcontrol.c
@@ -188,10 +188,6 @@ int btstack_main(int argc, const char * argv[]){
     // turn on!
     hci_power_control(HCI_POWER_ON);
 
-    // go!
-    run_loop_execute(); 
-    
-    // happy compiler!
     return 0;
 }
 

docs/manual/btstack_gettingstarted.tex
@@ -1045,7 +1045,6 @@ int main(void){
     // go!
     run_loop_execute(); 
     
-    // happy compiler!
     return 0;
 }
 \end{lstlisting}

example/embedded/ancs_client.c
@@ -205,9 +205,5 @@ int btstack_main(int argc, const char * argv[]){
     // turn on!
     hci_power_control(HCI_POWER_ON);
     
-    // go!
-    run_loop_execute(); 
-    
-    // happy compiler!
     return 0;
 }

example/embedded/ble_central_test.c
@@ -458,9 +458,5 @@ int btstack_main(int argc, const char * argv[]){
     // turn on!
     hci_power_control(HCI_POWER_ON);
     
-    // go!
-    run_loop_execute(); 
-    
-    // happy compiler!
     return 0;
 }

example/embedded/ble_client.c
@@ -827,10 +827,7 @@ int btstack_main(int argc, const char * argv[]){
 
     // turn on!
     hci_power_control(HCI_POWER_ON);
-    // go!
-    run_loop_execute(); 
     
-    // happy compiler!
     return 0;
 }
 

example/embedded/ble_peripheral.c
@@ -986,9 +986,5 @@ int btstack_main(int argc, const char * argv[]){
     // turn on!
     hci_power_control(HCI_POWER_ON);
     
-    // go!
-    run_loop_execute(); 
-    
-    // happy compiler!
     return 0;
 }

commit c9bc88e6504d5087d6eebb5d90c741cc55b61e3c

fix regression in c13e0a89 caused by timer structs in btstack_main

example/embedded/spp_and_le_counter.c
@@ -77,6 +77,7 @@ static uint8_t   spp_service_buffer[150];
 static int       le_notification_enabled;
 
 // THE Couner
+static timer_source_t heartbeat;
 static int  counter = 0;
 static char counter_string[30];
 static int  counter_string_len;
@@ -291,7 +292,6 @@ int btstack_main(void)
     att_server_init(profile_data, att_read_callback, att_write_callback);    
     att_dump_attributes();
     // set one-shot timer
-    timer_source_t heartbeat;
     heartbeat.process = &heartbeat_handler;
     run_loop_set_timer(&heartbeat, HEARTBEAT_PERIOD_MS);
     run_loop_add_timer(&heartbeat);

example/embedded/spp_counter.c
@@ -67,7 +67,7 @@
 
 static uint16_t  rfcomm_channel_id;
 static uint8_t   spp_service_buffer[150];
-
+static timer_source_t heartbeat;
 
 static void packet_handler (void * connection, uint8_t packet_type, uint16_t channel, uint8_t *packet, uint16_t size){
     bd_addr_t event_addr;
@@ -200,7 +200,6 @@ int btstack_main(int argc, const char * argv[]){
     hci_ssp_set_io_capability(SSP_IO_CAPABILITY_DISPLAY_YES_NO);
 
     // set one-shot timer
-    timer_source_t heartbeat;
     heartbeat.process = &heartbeat_handler;
     run_loop_set_timer(&heartbeat, HEARTBEAT_PERIOD_MS);
     run_loop_add_timer(&heartbeat);

example/embedded/spp_flowcontrol.c
@@ -65,6 +65,7 @@ static uint8_t   rfcomm_channel_nr = 1;
 static uint16_t  rfcomm_channel_id;
 static uint8_t   rfcomm_send_credit = 0;
 static uint8_t   spp_service_buffer[150];
+static timer_source_t heartbeat;
 
 // Bluetooth logic
 static void packet_handler (void * connection, uint8_t packet_type, uint16_t channel, uint8_t *packet, uint16_t size){
@@ -178,7 +179,6 @@ int btstack_main(int argc, const char * argv[]){
     sdp_register_service_internal(NULL, service_record_item);
     
     // set one-shot timer
-    timer_source_t heartbeat;
     heartbeat.process = &heartbeat_handler;
     run_loop_set_timer(&heartbeat, HEARTBEAT_PERIOD_MS);
     run_loop_add_timer(&heartbeat);