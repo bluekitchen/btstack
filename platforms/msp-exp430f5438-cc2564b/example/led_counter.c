95fb93797daaf94135c9217bd8606bdee5f205ed
Milanka Ringwald <mila@ringwald.ch>
2015-04-17 11:53:41 +0200

    copyright update

--- platforms/msp-exp430f5438-cc2564b/example/led_counter.c
+++ platforms/msp-exp430f5438-cc2564b/example/led_counter.c
@@ -1,7 +1,51 @@
-
-/* EXAMPLE_START(led_counter): UART and timer interrupt without Bluetooth
- * @brief The example uses the BTstack run loop to blink an LED. The example demonstrates how to setup hardware, initialize BTstack without Bluetooth, provide a periodic timer to toggle an LED and print number of toggles as a minimal BTstack test.
+/*
+ * Copyright (C) 2014 BlueKitchen GmbH
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the copyright holders nor the names of
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ * 4. Any redistribution, use, or modification is done solely for
+ *    personal benefit and not for any commercial purpose or for
+ *    monetary gain.
+ *
+ * THIS SOFTWARE IS PROVIDED BY BLUEKITCHEN GMBH AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL MATTHIAS
+ * RINGWALD OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
+ * THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * Please inquire about commercial licensing options at 
+ * contact@bluekitchen-gmbh.com
+ *
  */
+
+// *****************************************************************************
+/* EXAMPLE_START(led_counter): UART and timer interrupt without Bluetooth
+ *
+ * @text The example uses the BTstack run loop to blink an LED.
+ * The example demonstrates how to setup hardware, initialize BTstack without
+ * Bluetooth, provide a periodic timer to toggle an LED and print number of
+ * toggles as a minimal BTstack test.
+ */
+// *****************************************************************************
+
+
 
 #include <stdint.h>
 #include <stdio.h>
@@ -30,7 +74,8 @@ static void run_loop_register_timer(timer_source_t *timer, uint16_t period){
 }
 
 /* @section Periodic Timer Setup 
- * @brief As timers in BTstack are single shot,
+ *
+ * @text As timers in BTstack are single shot,
  * the periodic counter is implemented by re-registering the timer source in the
  * heartbeat handler callback function. Listing LEDToggler shows heartbeat handler
  * adapted to periodically toggle an LED and print number of toggles.  
@@ -52,7 +97,8 @@ static void heartbeat_handler(timer_source_t *ts){
 /* SNIPPET_END */
 
 /* @section Turn On and Go
- * @brief Listing RunLoopExecution shows how to setup and start the run loop.
+ *
+ * @text Listing RunLoopExecution shows how to setup and start the run loop.
  * For hardware and BTstack setup, please check the source code in
  * \path{../src/main.c}.
  */
@@ -65,8 +111,8 @@ static void timer_setup(){
 }
 /* SNIPPET_END */
 
-/* @brief Listing MainConfiguration shows main application code. It is
- * called after hardware and BTstack configuration (memory, run loop and
+/* @text Listing MainConfiguration shows main application code.
+ * It is called after hardware and BTstack configuration (memory, run loop and
  * transport layer) by the platform main in
  * \path{platforms/PLATFORM_NAME/src/main.c}.
  */
