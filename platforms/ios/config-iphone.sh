61d30832001719f3206fee985367fb82e66263f4
Matthias Ringwald <matthias@ringwald.ch>
2015-05-03 21:04:14 +0200

    call get_version.sh from ios src makefile

--- platforms/ios/config-iphone.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# get version from repository
-tools/get_version.sh
-
-# check if $THEOS is set
-
-# use theos Makefiles ??
-cp platforms/ios/example/Makefile.iphone example/daemon/Makefile
