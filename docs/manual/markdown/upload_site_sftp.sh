360f3fe3b00a65b2cecd867dc2d3020f189fe858
Matthias Ringwald <matthias@ringwald.ch>
2015-05-06 17:17:32 +0200

    renamed generated site folder to btstack, provide script to upload docs to server

---
+++ docs/manual/markdown/upload_site_sftp.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+USAGE="Usage: upload_site_sftp.sh host path user"
+
+# command line checks, bash
+if [ $# -ne 3 ]; then
+        echo ${USAGE}
+        exit 0
+fi
+host=$1
+path=$2
+user=$3
+
+echo Uploading generated docs to ${host}/${path}/btstack with user ${user}
+
+# SFTP is very peculiar: recursive put only works for a single directory
+sftp ${user}@${host} << EOF
+  mkdir ${path}/btstack
+  put -r btstack ${path}
+  quit
+EOF
