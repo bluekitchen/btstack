0c9edccf9f613cb1cfb6e26133fc80d4590d9135
Matthias Ringwald <matthias@ringwald.ch>
2015-06-03 19:36:37 +0200

    doku for iBeacon arduino examples

--- platforms/arduino/examples/iBeaconScanner/iBeaconScanner.ino
+++ platforms/arduino/examples/iBeaconScanner/iBeaconScanner.ino
@@ -2,19 +2,38 @@
 #include <BTstack.h>
 #include <SPI.h>
 
+/* EXAMPLE_START(iBeaconScanner): iBeacon Scanner
+ *
+ * @section Setup
+ * 
+ * @text After BTstack.setup(), BTstack is configured to call
+ * advertisementCallback whenever an Advertisement was received.
+ * Then, a device discovery is started
+ */
+
+/* LISTING_START(iBeaconSetup): iBeacon Scanner Setup */
 void setup(void){
     Serial.begin(9600);
-
-    BTstack.setBLEAdvertisementCallback(advertisementCallback);
     BTstack.setup();
+    BTstack.setBLEAdvertisementCallback(advertisementCallback);
     BTstack.bleStartScanning();
 }
-
+/* LISTING_END(iBeaconSetup): iBeacon Scanner Setup */
 
 void loop(void){
     BTstack.loop();
 }
 
+/*
+ * @section Advertisment Callback
+ *
+ * @text Whenever an Advertisement is received, isIBeacon() checks if 
+ * it contains an iBeacon. If yes, the Major ID, Minor ID, and UUID
+ * is printed.
+ * If it's not an iBeacon, only the BD_ADDR and the received signal strength
+ * (RSSI) is shown.
+ */
+/* LISTING_START(iBeaconCallback): iBeacon Scanner Callback */
 void advertisementCallback(BLEAdvertisement *adv) {
     if (adv->isIBeacon()) {
         Serial.print("iBeacon found ");
@@ -36,4 +55,5 @@ void advertisementCallback(BLEAdvertisement *adv) {
         Serial.println(adv->getRssi());
     }
 }
+/* LISTING_END(iBeaconCallback): iBeacon Scanner Callback */
 
