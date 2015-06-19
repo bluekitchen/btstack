1429564c1d37d614061b1fb8eeca71fe396eb4ff
Matthias Ringwald <matthias@ringwald.ch>
2015-06-19 11:31:44 +0200

    removed #include <sys/uio.h> from examples/pts tests

--- test/pts/ble_central_test.c
+++ test/pts/ble_central_test.c
@@ -45,7 +45,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/uio.h>
 #include <unistd.h>
 
 #include "btstack-config.h"
--- test/pts/l2cap_test.c
+++ test/pts/l2cap_test.c
@@ -47,7 +47,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/uio.h>
 #include <unistd.h>
 
 #include <btstack/hci_cmds.h>
