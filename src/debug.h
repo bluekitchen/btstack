5ee1c190aa5cbad5bf5819269e2a7d2b0439cfdd
Matthias Ringwald <matthias@ringwald.ch>
2015-09-21 17:57:03 +0200

    avoid namecollision with Kinetis SDK (and probably others)

--- src/debug.h
+++ src/debug.h
@@ -63,17 +63,17 @@ static inline void __log_unused(const char *format, ...) {
 
 #ifdef __AVR__
 #define HCI_DUMP_LOG(log_level, format, ...) hci_dump_log_P(log_level, PSTR(format), ## __VA_ARGS__)
-#define PRINTF(format, ...)       printf_P(PSTR(format), ## __VA_ARGS__)
+#define BTSTACK_PRINTF(format, ...)          printf_P(PSTR(format), ## __VA_ARGS__)
 #else
 #define HCI_DUMP_LOG(log_level, format, ...) hci_dump_log(log_level, format, ## __VA_ARGS__)
-#define PRINTF(format, ...)       printf(format, ## __VA_ARGS__)
+#define BTSTACK_PRINTF(format, ...)          printf(format, ## __VA_ARGS__)
 #endif
 
 #ifdef ENABLE_LOG_DEBUG
 #ifdef HAVE_HCI_DUMP
 #define log_debug(format, ...)  HCI_DUMP_LOG(LOG_LEVEL_DEBUG, format,  ## __VA_ARGS__)
 #else
-#define log_debug(format, ...)  PRINTF(format "\n",  ## __VA_ARGS__)
+#define log_debug(format, ...)  BTSTACK_PRINTF(format "\n",  ## __VA_ARGS__)
 #endif
 #else
 #define log_debug(...) __log_unused(__VA_ARGS__)
@@ -83,7 +83,7 @@ static inline void __log_unused(const char *format, ...) {
 #ifdef HAVE_HCI_DUMP
 #define log_info(format, ...)  HCI_DUMP_LOG(LOG_LEVEL_INFO, format,  ## __VA_ARGS__)
 #else
-#define log_info(format, ...)  PRINTF(format "\n",  ## __VA_ARGS__)
+#define log_info(format, ...)  BTSTACK_PRINTF(format "\n",  ## __VA_ARGS__)
 #endif
 #else
 #define log_info(...) __log_unused(__VA_ARGS__)
@@ -93,7 +93,7 @@ static inline void __log_unused(const char *format, ...) {
 #ifdef HAVE_HCI_DUMP
 #define log_error(format, ...)  HCI_DUMP_LOG(LOG_LEVEL_ERROR, format,  ## __VA_ARGS__)
 #else
-#define log_error(format, ...)  PRINTF(format "\n",  ## __VA_ARGS__)
+#define log_error(format, ...)  BTSTACK_PRINTF(format "\n",  ## __VA_ARGS__)
 #endif
 #else
 #define log_error(...) __log_unused(__VA_ARGS__)
