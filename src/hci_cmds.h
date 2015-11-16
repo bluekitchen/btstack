a8307d38d69822f16e8554d3aa17e234bb4c3dd1
Matthias Ringwald <matthias@ringwald.ch>
2015-11-16 15:03:02 +0100

    extract daemon cmds from hci_cmds into platform/daemon/src/daemon_cmd

--- src/hci_cmds.h
+++ src/hci_cmds.h
@@ -96,15 +96,6 @@ typedef enum {
 
 
 // HCI Commands - see hci_cmds.c for info on parameters
-extern const hci_cmd_t btstack_get_state;
-extern const hci_cmd_t btstack_set_power_mode;
-extern const hci_cmd_t btstack_set_acl_capture_mode;
-extern const hci_cmd_t btstack_get_version;
-extern const hci_cmd_t btstack_get_system_bluetooth_enabled;
-extern const hci_cmd_t btstack_set_system_bluetooth_enabled;
-extern const hci_cmd_t btstack_set_discoverable;
-extern const hci_cmd_t btstack_set_bluetooth_enabled;    // only used by btstack config
-    
 extern const hci_cmd_t hci_accept_connection_request;
 extern const hci_cmd_t hci_accept_synchronous_connection;
 extern const hci_cmd_t hci_authentication_requested;
@@ -199,68 +190,6 @@ extern const hci_cmd_t hci_le_start_encryption;
 extern const hci_cmd_t hci_le_test_end;
 extern const hci_cmd_t hci_le_transmitter_test;
     
-extern const hci_cmd_t l2cap_accept_connection_cmd;
-extern const hci_cmd_t l2cap_create_channel_cmd;
-extern const hci_cmd_t l2cap_create_channel_mtu_cmd;
-extern const hci_cmd_t l2cap_decline_connection_cmd;
-extern const hci_cmd_t l2cap_disconnect_cmd;
-extern const hci_cmd_t l2cap_register_service_cmd;
-extern const hci_cmd_t l2cap_unregister_service_cmd;
-
-extern const hci_cmd_t sdp_register_service_record_cmd;
-extern const hci_cmd_t sdp_unregister_service_record_cmd;
-extern const hci_cmd_t sdp_client_query_rfcomm_services_cmd;
-extern const hci_cmd_t sdp_client_query_services_cmd;
-
-
-// accept connection @param bd_addr(48), rfcomm_cid (16)
-extern const hci_cmd_t rfcomm_accept_connection_cmd;
-// create rfcomm channel: @param bd_addr(48), channel (8)
-extern const hci_cmd_t rfcomm_create_channel_cmd;
-// create rfcomm channel: @param bd_addr(48), channel (8), mtu (16), credits (8)
-extern const hci_cmd_t rfcomm_create_channel_with_initial_credits_cmd;
-// decline rfcomm disconnect,@param bd_addr(48), rfcomm cid (16), reason(8)
-extern const hci_cmd_t rfcomm_decline_connection_cmd;
-// disconnect rfcomm disconnect, @param rfcomm_cid(8), reason(8)
-extern const hci_cmd_t rfcomm_disconnect_cmd;
-// register rfcomm service: @param channel(8), mtu (16)
-extern const hci_cmd_t rfcomm_register_service_cmd;
-// register rfcomm service: @param channel(8), mtu (16), initial credits (8)
-extern const hci_cmd_t rfcomm_register_service_with_initial_credits_cmd;
-// unregister rfcomm service, @param service_channel(16)
-extern const hci_cmd_t rfcomm_unregister_service_cmd;
-// request persisten rfcomm channel for service name: serive name (char*) 
-extern const hci_cmd_t rfcomm_persistent_channel_for_service_cmd;
-extern const hci_cmd_t rfcomm_grants_credits_cmd;
-
-extern const hci_cmd_t gap_disconnect_cmd;
-extern const hci_cmd_t gap_le_scan_start;
-extern const hci_cmd_t gap_le_scan_stop;
-extern const hci_cmd_t gap_le_set_scan_parameters;
-extern const hci_cmd_t gap_le_connect_cmd;
-extern const hci_cmd_t gap_le_connect_cancel_cmd;
-extern const hci_cmd_t gatt_discover_primary_services_cmd;
-
-extern const hci_cmd_t gatt_discover_primary_services_by_uuid16_cmd;
-extern const hci_cmd_t gatt_discover_primary_services_by_uuid128_cmd;
-extern const hci_cmd_t gatt_find_included_services_for_service_cmd;
-extern const hci_cmd_t gatt_discover_characteristics_for_service_cmd;
-extern const hci_cmd_t gatt_discover_characteristics_for_service_by_uuid128_cmd;
-extern const hci_cmd_t gatt_discover_characteristic_descriptors_cmd;
-extern const hci_cmd_t gatt_read_value_of_characteristic_cmd;
-extern const hci_cmd_t gatt_read_long_value_of_characteristic_cmd;
-extern const hci_cmd_t gatt_write_value_of_characteristic_without_response_cmd;
-extern const hci_cmd_t gatt_write_value_of_characteristic_cmd;
-extern const hci_cmd_t gatt_write_long_value_of_characteristic_cmd;
-extern const hci_cmd_t gatt_reliable_write_long_value_of_characteristic_cmd;
-extern const hci_cmd_t gatt_read_characteristic_descriptor_cmd;
-extern const hci_cmd_t gatt_read_long_characteristic_descriptor_cmd;
-extern const hci_cmd_t gatt_write_characteristic_descriptor_cmd;
-extern const hci_cmd_t gatt_write_long_characteristic_descriptor_cmd;
-extern const hci_cmd_t gatt_write_client_characteristic_configuration_cmd;
-extern const hci_cmd_t gatt_get_mtu;
-
-
 #if defined __cplusplus
 }
 #endif
