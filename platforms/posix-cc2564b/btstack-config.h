6b26a6b00b8ab2d83acd9ebb874608e9de33bd9e
Matthias Ringwald <matthias@ringwald.ch>
2016-01-14 14:20:22 +0100

    add HAVE_SCO_OVER_HCI for posix-cc2564b

--- platforms/posix-cc2564b/btstack-config.h
+++ platforms/posix-cc2564b/btstack-config.h
@@ -3,22 +3,23 @@
 #ifndef __BTSTACK_CONFIG
 #define __BTSTACK_CONFIG
 
-#define HAVE_TRANSPORT_USB
+#define ENABLE_LOG_ERROR
+#define ENABLE_LOG_INFO 
 #define HAVE_BLE
-#define USE_POSIX_RUN_LOOP
-#define HAVE_SDP
+#define HAVE_BZERO
+#define HAVE_HCI_DUMP
+#define HAVE_MALLOC
 #define HAVE_RFCOMM
-#define REMOTE_DEVICE_DB remote_device_db_iphone
+#define HAVE_SDP
 #define HAVE_SO_NOSIGPIPE
 #define HAVE_TIME
-#define HAVE_MALLOC
-#define HAVE_BZERO
-#define SDP_DES_DUMP
-#define ENABLE_LOG_INFO 
-#define ENABLE_LOG_ERROR
-#define HCI_INCOMING_PRE_BUFFER_SIZE 14 // sizeof benep heade, avoid memcpy
+#define HAVE_SCO
+#define HAVE_SCO_OVER_HCI
 #define HCI_ACL_PAYLOAD_SIZE (1691 + 4)
-#define HAVE_HCI_DUMP
+#define HCI_INCOMING_PRE_BUFFER_SIZE 14 // sizeof BNEP header, avoid memcpy
+#define REMOTE_DEVICE_DB remote_device_db_iphone
 #define SDP_DES_DUMP
+#define SDP_DES_DUMP
+#define USE_POSIX_RUN_LOOP
 
 #endif
