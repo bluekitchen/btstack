615ae44436c0f8488ffbc1cbf94785ea755a57ee
Matthias Ringwald <matthias@ringwald.ch>
2016-03-30 17:02:46 +0200

    posix: cleanup fast FTDI hack

--- port/posix-h4/main.c
+++ port/posix-h4/main.c
@@ -95,10 +95,10 @@ void hal_led_toggle(void){
     printf("LED State %u\n", led_state);
 }
 static void use_fast_uart(void){
-#if defined(HAVE_B1200_MAPPED_TO_3000000) || defined(HAVE_B300_MAPPED_TO_3000000)
+#if defined(HAVE_POSIX_B240000_MAPPED_TO_3000000) || defined(HAVE_POSIX_B600_MAPPED_TO_3000000)
     printf("Using 3000000 baud.\n");
     config.baudrate_main = 3000000;
-#elif defined(HAVE_B1200_MAPPED_TO_2000000) || defined(HAVE_POSIX_B300_MAPPED_TO_2000000)
+#elif defined(HAVE_POSIX_B1200_MAPPED_TO_2000000) || defined(HAVE_POSIX_B300_MAPPED_TO_2000000)
     printf("Using 2000000 baud.\n");
     config.baudrate_main = 2000000;
 #else
