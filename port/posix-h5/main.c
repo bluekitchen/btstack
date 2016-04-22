94a5538cc69477d2f91829a81568a7c58aac3c4c
Matthias Ringwald <matthias@ringwald.ch>
2016-04-22 16:28:04 +0200

    posix: added posix-h5 port

---
+++ port/posix-h5/main.c
@@ -0,0 +1,191 @@
+/*
+ * Copyright (C) 2014 BlueKitchen GmbH
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the copyright holders nor the names of
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ * 4. Any redistribution, use, or modification is done solely for
+ *    personal benefit and not for any commercial purpose or for
+ *    monetary gain.
+ *
+ * THIS SOFTWARE IS PROVIDED BY BLUEKITCHEN GMBH AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL MATTHIAS
+ * RINGWALD OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
+ * THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * Please inquire about commercial licensing options at 
+ * contact@bluekitchen-gmbh.com
+ *
+ */
+
+// *****************************************************************************
+//
+// minimal setup for HCI code
+//
+// *****************************************************************************
+
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <signal.h>
+
+#include "btstack_config.h"
+
+#include "btstack_debug.h"
+#include "btstack_event.h"
+#include "btstack_link_key_db_fs.h"
+#include "btstack_memory.h"
+#include "btstack_run_loop.h"
+#include "btstack_run_loop_posix.h"
+#include "hci.h"
+#include "hci_dump.h"
+#include "stdin_support.h"
+
+#include "btstack_chipset_bcm.h"
+#include "btstack_chipset_csr.h"
+#include "btstack_chipset_cc256x.h"
+#include "btstack_chipset_em9301.h"
+#include "btstack_chipset_stlc2500d.h"
+#include "btstack_chipset_tc3566x.h"
+
+int btstack_main(int argc, const char * argv[]);
+
+static hci_transport_config_uart_t config = {
+    HCI_TRANSPORT_CONFIG_UART,
+    115200,
+    0,  // main baudrate
+    1,  // flow control
+    NULL,
+};
+
+static btstack_packet_callback_registration_t hci_event_callback_registration;
+
+static void sigint_handler(int param){
+
+#ifndef _WIN32
+    // reset anyway
+    btstack_stdin_reset();
+#endif
+
+    log_info(" <= SIGINT received, shutting down..\n");   
+    hci_power_control(HCI_POWER_OFF);
+    hci_close();
+    log_info("Good bye, see you.\n");    
+    exit(0);
+}
+
+static int led_state = 0;
+void hal_led_toggle(void){
+    led_state = 1 - led_state;
+    printf("LED State %u\n", led_state);
+}
+static void use_fast_uart(void){
+    // printf("Using 921600 baud.\n");
+    // config.baudrate_main = 921600;
+}
+
+static void local_version_information_callback(uint8_t * packet){
+    printf("Local version information:\n");
+    uint16_t hci_version    = little_endian_read_16(packet, 4);
+    uint16_t hci_revision   = little_endian_read_16(packet, 6);
+    uint16_t lmp_version    = little_endian_read_16(packet, 8);
+    uint16_t manufacturer   = little_endian_read_16(packet, 10);
+    uint16_t lmp_subversion = little_endian_read_16(packet, 12);
+    printf("- HCI Version  0x%04x\n", hci_version);
+    printf("- HCI Revision 0x%04x\n", hci_revision);
+    printf("- LMP Version  0x%04x\n", lmp_version);
+    printf("- LMP Revision 0x%04x\n", lmp_subversion);
+    printf("- Manufacturer 0x%04x\n", manufacturer);
+    switch (manufacturer){
+        case COMPANY_ID_CAMBRIDGE_SILICON_RADIO:
+            printf("Cambridge Silicon Radio CSR chipset.\n");
+            use_fast_uart();
+            hci_set_chipset(btstack_chipset_csr_instance());
+            break;
+        case COMPANY_ID_TEXAS_INSTRUMENTS_INC: 
+            printf("Texas Instruments - CC256x compatible chipset.\n");
+            use_fast_uart();
+            hci_set_chipset(btstack_chipset_cc256x_instance());
+            break;
+        case COMPANY_ID_BROADCOM_CORPORATION:   
+            printf("Broadcom chipset. Not supported yet\n");
+            // hci_set_chipset(btstack_chipset_bcm_instance());
+            break;
+        case COMPANY_ID_ST_MICROELECTRONICS:   
+            printf("ST Microelectronics - using STLC2500d driver.\n");
+            use_fast_uart();
+            hci_set_chipset(btstack_chipset_stlc2500d_instance());
+            break;
+        case COMPANY_ID_EM_MICROELECTRONICS_MARIN:
+            printf("EM Microelectronics - using EM9301 driver.\n");
+            hci_set_chipset(btstack_chipset_em9301_instance());
+            break;
+        default:
+            printf("Unknown manufacturer / manufacturer not supported yet.\n");
+            break;
+    }
+}
+
+static void packet_handler (uint8_t packet_type, uint16_t channel, uint8_t *packet, uint16_t size){
+    if (packet_type != HCI_EVENT_PACKET) return;
+    if (hci_event_packet_get_type(packet) != BTSTACK_EVENT_STATE) return;
+    if (btstack_event_state_get_state(packet) != HCI_STATE_WORKING) return;
+    printf("BTstack up and running.\n");
+}
+
+
+int main(int argc, const char * argv[]){
+
+	/// GET STARTED with BTstack ///
+	btstack_memory_init();
+    btstack_run_loop_init(btstack_run_loop_posix_get_instance());
+	    
+    // use logger: format HCI_DUMP_PACKETLOGGER, HCI_DUMP_BLUEZ or HCI_DUMP_STDOUT
+    hci_dump_open("/tmp/hci_dump.pklg", HCI_DUMP_PACKETLOGGER);
+
+    // pick serial port
+    config.device_name = "/dev/tty.usbserial-A900K0VK";
+
+    // init HCI
+    const btstack_uart_block_t * uart_driver = btstack_uart_block_posix_instance();
+    const hci_transport_t * transport = hci_transport_h5_instance(uart_driver);
+    const btstack_link_key_db_t * link_key_db = btstack_link_key_db_fs_instance();
+	hci_init(transport, (void*) &config);
+    hci_set_link_key_db(link_key_db);
+    
+    // setup dynamic chipset driver setup
+    hci_set_local_version_information_callback(&local_version_information_callback);
+
+    // register for HCI events
+    hci_event_callback_registration.callback = &packet_handler;
+    hci_add_event_handler(&hci_event_callback_registration);
+
+    // handle CTRL-c
+    signal(SIGINT, sigint_handler);
+
+    // setup app
+    btstack_main(argc, argv);
+
+    // go
+    btstack_run_loop_execute();    
+
+    return 0;
+}
