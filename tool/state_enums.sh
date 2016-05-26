a068cdf483b2f9669acc4ca75bcbeab23b82ff24
Matthias Ringwald <matthias@ringwald.ch>
2016-05-26 11:23:44 +0200

    tool: state_enums.sh enumerates SM and RFCOMM states

---
+++ tool/state_enums.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+BTSTACK_ROOT=`dirname $0`'/..'
+
+# SM
+echo "Security Manager states"
+grep SM_ $BTSTACK_ROOT/src/hci.h | nl -v0
+
+echo
+
+# RFCOMM
+echo "RFCOMM Multiplexer states"
+grep RFCOMM_MULTIPLEXER_ $BTSTACK_ROOT/src/classic/rfcomm.h | grep -v RFCOMM_MULTIPLEXER_EVENT | grep -v RFCOMM_MULTIPLEXER_STATE | nl
+echo "RFCOMM Channel states"
+grep RFCOMM_CHANNEL_ $BTSTACK_ROOT/src/classic/rfcomm.h | grep -v RFCOMM_CHANNEL_STATE | grep -v "// state variables" | nl
+
+echo
