5b4ff0f76de63bc37890a49a94815f4f09ac38e3
Milanka Ringwald <mila@ringwald.ch>
2016-07-23 15:19:56 +0200

    sbc: extract hfp_msbc

--- test/sbc/sbc_encoder_test.c
+++ test/sbc/sbc_encoder_test.c
@@ -52,70 +52,13 @@
 
 #include "btstack.h"
 #include "btstack_sbc_encoder.h"
+#include "hfp_msbc.h"
 
-#define MSBC_FRAME_SIZE 57
 
 static int num_frames = 0;
 
 static int16_t read_buffer[8*16*2];
 static uint8_t output_buffer[24];
-
-static btstack_sbc_encoder_state_t state;
-static uint8_t msbc_padding[] = {0,0,0};
-
-static uint8_t msbc_buffer[2*(MSBC_FRAME_SIZE + sizeof(msbc_padding))];
-static int msbc_buffer_offset = 0; 
-
-void hfp_msbc_init(void);
-int  hfp_msbc_num_audio_samples_per_frame(void);
-
-int  hfp_msbc_can_encode_audio_frame_now(void);
-void hfp_msbc_encode_audio_frame(int16_t * pcm_samples);
-
-int  hfp_msbc_num_bytes_in_stream(void);
-void hfp_msbc_read_from_stream(uint8_t * buf, int size);
-
-void hfp_msbc_init(void){
-    btstack_sbc_encoder_init(&state, SBC_MODE_mSBC, 16, 8, 0, 16000, 26);
-    msbc_buffer_offset = 0;
-}
-
-int hfp_msbc_can_encode_audio_frame_now(void){
-    return sizeof(msbc_buffer) - msbc_buffer_offset >= MSBC_FRAME_SIZE + sizeof(msbc_padding); 
-}
-
-void hfp_msbc_encode_audio_frame(int16_t * pcm_samples){
-    if (!hfp_msbc_can_encode_audio_frame_now()) return;
-
-    btstack_sbc_encoder_process_data(pcm_samples);
-    
-    memcpy(msbc_buffer + msbc_buffer_offset, msbc_padding, sizeof(msbc_padding));
-    msbc_buffer_offset += sizeof(msbc_padding);
-
-    memcpy(msbc_buffer + msbc_buffer_offset, btstack_sbc_encoder_sbc_buffer(), MSBC_FRAME_SIZE);
-    msbc_buffer_offset += MSBC_FRAME_SIZE;
-}
-
-void hfp_msbc_read_from_stream(uint8_t * buf, int size){
-    int bytes_to_copy = size;
-    if (size > msbc_buffer_offset){
-        bytes_to_copy = msbc_buffer_offset;
-        log_error("sbc frame storage is smaller then the output buffer");
-        return;
-    }
-
-    memcpy(buf, msbc_buffer, bytes_to_copy);
-    memmove(msbc_buffer, msbc_buffer + bytes_to_copy, sizeof(msbc_buffer) - bytes_to_copy);
-    msbc_buffer_offset -= bytes_to_copy;
-}
-
-int hfp_msbc_num_bytes_in_stream(void){
-    return msbc_buffer_offset;
-}
-
-int hfp_msbc_num_audio_samples_per_frame(void){
-    return btstack_sbc_encoder_num_audio_samples();
-}
 
 static ssize_t __read(int fd, void *buf, size_t count){
     ssize_t len, pos = 0;
@@ -131,7 +74,6 @@ static ssize_t __read(int fd, void *buf, size_t count){
     return pos;
 }
 
-
 static int read_audio_frame(int wav_fd){
     int i;
     int bytes_read = 0;  
@@ -145,7 +87,6 @@ static int read_audio_frame(int wav_fd){
     }
     return bytes_read;
 }
-
 
 static void read_wav_header(int wav_fd){
     uint8_t buf[40];
