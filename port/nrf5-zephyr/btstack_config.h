43ab8f9679edbe0f5c79b7805bdfcc50791b8535
Matthias Ringwald <matthias@ringwald.ch>
2016-11-03 17:05:47 +0100

    Port of BTstack to Zephyr OS running on Nordic nRF5 Series

---
+++ port/nrf5-zephyr/btstack_config.h
@@ -0,0 +1,34 @@
+//
+// btstack_config.h for mRF5-Zephyr port
+//
+
+#ifndef __BTSTACK_CONFIG
+#define __BTSTACK_CONFIG
+
+// Port related features
+
+// BTstack features that can be enabled
+#define ENABLE_BLE
+#define ENABLE_LOG_INFO
+#define ENABLE_LOG_ERROR
+
+// BTstack configuration. buffers, sizes, ...
+#define HCI_ACL_PAYLOAD_SIZE 52
+#define MAX_NR_WHITELIST_ENTRIES 1
+#define MAX_NR_HCI_CONNECTIONS 1
+#define MAX_NR_SM_LOOKUP_ENTRIES 3
+#define MAX_NR_L2CAP_SERVICES 1
+#define MAX_NR_L2CAP_CHANNELS 1
+#define MAX_NR_GATT_CLIENTS 1
+#define MAX_NR_GATT_SUBCLIENTS 1
+
+#define MAX_NR_RFCOMM_MULTIPLEXERS 0
+#define MAX_NR_RFCOMM_SERVICES 0
+#define MAX_NR_RFCOMM_CHANNELS 0
+#define MAX_NR_HFP_CONNECTIONS 0
+#define MAX_NR_BTSTACK_LINK_KEY_DB_MEMORY_ENTRIES 0
+#define MAX_NR_BNEP_SERVICES 0
+#define MAX_NR_BNEP_CHANNELS 0
+#define MAX_NR_SERVICE_RECORD_ITEMS 1
+
+#endif
