0631daa1e11771ab2f53fd4febcf8b3327ccbc9d
Matt Kelly <Matthew.Joseph.Kelly@gmail.com>
2017-02-20 15:41:02 -0500

    esp32: Partially implement hal_cpu layer

--- port/esp32/main/hal_cpu.c
+++ port/esp32/main/hal_cpu.c
@@ -3,12 +3,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "freertos/FreeRTOS.h"
+
+static portMUX_TYPE global_int_mux = portMUX_INITIALIZER_UNLOCKED;
+
 void hal_cpu_disable_irqs(void){
     //printf("hal_cpu_disable_irqs\n");
+    portENTER_CRITICAL(&global_int_mux);
 }
+
 void hal_cpu_enable_irqs(void){
     //printf("hal_cpu_enable_irqs\n");
+    portEXIT_CRITICAL(&global_int_mux);
 }
+
 void hal_cpu_enable_irqs_and_sleep(void){
     //printf("hal_cpu_enable_irqs_and_sleep\n");
+    portEXIT_CRITICAL(&global_int_mux);
+    // @TODO
 }
