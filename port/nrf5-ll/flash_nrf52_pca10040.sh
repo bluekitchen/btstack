b615edf27271258c66ca363658a662bfedaba0ac
Matthias Ringwald <matthias@ringwald.ch>
2017-02-21 21:12:43 +0100

    nrf5-ll: allow to pass in .hex file to flash_nrf52_pca10040.sh

--- port/nrf5-ll/flash_nrf52_pca10040.sh
+++ port/nrf5-ll/flash_nrf52_pca10040.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
+BINARY=$1
+if [ X$BINARY = "X" ]; then
+	echo "Usage: $0 path/to/app.hex"
+	exit 0
+fi
 nrfjprog --eraseall -f nrf52
-nrfjprog --program outdir/nrf52_pca10040/zephyr.hex -f nrf52
+echo "Flashing $BINARY"
+nrfjprog --program $BINARY -f nrf52
 nrfjprog --reset -f nrf52
