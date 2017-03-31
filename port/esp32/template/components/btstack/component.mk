82cfdfc2b7f4361ab83893839b82ada82bfd43aa
Matthias Ringwald <matthias@ringwald.ch>
2017-03-31 17:20:30 +0200

    esp32: prepare example template

--- port/esp32/template/components/btstack/component.mk
+++ port/esp32/template/components/btstack/component.mk
@@ -6,7 +6,7 @@
 # in the build directory. This behaviour is entirely configurable,
 # please read the ESP-IDF documents if you need to do this.
 #
-BTSTACK_ROOT := ../../../..
+BTSTACK_ROOT := ../../../../..
 
 COMPONENT_ADD_INCLUDEDIRS := \
 	${BTSTACK_ROOT}/3rd-party/bluedroid/decoder/include \
