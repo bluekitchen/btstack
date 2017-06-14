56b31fad2a490a79afbf380730d9deb2b9fcc0dc
Matthias Ringwald <matthias@ringwald.ch>
2017-06-14 15:47:43 +0200

    test/avrpc: add guards for HAVE_BTSTACK_STDIN

--- test/btstack_config.h
+++ test/btstack_config.h
@@ -9,7 +9,7 @@
 #define HAVE_MALLOC
 #define HAVE_POSIX_TIME
 #define HAVE_POSIX_FILE_IO
-#define HAVE_POSIX_STDIN
+#define HAVE_BTSTACK_STDIN
 
 // BTstack features that can be enabled
 #define ENABLE_BLE
