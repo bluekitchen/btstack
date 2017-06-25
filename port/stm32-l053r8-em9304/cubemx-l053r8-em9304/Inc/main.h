6e78c2056a718d7f817b7cd373f33a85b366046a
Matthias Ringwald <matthias@ringwald.ch>
2017-06-25 10:46:06 +0200

    stm32-l053r8-em9304: use PC0=A5 as debug output #0

--- port/stm32-l053r8-em9304/cubemx-l053r8-em9304/Inc/main.h
+++ port/stm32-l053r8-em9304/cubemx-l053r8-em9304/Inc/main.h
@@ -48,6 +48,8 @@
 
 #define B1_Pin GPIO_PIN_13
 #define B1_GPIO_Port GPIOC
+#define DEBUG_0_Pin GPIO_PIN_0
+#define DEBUG_0_GPIO_Port GPIOC
 #define USART_TX_Pin GPIO_PIN_2
 #define USART_TX_GPIO_Port GPIOA
 #define USART_RX_Pin GPIO_PIN_3
