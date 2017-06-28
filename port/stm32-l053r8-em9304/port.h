72c93b66ff5d8be9e11d7e22fcf5ea2a6a3917ef
Matthias Ringwald <matthias@ringwald.ch>
2017-06-28 11:26:19 +0200

    stm32-l053r8-em9304: fix copyright year

--- port/stm32-l053r8-em9304/port.h
+++ port/stm32-l053r8-em9304/port.h
@@ -1,6 +1,6 @@
 
 /*
- * Copyright (C) 2014 BlueKitchen GmbH
+ * Copyright (C) 2017 BlueKitchen GmbH
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
