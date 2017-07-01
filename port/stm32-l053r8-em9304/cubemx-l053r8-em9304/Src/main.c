c31a3132eef15e8e7f4d33312aa27163b2c1d387
Matthias Ringwald <matthias@ringwald.ch>
2017-07-01 19:09:34 +0200

    stm32-l053r8-em9304: use SPI at 8 Mhz

--- port/stm32-l053r8-em9304/cubemx-l053r8-em9304/Src/main.c
+++ port/stm32-l053r8-em9304/cubemx-l053r8-em9304/Src/main.c
@@ -188,7 +188,7 @@ static void MX_SPI1_Init(void)
   hspi1.Init.CLKPolarity = SPI_POLARITY_LOW;
   hspi1.Init.CLKPhase = SPI_PHASE_1EDGE;
   hspi1.Init.NSS = SPI_NSS_SOFT;
-  hspi1.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_32;
+  hspi1.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_4;
   hspi1.Init.FirstBit = SPI_FIRSTBIT_MSB;
   hspi1.Init.TIMode = SPI_TIMODE_DISABLE;
   hspi1.Init.CRCCalculation = SPI_CRCCALCULATION_DISABLE;
