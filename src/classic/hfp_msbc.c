91a08b113f3a191ee13ffb3b4ce0684f89165ce7
Matthias Ringwald <matthias@ringwald.ch>
2017-07-19 16:16:23 +0200

    fix compile: missing <stdint.h>, new channel mode param for sbc encoder

--- src/classic/hfp_msbc.c
+++ src/classic/hfp_msbc.c
@@ -66,7 +66,7 @@ static uint8_t msbc_buffer[2*(MSBC_FRAME_SIZE + MSBC_EXTRA_SIZE)];
 static int msbc_buffer_offset = 0; 
 
 void hfp_msbc_init(void){
-    btstack_sbc_encoder_init(&state, SBC_MODE_mSBC, 16, 8, 0, 16000, 26);
+    btstack_sbc_encoder_init(&state, SBC_MODE_mSBC, 16, 8, 0, 16000, 26, 0);
     msbc_buffer_offset = 0;
     msbc_sequence_number = 0;
 }
