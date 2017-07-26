58a1b1bbd780fce065cc682a3cd9375e3ada0688
Matthias Ringwald <matthias@ringwald.ch>
2017-07-26 19:19:33 +0200

    samv71-xplained-atwilc3000: add BTstack files

--- port/samv71-xplained-atwilc3000/config.mk
+++ port/samv71-xplained-atwilc3000/config.mk
@@ -52,6 +52,7 @@ TARGET_SRAM = getting-started_sram.elf
 
 # List of C source files.
 CSRCS = \
+       ../main            \
        common/services/clock/samv71/sysclk.c              \
        common/services/serial/usart_serial.c              \
        common/utils/interrupt/interrupt_sam_nvic.c        \
@@ -70,7 +71,6 @@ CSRCS = \
        sam/utils/cmsis/samv71/source/templates/gcc/startup_samv71.c \
        sam/utils/cmsis/samv71/source/templates/system_samv71.c \
        sam/utils/syscalls/gcc/syscalls.c                  \
-       ../main            \
 
 # List of assembler source files.
 ASSRCS = 
@@ -81,6 +81,7 @@ INC_PATH = \
        common/services/clock                              \
        common/services/gpio                               \
        common/services/ioport                             \
+       common/services/delay                              \
        common/services/serial                             \
        common/services/serial/sam_uart                    \
        common/utils                                       \
@@ -96,6 +97,7 @@ INC_PATH = \
        sam/drivers/tc                                     \
        sam/drivers/uart                                   \
        sam/drivers/usart                                  \
+       sam/drivers/xdmac                                  \
        sam/utils                                          \
        sam/utils/cmsis/samv71/include                     \
        sam/utils/cmsis/samv71/source/templates            \
@@ -105,6 +107,51 @@ INC_PATH = \
        thirdparty/CMSIS/Include                           \
        thirdparty/CMSIS/Lib/GCC \
        ..
+
+BTSTACK_ROOT = ../../..
+INC_PATH += ${BTSTACK_ROOT}/src/ble
+INC_PATH += ${BTSTACK_ROOT}/src/ble/gatt-service
+INC_PATH += ${BTSTACK_ROOT}/src/classic
+INC_PATH += ${BTSTACK_ROOT}/src
+INC_PATH += ${BTSTACK_ROOT}/3rd-party/micro-ecc
+INC_PATH += ${BTSTACK_ROOT}/platform/embedded
+INC_PATH += ${BTSTACK_ROOT}/chipset/atwilc3000
+
+# VPATH += ${BTSTACK_ROOT}/src
+# VPATH += ${BTSTACK_ROOT}/src/ble
+# VPATH += ${BTSTACK_ROOT}/src/ble/gatt-service
+# VPATH += ${BTSTACK_ROOT}/src/classic
+# VPATH += ${BTSTACK_ROOT}/platform/embedded
+# VPATH += ${BTSTACK_ROOT}/example
+# VPATH += ${BTSTACK_ROOT}/3rd-party/micro-ecc
+
+CSRCS += \
+	${BTSTACK_ROOT}/src/ad_parser.c \
+	${BTSTACK_ROOT}/src/ble/ancs_client.c \
+	${BTSTACK_ROOT}/src/ble/att_db.c \
+	${BTSTACK_ROOT}/src/ble/att_dispatch.c \
+	${BTSTACK_ROOT}/src/ble/att_server.c \
+	${BTSTACK_ROOT}/src/ble/gatt-service/battery_service_server.c \
+	${BTSTACK_ROOT}/src/btstack_linked_list.c \
+	${BTSTACK_ROOT}/src/btstack_memory.c \
+	${BTSTACK_ROOT}/src/btstack_memory_pool.c \
+	${BTSTACK_ROOT}/src/btstack_ring_buffer.c \
+	${BTSTACK_ROOT}/src/btstack_run_loop.c \
+	${BTSTACK_ROOT}/platform/embedded/btstack_run_loop_embedded.c \
+	${BTSTACK_ROOT}/platform/embedded/btstack_uart_block_embedded.c \
+	${BTSTACK_ROOT}/src/btstack_util.c \
+	${BTSTACK_ROOT}/src/ble/gatt-service/device_information_service_server.c \
+	${BTSTACK_ROOT}/src/ble/gatt_client.c \
+	${BTSTACK_ROOT}/src/hci.c \
+	${BTSTACK_ROOT}/src/hci_cmd.c \
+	${BTSTACK_ROOT}/src/hci_dump.c \
+	${BTSTACK_ROOT}/src/hci_transport_h4.c \
+	${BTSTACK_ROOT}/src/l2cap.c \
+	${BTSTACK_ROOT}/src/l2cap_signaling.c \
+	${BTSTACK_ROOT}/src/ble/le_device_db_memory.c \
+	${BTSTACK_ROOT}/src/ble/sm.c \
+	${BTSTACK_ROOT}/3rd-party/micro-ecc/uECC.c \
+	${BTSTACK_ROOT}/example/led_counter.c \
 
 # Additional search paths for libraries.
 LIB_PATH =  \
