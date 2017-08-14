ee3296534062c17d151f5e9aefb423a586ccd6d3
Matthias Ringwald <matthias@ringwald.ch>
2017-08-14 16:45:48 +0200

    test/flash_tlv: fix test

--- test/btstack_config.h
+++ test/btstack_config.h
@@ -29,8 +29,8 @@
 // BTstack configuration. buffers, sizes, ...
 #define HCI_ACL_PAYLOAD_SIZE 52
 #define HCI_INCOMING_PRE_BUFFER_SIZE 4
-#define MAX_NR_LE_DEVICE_DB_ENTRIES 4
 
 #define NVM_NUM_LINK_KEYS 2
+#define NVM_NUM_DEVICE_DB_ENTRIES 4
 
 #endif
