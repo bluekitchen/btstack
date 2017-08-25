a0b8a58cf86bb5dcb706e025ce3ffb1106a93d23
Milanka Ringwald <mila@ringwald.ch>
2017-08-25 16:48:50 +0200

    avdtp: on connect request, check if streaming is already established

--- src/classic/avdtp_util.h
+++ src/classic/avdtp_util.h
@@ -61,6 +61,7 @@ avdtp_stream_endpoint_t * avdtp_stream_endpoint_for_l2cap_cid(uint16_t l2cap_cid
 avdtp_stream_endpoint_t * avdtp_stream_endpoint_with_seid(uint8_t seid, avdtp_context_t * context);
 avdtp_stream_endpoint_t * avdtp_stream_endpoint_associated_with_acp_seid(uint16_t acp_seid, avdtp_context_t * context);
 avdtp_stream_endpoint_t * avdtp_stream_endpoint_for_seid(uint16_t seid, avdtp_context_t * context);
+avdtp_stream_endpoint_t * avdtp_stream_endpoint_for_signaling_cid(uint16_t l2cap_cid, avdtp_context_t * context);
 
 static inline uint8_t avdtp_header(uint8_t tr_label, avdtp_packet_type_t packet_type, avdtp_message_type_t msg_type){
     return (tr_label<<4) | ((uint8_t)packet_type<<2) | (uint8_t)msg_type;
