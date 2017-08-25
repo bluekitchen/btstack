2dd6d27267a3ffb5b6630bfb2041c3c85647f9b4
Matthias Ringwald <matthias@ringwald.ch>
2017-08-25 16:11:03 +0200

    sm: only store derived link key once, fixes compile warning

--- src/ble/sm.c
+++ src/ble/sm.c
@@ -1540,7 +1540,6 @@ static void sm_sc_cmac_done(uint8_t * hash){
             if (IS_RESPONDER(sm_conn->sm_role)){
                 sm_conn->sm_engine_state = SM_RESPONDER_IDLE; 
             } else {
-                gap_store_link_key_for_bd_addr(setup->sm_s_address, setup->sm_t, link_key_type);
                 sm_conn->sm_engine_state = SM_INITIATOR_CONNECTED; 
             }
             sm_done_for_handle(sm_conn->sm_handle);
