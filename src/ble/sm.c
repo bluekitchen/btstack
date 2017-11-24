4816392915a02a434ad01a33c9f9306da5d7957c
Matthias Ringwald <matthias@ringwald.ch>
2017-11-24 16:29:20 +0100

    sm: don't notify client upon new le device entry if entry creation failed

--- src/ble/sm.c
+++ src/ble/sm.c
@@ -1382,9 +1382,9 @@ static void sm_key_distribution_handle_all_received(sm_connection_t * sm_conn){
         le_db_index = le_device_db_add(setup->sm_peer_addr_type, setup->sm_peer_address, setup->sm_peer_irk);
     }
 
-    sm_notify_client_index(SM_EVENT_IDENTITY_CREATED, sm_conn->sm_handle, setup->sm_peer_addr_type, setup->sm_peer_address, le_db_index);
-
     if (le_db_index >= 0){
+
+        sm_notify_client_index(SM_EVENT_IDENTITY_CREATED, sm_conn->sm_handle, setup->sm_peer_addr_type, setup->sm_peer_address, le_db_index);
 
 #ifdef ENABLE_LE_SIGNED_WRITE
         // store local CSRK
