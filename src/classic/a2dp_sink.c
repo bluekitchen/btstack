fbc78aa395a3dbd173f4d0305e8ccc5a346d9354
Milanka Ringwald <mila@ringwald.ch>
2018-04-17 15:28:51 +0200

    a2dp: remove l2cap register service from init

--- src/classic/a2dp_sink.c
+++ src/classic/a2dp_sink.c
@@ -157,7 +157,6 @@ void a2dp_sink_register_media_handler(void (*callback)(uint8_t local_seid, uint8
 
 void a2dp_sink_init(void){
     avdtp_sink_init(&a2dp_sink_context);
-    l2cap_register_service(&packet_handler, BLUETOOTH_PROTOCOL_AVDTP, 0xffff, LEVEL_0);
 }
 
 uint8_t a2dp_sink_create_stream_endpoint(avdtp_media_type_t media_type, avdtp_media_codec_type_t media_codec_type, 
