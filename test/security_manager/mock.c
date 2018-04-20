6894dd39866b65ff3aaedaeed37dd3eb18f285a0
Matthias Ringwald <matthias@ringwald.ch>
2018-04-20 11:10:59 +0200

    test/sm: change to btstack_cyrpto - comment aes_cmac tests, fix test

--- test/security_manager/mock.c
+++ test/security_manager/mock.c
@@ -8,10 +8,9 @@
 #include "hci_dump.h"
 #include "l2cap.h"
 #include "rijndael.h"
-
+#include "btstack_linked_list.h"
 
 static btstack_packet_handler_t le_data_handler;
-static btstack_packet_handler_t event_packet_handler;
 
 static uint8_t packet_buffer[256];
 static uint16_t packet_buffer_len = 0;
@@ -20,6 +19,7 @@ static uint8_t aes128_cyphertext[16];
 
 static hci_connection_t  the_connection;
 static btstack_linked_list_t     connections;
+static btstack_linked_list_t     event_packet_handlers;
 
 void mock_init(void){
 	the_connection.item.next = NULL;
@@ -58,9 +58,12 @@ void aes128_calc_cyphertext(uint8_t key[16], uint8_t plaintext[16], uint8_t cyph
 
 void mock_simulate_hci_event(uint8_t * packet, uint16_t size){
 	hci_dump_packet(HCI_EVENT_PACKET, 1, packet, size);
-	if (event_packet_handler){
-		event_packet_handler(HCI_EVENT_PACKET, 0, packet, size);
-	}
+	btstack_linked_list_iterator_t  it;
+	btstack_linked_list_iterator_init(&it ,&event_packet_handlers);
+    while (btstack_linked_list_iterator_has_next(&it)){
+       	btstack_packet_callback_registration_t * item = (btstack_packet_callback_registration_t *) btstack_linked_list_iterator_next(&it);
+		item->callback(HCI_EVENT_PACKET, 0, packet, size);
+    }
 	if (le_data_handler){
 		le_data_handler(HCI_EVENT_PACKET, 0, packet, size);
 	}
@@ -207,7 +210,7 @@ void l2cap_register_fixed_channel(btstack_packet_handler_t packet_handler, uint1
 }
 
 void hci_add_event_handler(btstack_packet_callback_registration_t * callback_handler){
-	event_packet_handler = callback_handler->callback;
+	btstack_linked_list_add(&event_packet_handlers, (btstack_linked_item_t *) callback_handler);
 }
 
 int l2cap_reserve_packet_buffer(void){
