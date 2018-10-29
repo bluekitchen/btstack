995faa292db215ca122bed722ea5056d014ede23
Matthias Ringwald <matthias@ringwald.ch>
2018-10-29 22:33:01 +0100

    test/crypto: test btstack_crypto ccm

--- test/crypto/aes_ccm_test.c
+++ test/crypto/aes_ccm_test.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 #include <stdint.h>
 #include "btstack_util.h"
-#include "aes_cmac.h"
-#include <errno.h>
 #include "aes_ccm.h"
+#include "btstack_crypto.h"
 
 typedef uint8_t key_t[16];
 
@@ -26,6 +25,10 @@ static int parse_hex(uint8_t * buffer, const char * hex_string){
 	return len;
 }
 
+static void ccm_done(void * arg){
+
+}
+
 static void message_24(void){
 	DEFINE_KEY(encryption_key, "0953fa93e7caac9638f58820220a398e");
 
@@ -37,10 +40,22 @@ static void message_24(void){
 	parse_hex(plaintext, "9736e6a03401de1547118463123e5f6a17b9");
 	printf("%16s: ", "plaintext"); printf_hexdump(plaintext, sizeof(plaintext));
 
+	printf("Reference:\n");
 	uint8_t ciphertext[18+4];
 	bt_mesh_ccm_encrypt(encryption_key, network_nonce, plaintext, sizeof(plaintext), NULL, 0, ciphertext, 4);
 	printf("%16s: ", "ciphertext"); printf_hexdump(ciphertext, 18);
 	printf("%16s: ", "NetMIC");     printf_hexdump(&ciphertext[18], 4);
+
+	// btstack_crypto 
+	printf("btstack_crypto:\n");
+	uint8_t net_mic[4];
+	btstack_crypto_init();
+	btstack_crypto_ccm_t btstack_crypto_ccm;
+	btstack_crypo_ccm_init(&btstack_crypto_ccm, encryption_key, network_nonce, sizeof(plaintext), 4);
+	btstack_crypto_ccm_encrypt_block(&btstack_crypto_ccm, sizeof(plaintext), plaintext, ciphertext, &ccm_done, NULL);
+	btstack_crypo_ccm_get_authentication_value(&btstack_crypto_ccm, net_mic);
+	printf("%16s: ", "ciphertext"); printf_hexdump(ciphertext, 18);
+	printf("%16s: ", "NetMIC");     printf_hexdump(net_mic, 4);
 }
 
 int main(void){
