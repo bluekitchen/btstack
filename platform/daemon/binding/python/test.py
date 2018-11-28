5f5762db78851cd74a94b218e27ac42cd2acf1b3
Matthias Ringwald <matthias@ringwald.ch>
2018-11-28 21:10:06 +0100

    daemon/binding/python: show warning if not running on Python 3

--- platform/daemon/binding/python/test.py
+++ platform/daemon/binding/python/test.py
@@ -1,16 +1,22 @@
 #!/usr/bin/env python3
 
 from btstack import btstack_server, btstack_client, event_factory
+import sys
 
 def packet_handler(packet):
-	global btstack_client
-	if isinstance(packet, event_factory.BTstackEventState):
-		print("BTstack state: %u" % packet.get_state())
-		if packet.get_state() == 2:
-			print('BTstack up and running, starting scan')
-			btstack_client.gap_le_scan_start()
-	if isinstance(packet, event_factory.GAPEventAdvertisingReport):
-		print(packet)
+    global btstack_client
+    if isinstance(packet, event_factory.BTstackEventState):
+        print("BTstack state: %u" % packet.get_state())
+        if packet.get_state() == 2:
+            print('BTstack up and running, starting scan')
+            btstack_client.gap_le_scan_start()
+    if isinstance(packet, event_factory.GAPEventAdvertisingReport):
+        print(packet)
+
+# check version
+if sys.version_info < (3, 0):
+    print('BTstack Server Client library, requires Python 3.x or higher.\n')
+    sys.exit(10)
 
 # Conrtrol for BTstack Server
 btstack_server = btstack_server.BTstackServer()
@@ -26,6 +32,6 @@ btstack_client = btstack_client.BTstackClient()
 # connect to slient, register for HCI packets and power up
 ok = btstack_client.connect()
 if ok:
-	btstack_client.register_packet_handler(packet_handler)
-	btstack_client.btstack_set_power_mode(1)
-	btstack_client.run()
+    btstack_client.register_packet_handler(packet_handler)
+    btstack_client.btstack_set_power_mode(1)
+    btstack_client.run()
