3d3351f352187ace4a1668eb322331de2991769a
Matthias Ringwald <matthias@ringwald.ch>
2019-02-22 22:15:59 +0100

    portaudio: split audio interface

--- port/libusb/main.c
+++ port/libusb/main.c
@@ -170,7 +170,8 @@ int main(int argc, const char * argv[]){
 #endif    
 
 #ifdef HAVE_PORTAUDIO
-    btstack_audio_set_instance(btstack_audio_portaudio_get_instance());
+    btstack_audio_sink_set_instance(btstack_audio_portaudio_sink_get_instance());
+    btstack_audio_source_set_instance(btstack_audio_portaudio_source_get_instance());
 #endif
 
     // inform about BTstack state
