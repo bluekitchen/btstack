1ba35d6a8d2fb81e4a4e8204a6f47f0d376ef7b1
Matthias Ringwald <matthias@ringwald.ch>
2019-03-04 18:05:18 +0100

    stm32-f4discovery-cc256x: use irq priority 0 for Bluetooth UART and both DMA Channels

--- port/stm32-f4discovery-cc256x/eclipse-template/src/dma.c
+++ port/stm32-f4discovery-cc256x/eclipse-template/src/dma.c
@@ -56,7 +56,7 @@ void MX_DMA_Init(void)
 
   /* DMA interrupt init */
   /* DMA1_Stream1_IRQn interrupt configuration */
-  HAL_NVIC_SetPriority(DMA1_Stream1_IRQn, 2, 0);
+  HAL_NVIC_SetPriority(DMA1_Stream1_IRQn, 0, 0);
   HAL_NVIC_EnableIRQ(DMA1_Stream1_IRQn);
   /* DMA1_Stream4_IRQn interrupt configuration */
   HAL_NVIC_SetPriority(DMA1_Stream4_IRQn, 0, 0);
