bluekitchen/btstack

commit 766df38f0f4395fc10e176b5f65986fc2001260d

posix: fix includes in btstack_network_posix.c for freebsd

platform/posix/btstack_network_posix.c
@@ -57,12 +57,10 @@
 #include <string.h>
 #include <unistd.h>
 
-#include <net/if_arp.h>
-
-#ifdef __APPLE__
+#if defined(__APPLE__) || defined(__FreeBSD__)
 #include <net/if.h>
+#include <net/if_arp.h>
 #include <net/if_types.h>
-
 #include <netinet/if_ether.h>
 #include <netinet/in.h>
 #endif
@@ -85,7 +83,7 @@ static uint8_t network_buffer[BNEP_MTU_MIN];
 static size_t  network_buffer_len = 0;
 static char tap_dev_name[16];
 
-#ifdef __APPLE__
+#if defined(__APPLE__) || defined(__FreeBSD__)
 // tuntaposx provides fixed set of tapX devices
 static const char * tap_dev = "/dev/tap0";
 static const char * tap_dev_name_template = "tap0";

commit 4f7ec9205a3818e5445c96bb96e1b110dddc1fc4

posix: fix compile for linux again (regression from 766df38f0f)

platform/posix/btstack_network_posix.c
@@ -51,6 +51,7 @@
 #include <errno.h>
 #include <fcntl.h>
 #include <ifaddrs.h>
+#include <net/if_arp.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -59,7 +60,6 @@
 
 #if defined(__APPLE__) || defined(__FreeBSD__)
 #include <net/if.h>
-#include <net/if_arp.h>
 #include <net/if_types.h>
 #include <netinet/if_ether.h>
 #include <netinet/in.h>