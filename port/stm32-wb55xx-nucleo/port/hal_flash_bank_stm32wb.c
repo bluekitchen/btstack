615cd737fdbd34800286a79c9f2bd35d0f932c83
TREFOU Felix <ftrefou@onewave.fr>
2019-06-07 16:07:53 +0200

    stm32wb_nucleo: Updated headers

--- port/stm32-wb55xx-nucleo/port/hal_flash_bank_stm32wb.c
+++ port/stm32-wb55xx-nucleo/port/hal_flash_bank_stm32wb.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2017 BlueKitchen GmbH
+ * Copyright (C) 2019 BlueKitchen GmbH
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -13,8 +13,11 @@
  * 3. Neither the name of the copyright holders nor the names of
  *    contributors may be used to endorse or promote products derived
  *    from this software without specific prior written permission.
+ * 4. Any redistribution, use, or modification is done solely for
+ *    personal benefit and not for any commercial purpose or for
+ *    monetary gain.
  *
- * THIS SOFTWARE IS PROVIDED BY MATTHIAS RINGWALD AND CONTRIBUTORS
+ * THIS SOFTWARE IS PROVIDED BY BLUEKITCHEN GMBH AND CONTRIBUTORS
  * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
  * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
  * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL MATTHIAS
@@ -27,13 +30,14 @@
  * THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
+ * Please inquire about commercial licensing options at 
+ * contact@bluekitchen-gmbh.com
+ *
  */
 
 /*
- *  hal_flash_bank_stm32.c
- * 
- *  HAL abstraction for Flash memory that can be written anywhere
- *  after being erased
+ *  Made for BlueKitchen by OneWave with <3
+ *      Author: ftrefou@onewave.io
  */
 
 #include <stdint.h>
