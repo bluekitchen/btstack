7846277fa54fd21da9eb70e4d68bc13c0c7bf2e2
Matthias Ringwald <matthias@ringwald.ch>
2019-08-29 21:07:09 +0200

    tool/metric: Makefile to run ccsm and process it, minimal inttypes.h for ccsm

---
+++ tool/metrics/btstack_config.h
@@ -0,0 +1,53 @@
+//
+// btstack_config.h for metric calculation
+//
+
+#ifndef __BTSTACK_CONFIG
+#define __BTSTACK_CONFIG
+
+// Port related features
+#define HAVE_BTSTACK_STDIN
+
+// BTstack features that can be enabled
+#define ENABLE_BLE
+#define ENABLE_CLASSIC
+#define ENABLE_HFP_WIDE_BAND_SPEECH
+#define ENABLE_LE_CENTRAL
+#define ENABLE_LE_PERIPHERAL
+#define ENABLE_LE_SECURE_CONNECTIONS
+#define ENABLE_LE_DATA_CHANNELS
+#define ENABLE_MICRO_ECC_FOR_LE_SECURE_CONNECTIONS
+#define ENABLE_LE_DATA_LENGTH_EXTENSION
+#define ENABLE_ATT_DELAYED_RESPONSE
+#define ENABLE_LOG_ERROR
+#define ENABLE_LOG_INFO 
+#define ENABLE_SCO_OVER_HCI
+#define ENABLE_SDP_DES_DUMP
+#define ENABLE_L2CAP_ENHANCED_RETRANSMISSION_MODE
+
+// BTstack configuration. buffers, sizes, ...
+#define HCI_ACL_PAYLOAD_SIZE (1691 + 4)
+#define HCI_INCOMING_PRE_BUFFER_SIZE 14 // sizeof BNEP header, avoid memcpy
+
+#define MAX_NR_LE_DEVICE_DB_ENTRIES    4
+
+#define NVM_NUM_DEVICE_DB_ENTRIES     10
+
+// Mesh Configuration
+#define ENABLE_MESH
+#define ENABLE_MESH_ADV_BEARER
+#define ENABLE_MESH_GATT_BEARER
+#define ENABLE_MESH_PB_ADV
+#define ENABLE_MESH_PB_GATT
+#define ENABLE_MESH_PROXY_SERVER
+#define ENABLE_MESH_PROVISIONER
+
+#define MAX_NR_MESH_TRANSPORT_KEYS    16
+#define MAX_NR_MESH_VIRTUAL_ADDRESSES 16
+#define MAX_NR_MESH_SUBNETS            2
+
+// allow for one NetKey update
+#define MAX_NR_MESH_NETWORK_KEYS      (MAX_NR_MESH_SUBNETS+1)
+
+#endif
+
