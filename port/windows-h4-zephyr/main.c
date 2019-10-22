ae1c72f5b6a46aa684b4f3024289791accbd6bcc
Matthias Ringwald <matthias@ringwald.ch>
2019-10-22 10:08:50 +0200

    windows-h4-zephyr: remove unused include

--- port/windows-h4-zephyr/main.c
+++ port/windows-h4-zephyr/main.c
@@ -53,7 +53,6 @@
 
 #include "btstack_debug.h"
 #include "btstack_event.h"
-#include "btstack_link_key_db_fs.h"
 #include "btstack_memory.h"
 #include "btstack_run_loop.h"
 #include "btstack_run_loop_windows.h"
