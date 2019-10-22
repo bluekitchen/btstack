4eb8f7a7eb585138710161fd8dac77b9b62ba560
Milanka Ringwald <mila@ringwald.ch>
2019-10-22 14:55:07 +0200

    map_client: fix compile

--- test/map_client/btstack_config.h
+++ test/map_client/btstack_config.h
@@ -33,5 +33,6 @@
 // BTstack configuration. buffers, sizes, ...
 #define HCI_ACL_PAYLOAD_SIZE (1691 + 4)
 #define HCI_INCOMING_PRE_BUFFER_SIZE 14 // sizeof BNEP header, avoid memcpy
+#define NVM_NUM_DEVICE_DB_ENTRIES      20
 
 #endif
