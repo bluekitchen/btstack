78764d994a2c70940f6b87b80fb7a5621da9f1f7
Matthias Ringwald <matthias@ringwald.ch>
2020-02-25 12:42:23 +0100

    renesas-tb-s1ja: implement hal_flash_bank_synergy

---
+++ port/renesas-tb-s1ja-cc256x/template/btstack_example/synergy_cfg/ssp_cfg/driver/r_flash_cfg.h
@@ -0,0 +1,6 @@
+/* generated configuration header file - do not edit */
+#ifndef R_FLASH_CFG_H_
+#define R_FLASH_CFG_H_
+#define FLASH_CFG_PARAM_CHECKING_ENABLE (BSP_CFG_PARAM_CHECKING_ENABLE)
+#define FLASH_CFG_PARAM_CODE_FLASH_PROGRAMMING_ENABLE (0)
+#endif /* R_FLASH_CFG_H_ */
