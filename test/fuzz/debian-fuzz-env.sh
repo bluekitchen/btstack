245302af3cb01a30169912db6dbd07a1abe94033
Matthias Ringwald <matthias@ringwald.ch>
2020-04-01 10:32:44 +0200

    test/fuzz: create image for libfuzz if needed

--- test/fuzz/debian-fuzz-env.sh
+++ test/fuzz/debian-fuzz-env.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
+set -e
+
 DIR=`dirname $0`
 BTSTACK_ROOT="/Projects/btstack/"
-# call to build image
-# docker image build -t fuzz .
+
+# build image if it doesn't exist
+if [[ "$(docker images -q fuzz 2> /dev/null)" == "" ]]; then
+  	echo "Image for libfuzz does not exist, creating it now..."
+	docker image build --no-cache -t fuzz .
+fi
+
+# enter fuzz container and bring repo along
 docker run --rm -ti -v $BTSTACK_ROOT:/btstack  -w /btstack/test/fuzz fuzz
 
