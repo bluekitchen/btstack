bluekitchen/btstack

commit 3251a10887e6e0a8d4015b375a0ab472839b09e5

gap: fixed gap_set_scan_parameters() if issued right before gap_start_scan() - issue #275

src/hci.c
@@ -3413,17 +3413,22 @@ static void hci_run(void){
     && ((hci_stack->le_own_addr_type == BD_ADDR_TYPE_LE_PUBLIC) || hci_stack->le_random_address_set)){
 
 #ifdef ENABLE_LE_CENTRAL
-        // handle le scan
-        if ((hci_stack->le_scanning_enabled != hci_stack->le_scanning_active)){
-            hci_stack->le_scanning_active = hci_stack->le_scanning_enabled;
-            hci_send_cmd(&hci_le_set_scan_enable, hci_stack->le_scanning_enabled, 0);
+        // parameter change requires scanning to be stopped first
+        if (hci_stack->le_scan_type != 0xff) {
+            if (hci_stack->le_scanning_active){
+                hci_stack->le_scanning_active = 0;
+                hci_send_cmd(&hci_le_set_scan_enable, 0, 0);
+            } else {
+                int scan_type = (int) hci_stack->le_scan_type;
+                hci_stack->le_scan_type = 0xff;
+                hci_send_cmd(&hci_le_set_scan_parameters, scan_type, hci_stack->le_scan_interval, hci_stack->le_scan_window, hci_stack->le_own_addr_type, 0);
+            }
             return;
         }
-        if (hci_stack->le_scan_type != 0xff){
-            // defaults: active scanning, accept all advertisement packets
-            int scan_type = hci_stack->le_scan_type;
-            hci_stack->le_scan_type = 0xff;
-            hci_send_cmd(&hci_le_set_scan_parameters, scan_type, hci_stack->le_scan_interval, hci_stack->le_scan_window, hci_stack->le_own_addr_type, 0);
+        // finally, we can enable/disable le scan
+        if ((hci_stack->le_scanning_enabled && !hci_stack->le_scanning_active)){
+            hci_stack->le_scanning_active = hci_stack->le_scanning_enabled;
+            hci_send_cmd(&hci_le_set_scan_enable, hci_stack->le_scanning_enabled, 0);
             return;
         }
 #endif

commit 787e73a54d891ae70263abdfbcf486b2e33e67c1

hci: fix gap_stop_scan - regression from 3251a10

src/hci.c
@@ -3434,7 +3434,7 @@ static void hci_run(void){
             return;
         }
         // finally, we can enable/disable le scan
-        if ((hci_stack->le_scanning_enabled && !hci_stack->le_scanning_active)){
+        if ((hci_stack->le_scanning_enabled != hci_stack->le_scanning_active)){
             hci_stack->le_scanning_active = hci_stack->le_scanning_enabled;
             hci_send_cmd(&hci_le_set_scan_enable, hci_stack->le_scanning_enabled, 0);
             return;