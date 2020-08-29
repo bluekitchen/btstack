398e1682e3cc0ebd4666bfd694d84b24be8a04ea
Matthias Ringwald <matthias@ringwald.ch>
2020-08-29 22:44:47 +0200

    test/auto-pts: skip chipset/zephyr

--- test/auto-pts/coverage.sh
+++ test/auto-pts/coverage.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-# collect traces from auto-pts run
-lcov --capture --rc lcov_branch_coverage=1 --directory . --exclude "/Applications/*" --exclude "/Library/*" --exclude "/usr/*" --exclude "*/test/*" --output-file coverage-pts.info
+# collect traces from auto-pts run (exclude chipset/zephyr)
+lcov --capture --rc lcov_branch_coverage=1 --directory . --exclude "/Applications/*" --exclude "/Library/*" --exclude "/usr/*" --exclude "*/test/*"  --exclude "*/chipset/*" --output-file coverage-pts.info
 
 # optional: download unit tests, merge and filter results like in ../Makefile
 
