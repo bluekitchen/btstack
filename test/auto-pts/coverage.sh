3011ca4a9a99eb205bc09ae3236780a25c6e6ace
Matthias Ringwald <matthias@ringwald.ch>
2020-09-18 18:17:42 +0200

    test/coverage: fix paths

--- test/auto-pts/coverage.sh
+++ test/auto-pts/coverage.sh
@@ -3,9 +3,6 @@
 # collect traces from auto-pts run (exclude chipset/zephyr)
 lcov --capture --rc lcov_branch_coverage=1 --directory . --exclude "/Applications/*" --exclude "/Library/*" --exclude "/usr/*" --exclude "*/test/*"  --exclude "*/chipset/*" --output-file coverage-pts.info
 
-# strip path prefix such that paths start with 'btstack'
-sed -i.bak -e 's|/Users/mringwal/buildbot-worker/auto-pts/||' coverage-pts.info
-
 # optional: download unit tests, merge and filter results like in ../Makefile
 
 # generate html output
