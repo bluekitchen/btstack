a921db493845ca4f3e5356bec3efb2b23208c8fa
Matthias Ringwald <matthias@ringwald.ch>
2020-09-21 17:40:21 +0200

    test/security_manager_sc: compile without classic

---
+++ test/security_manager_sc/btstack_config.h
@@ -0,0 +1,37 @@
+//
+// btstack_config.h for security manager sc live test based on libusb port //
+
+#ifndef BTSTACK_CONFIG
+#define BTSTACK_CONFIG
+
+// Port related features
+#define HAVE_MALLOC
+#define HAVE_POSIX_FILE_IO
+#define HAVE_BTSTACK_STDIN
+#define HAVE_POSIX_TIME
+
+// BTstack features that can be enabled
+#define ENABLE_BLE
+#define ENABLE_LE_CENTRAL
+#define ENABLE_LE_PERIPHERAL
+#define ENABLE_LE_SECURE_CONNECTIONS
+#define ENABLE_LE_DATA_CHANNELS
+#define ENABLE_MICRO_ECC_FOR_LE_SECURE_CONNECTIONS
+#define ENABLE_LE_DATA_LENGTH_EXTENSION
+#define ENABLE_LE_PRIVACY_ADDRESS_RESOLUTION
+#define ENABLE_ATT_DELAYED_RESPONSE
+#define ENABLE_LOG_ERROR
+#define ENABLE_LOG_INFO 
+// #define ENABLE_SOFTWARE_AES128
+
+// BTstack configuration. buffers, sizes, ...
+#define HCI_ACL_PAYLOAD_SIZE (1691 + 4)
+#define HCI_INCOMING_PRE_BUFFER_SIZE 14 // sizeof BNEP header, avoid memcpy
+
+#define NVM_NUM_DEVICE_DB_ENTRIES      16
+
+// allow for one NetKey update
+#define MAX_NR_MESH_NETWORK_KEYS      (MAX_NR_MESH_SUBNETS+1)
+
+#endif
+
