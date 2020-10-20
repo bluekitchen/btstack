99c44ab2c768e001ead3f3cff85a05be25b0494c
Matthias Ringwald <matthias@ringwald.ch>
2020-10-20 18:03:19 +0200

    sm_pairing_peripheral: fix IO Capabilities for Secure Connections with Just Works

--- example/sm_pairing_peripheral.c
+++ example/sm_pairing_peripheral.c
@@ -113,12 +113,12 @@ static void sm_peripheral_setup(void){
     // sm_set_secure_connections_only_mode(true);
 
     // LE Secure Connections, Just Works
-    // sm_set_io_capabilities(IO_CAPABILITY_DISPLAY_YES_NO);
+    // sm_set_io_capabilities(IO_CAPABILITY_NO_INPUT_NO_OUTPUT);
     // sm_set_authentication_requirements(SM_AUTHREQ_SECURE_CONNECTION);
 
     // LE Secure Connections, Numeric Comparison
-    sm_set_io_capabilities(IO_CAPABILITY_DISPLAY_YES_NO);
-    sm_set_authentication_requirements(SM_AUTHREQ_SECURE_CONNECTION|SM_AUTHREQ_MITM_PROTECTION);
+    // sm_set_io_capabilities(IO_CAPABILITY_DISPLAY_YES_NO);
+    // sm_set_authentication_requirements(SM_AUTHREQ_SECURE_CONNECTION|SM_AUTHREQ_MITM_PROTECTION);
 
     // LE Secure Pairing, Passkey entry initiator enter, responder (us) displays
     // sm_set_io_capabilities(IO_CAPABILITY_DISPLAY_ONLY);
