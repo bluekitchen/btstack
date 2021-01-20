fdd788fee88e5a9a85c930999195c7869b67b73d
Matthias Ringwald <matthias@ringwald.ch>
2021-01-20 17:26:07 +0100

    avdtp: move helper to set/get configuration into avdtp_util

--- src/classic/avdtp_util.h
+++ src/classic/avdtp_util.h
@@ -117,6 +117,21 @@ void a2dp_replace_subevent_id_and_emit_cmd(btstack_packet_handler_t callback, ui
 
 void a2dp_emit_stream_event(btstack_packet_handler_t callback, uint16_t cid, uint8_t local_seid, uint8_t subevent_id);
 
+
+// helper to set/get configuration
+void avdtp_config_sbc_set_sampling_frequency(uint8_t * config, uint16_t sampling_frequency_hz);
+void avdtp_config_sbc_store(uint8_t * config, uint16_t sampling_frequency_hz, avdtp_channel_mode_t channel_mode, uint8_t block_length, uint8_t subbands,
+                            avdtp_sbc_allocation_method_t  allocation_method, uint8_t min_bitpool_value, uint8_t max_bitpool_value);
+void avdtp_config_mpeg_audio_set_sampling_frequency(uint8_t * config, uint16_t sampling_frequency_hz);
+void avdtp_config_mpeg_audio_store(uint8_t * config, avdtp_mpeg_layer_t layer, uint8_t crc, avdtp_channel_mode_t channel_mode, uint8_t media_payload_format,
+                                   uint16_t sampling_frequency, uint8_t vbr, uint8_t bit_rate_index);
+void avdtp_config_mpeg_aac_set_sampling_frequency(uint8_t * config, uint16_t sampling_frequency_hz);
+void avdtp_config_mpeg_aac_store(uint8_t * config,  avdtp_aac_object_type_t object_type, uint32_t sampling_frequency, uint8_t channels, uint32_t bit_rate, uint8_t vbr);
+void avdtp_config_atrac_set_sampling_frequency(uint8_t * config, uint16_t sampling_frequency_hz);
+void avdtp_config_atrac_store(uint8_t * config, avdtp_atrac_version_t version, avdtp_channel_mode_t channel_mode, uint16_t sampling_frequency, uint8_t vbr,
+                              uint8_t bit_rate_index, uint16_t maximum_sul);
+
+
 #if defined __cplusplus
 }
 #endif
