3519b7439e78069d2d1570481ce897067d5d6a7f
Matthias Ringwald <matthias@ringwald.ch>
2021-04-08 11:32:52 +0200

    test/auto-pts: fix build

--- test/auto-pts/main.c
+++ test/auto-pts/main.c
@@ -61,7 +61,9 @@
 #include "btstack_run_loop_posix.h"
 #include "hal_led.h"
 #include "hci.h"
+#include "hci_transport_usb.h"
 #include "hci_dump.h"
+#include "hci_dump_posix_fs.h"
 #include "btstack_stdin.h"
 #include "btstack_audio.h"
 #include "btstack_tlv_posix.h"
@@ -216,7 +218,9 @@ int main(int argc, const char * argv[]){
     }
     strcat(pklg_path, ".pklg");
     printf("Packet Log: %s\n", pklg_path);
-    hci_dump_init(pklg_path, hci_dump_posix);
+    hci_dump_posix_fs_open(pklg_path, HCI_DUMP_PACKETLOGGER);
+    const hci_dump_t * hci_dump_impl = hci_dump_posix_fs_get_instance();
+    hci_dump_init(hci_dump_impl);
 
     // init HCI
 	hci_init(hci_transport_usb_instance(), NULL);
