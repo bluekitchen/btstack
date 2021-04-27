043de24a5a9bca0d33d71f6d0c15aaf684004ae4
Matthias Ringwald <matthias@ringwald.ch>
2021-04-27 09:39:00 +0200

    segger-rtt: set default to no block skip mode

--- 3rd-party/segger-rtt/SEGGER_RTT_Conf.h
+++ 3rd-party/segger-rtt/SEGGER_RTT_Conf.h
@@ -82,7 +82,7 @@ Revision: $Rev: 15929 $
 
 #define SEGGER_RTT_PRINTF_BUFFER_SIZE             (64u)    // Size of buffer for RTT printf to bulk-send chars via RTT     (Default: 64)
 
-#define SEGGER_RTT_MODE_DEFAULT                   SEGGER_RTT_MODE_BLOCK_IF_FIFO_FULL // Mode for pre-initialized terminal channel (buffer 0)
+#define SEGGER_RTT_MODE_DEFAULT                   SEGGER_RTT_MODE_NO_BLOCK_SKIP // Mode for pre-initialized terminal channel (buffer 0)
 
 /*********************************************************************
 *
