aaeb55ed5ded2b76c7a629855ccbd1e702280e44
Milanka Ringwald <mila@ringwald.ch>
2021-05-21 14:02:07 +0200

    test/pts: avrcp update test sequences
    # Conflicts:
    #	test/pts/avrcp_test.c

--- test/pts/avrcp_test.c
+++ test/pts/avrcp_test.c
@@ -75,8 +75,14 @@ static const char * device_addr_string = "00:1B:DC:08:E2:5C";
 static bd_addr_t device_addr;
 static bd_addr_t device_addr_browsing;
 
-static const uint8_t fragmented_message[] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9,  0, 1, 2, 3, 4, 5, 6, 7, 8, 9,  0, 1, 2, 3, 4, 5, 6, 7, 8, 9,  
-    0, 1, 2, 3, 4, 5, 6, 7, 8, 9,  0, 1, 2, 3, 4, 5, 6, 7, 8, 9,  0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+static const uint8_t fragmented_message[] = { 
+    0, 1, 2, 3, 4, 5, 6, 7, 8, 9,  
+    10, 11, 12, 13, 14, 15, 16, 17, 18, 19,  
+    20, 21, 22, 23, 24, 25, 26, 27, 28, 29,
+    30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 
+    40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 
+    50, 51, 52, 53, 54, 55, 56, 57, 58, 59 
+};
 
 typedef enum {
     AVRCP_BROWSING_STATE_IDLE,
