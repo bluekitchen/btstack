47f52328508220464867e8659183296aa3040071
Matthias Ringwald <matthias@ringwald.ch>
2021-05-27 16:55:56 +0200

    doc: prefix commit hash with branch name

--- doc/manual/update_getting_started.sh
+++ doc/manual/update_getting_started.sh
@@ -3,11 +3,12 @@
 # get commit and tag
 tag=`git tag --points-at HEAD`
 commit=`git rev-parse --short HEAD`
+branch=`git branch | sed -n -e 's/^\* \(.*\)/\1/p'`
 
 # use tag if available
 if [ -z "$tag" ]
 then
-  version="Commit $commit"
+  version="$branch-$commit"
 else
   version="Version $tag ($commit)"
 fi
