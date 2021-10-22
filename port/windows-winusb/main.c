0217d68be6118bb225f0369aaffa88adb0e5b1b9
U-ASRMICRO\maoliuhu <maoliuhu@HF-PC061.asrmicro.com>
2021-10-22 12:57:36 +0800

    fix bug that segment fault occur when run example in windows-winusb and windows-winusb-intel

--- port/windows-winusb/main.c
+++ port/windows-winusb/main.c
@@ -64,7 +64,7 @@
 #include "hci_dump.h"
 #include "hci_dump_posix_fs.h"
 #include "hci_transport.h"
-#include "hci_transport_h4.h"
+#include "hci_transport_usb.h"
 #include "btstack_stdin.h"
 
 int btstack_main(int argc, const char * argv[]);
