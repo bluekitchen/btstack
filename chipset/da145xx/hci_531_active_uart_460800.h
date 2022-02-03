58b031dbaf6dd1553e52200e4ccb3081bed5458d
Matthias Ringwald <matthias@ringwald.ch>
2022-02-03 22:48:42 +0100

    da145xx: add fw for da14531

---
+++ chipset/da145xx/hci_531_active_uart_460800.h
@@ -0,0 +1,15 @@
+
+/** 
+ * hci_531_active_uart_460800.h converted from hci_531_active_uart_460800.hex 
+ */
+
+#ifndef hci_531_active_uart_460800_H
+#define hci_531_active_uart_460800_H
+
+#include <stdint.h>
+
+extern const uint8_t  da145xx_fw_data[];
+extern const uint32_t da145xx_fw_size;
+extern const char *   da145xx_fw_name;
+
+#endif
