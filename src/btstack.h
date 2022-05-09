fff175580fdb2f78e1581c1795fd609b0cdae09f
Matthias Ringwald <matthias@ringwald.ch>
2022-05-09 22:14:44 +0200

    btstack: add pbap.h to user header files

--- src/btstack.h
+++ src/btstack.h
@@ -142,6 +142,7 @@
 #include "classic/hsp_hs.h"
 #include "classic/obex.h"
 #include "classic/pan.h"
+#include "classic/pbap.h"
 #include "classic/pbap_client.h"
 #include "classic/rfcomm.h"
 #include "classic/sdp_client.h"
