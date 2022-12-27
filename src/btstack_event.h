a30bfae77c00069c21c74eb63eb9b45f08c61ff3
Milanka Ringwald <mila@ringwald.ch>
2022-12-27 18:10:59 +0100

    btstack_event: add CSIS server connected/disconnected events
    # Conflicts:
    #	src/btstack_event.h

--- src/btstack_event.h
+++ src/btstack_event.h
@@ -16537,6 +16537,44 @@ static inline hci_con_handle_t gattservice_subevent_csis_coordinator_disconnecte
 }
 
 /**
+ * @brief Get field con_handle from event GATTSERVICE_SUBEVENT_CSIS_REMOTE_SERVER_CONNECTED
+ * @param event packet
+ * @return con_handle
+ * @note: btstack_type H
+ */
+static inline hci_con_handle_t gattservice_subevent_csis_remote_server_connected_get_con_handle(const uint8_t * event){
+    return little_endian_read_16(event, 3);
+}
+/**
+ * @brief Get field pacs_cid from event GATTSERVICE_SUBEVENT_CSIS_REMOTE_SERVER_CONNECTED
+ * @param event packet
+ * @return pacs_cid
+ * @note: btstack_type 2
+ */
+static inline uint16_t gattservice_subevent_csis_remote_server_connected_get_pacs_cid(const uint8_t * event){
+    return little_endian_read_16(event, 5);
+}
+/**
+ * @brief Get field status from event GATTSERVICE_SUBEVENT_CSIS_REMOTE_SERVER_CONNECTED
+ * @param event packet
+ * @return status
+ * @note: btstack_type 1
+ */
+static inline uint8_t gattservice_subevent_csis_remote_server_connected_get_status(const uint8_t * event){
+    return event[7];
+}
+
+/**
+ * @brief Get field pacs_cid from event GATTSERVICE_SUBEVENT_CSIS_REMOTE_SERVER_DISCONNECTED
+ * @param event packet
+ * @return pacs_cid
+ * @note: btstack_type 2
+ */
+static inline uint16_t gattservice_subevent_csis_remote_server_disconnected_get_pacs_cid(const uint8_t * event){
+    return little_endian_read_16(event, 3);
+}
+
+/**
  * @brief Get field con_handle from event GATTSERVICE_SUBEVENT_BASS_SERVER_CONNECTED
  * @param event packet
  * @return con_handle
