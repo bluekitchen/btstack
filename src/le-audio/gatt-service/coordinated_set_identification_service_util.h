0e29a064cdaa0223a6d9dbdaa086308c0125d3da
Milanka Ringwald <mila@ringwald.ch>
2022-12-27 18:13:40 +0100

    gatt-service/csis_util: extract util file

--- src/le-audio/gatt-service/coordinated_set_identification_service_util.h
+++ src/le-audio/gatt-service/coordinated_set_identification_service_util.h
@@ -40,12 +40,11 @@
  * 
  */
 
-#ifndef COORDINATED_SET_IDENTIFICATION_SERVICE_SERVER_H
-#define COORDINATED_SET_IDENTIFICATION_SERVICE_SERVER_H
+#ifndef COORDINATED_SET_IDENTIFICATION_SERVICE_UTIL_H
+#define COORDINATED_SET_IDENTIFICATION_SERVICE_UTIL_H
 
 #include <stdint.h>
 #include "le-audio/le_audio.h"
-#include "le-audio/gatt-service/coordinated_set_identification_service_util.h"
 
 #if defined __cplusplus
 extern "C" {
@@ -58,58 +57,25 @@ extern "C" {
  * 
  * To use with your application, add `#import <coordinated_set_identification_service.gatt>` to your .gatt file. 
  */
+#define CSIS_LOCK_TIMEOUT_MS            60000
+
+#define CSIS_LOCK_DENIED                0x80
+#define CSIS_LOCK_RELEASE_NOT_ALLOWED   0x81
+#define CSIS_INVALID_LOCK_VALUE         0x82
+#define CSIS_OOB_SIRK_ONLY              0x83
+#define CSIS_LOCK_ALREADY_GRANTED       0x84
 
 typedef enum {
-    CSIS_ENCRYPTED_SIRK_CALCULATION_STATE_IDLE = 0x00,
-    CSIS_ENCRYPTED_SIRK_CALCULATION_W2_START,
-    CSIS_ENCRYPTED_SIRK_CALCULATION_ACTIVE,
-    CSIS_ENCRYPTED_SIRK_CALCULATION_STATE_READY
-} csis_encrypted_sirk_calculation_state_t;
+    CSIS_MEMBER_UNLOCKED = 0x01,
+    CSIS_MEMBER_LOCKED,
+    CSIS_MEMBER_PROHIBITED
+} csis_member_lock_t;
 
-typedef struct {
-    uint16_t con_handle;
-
-    uint16_t sirk_configuration;
-    uint16_t member_lock_configuration;
-    uint16_t coordinated_set_size_configuration;
-
-    bool     is_lock_owner;
-    btstack_timer_source_t lock_timer;
-
-    uint8_t  scheduled_tasks;
-    btstack_context_callback_registration_t  scheduled_tasks_callback;
-
-    csis_encrypted_sirk_calculation_state_t  encrypted_sirk_state;
-    uint8_t encrypted_sirk[16];
-
-} csis_coordinator_t;
-/**
- * @brief Init Coordinated Set Identification Service Server with ATT DB
- */
-void coordinated_set_identification_service_server_init(const uint8_t coordinators_num, csis_coordinator_t * coordinators, 
-    uint8_t coordinated_set_size, uint8_t member_rank);
-
-uint8_t coordinated_set_identification_service_server_set_sirk(csis_sirk_type_t type, uint8_t * sirk, bool exposed_via_oob);
-
-uint8_t coordinated_set_identification_service_server_set_size(uint8_t coordinated_set_size);
-
-uint8_t coordinated_set_identification_service_server_set_rank(uint8_t member_rank);
-
-void coordinated_set_identification_service_server_calculate_rsi(void);
-
-/**
- * @brief Register callback.
- * @param callback
- */
-void coordinated_set_identification_service_server_register_packet_handler(btstack_packet_handler_t callback);
-
-void coordinated_set_identification_service_server_deinit(void);
-
-/* API_END */
-
-// PTS test only
-uint8_t coordinated_set_identification_service_server_simulate_member_connected(hci_con_handle_t con_handle);
-uint8_t coordinated_set_identification_service_server_simulate_set_lock(hci_con_handle_t con_handle, csis_member_lock_t lock);
+typedef enum {
+    CSIS_SIRK_TYPE_ENCRYPTED = 0x00,
+    CSIS_SIRK_TYPE_PUBLIC,
+    CSIS_SIRK_TYPE_PROHIBITED
+} csis_sirk_type_t;
 
 #if defined __cplusplus
 }
