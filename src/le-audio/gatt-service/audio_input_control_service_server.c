c0e3584f681a4f286ca296282a7a8bce35597b23
Milanka Ringwald <mila@ringwald.ch>
2023-01-27 10:32:33 +0100

    gatt-service/aics_server: rename vars

--- src/le-audio/gatt-service/audio_input_control_service_server.c
+++ src/le-audio/gatt-service/audio_input_control_service_server.c
@@ -71,53 +71,53 @@ static audio_input_control_service_server_t * aics_find_service_for_attribute_ha
 
 static uint16_t aics_read_callback(hci_con_handle_t con_handle, uint16_t attribute_handle, uint16_t offset, uint8_t * buffer, uint16_t buffer_size){
     UNUSED(con_handle);
-    audio_input_control_service_server_t * aics = aics_find_service_for_attribute_handle(attribute_handle);
-    if (aics == NULL){
+    audio_input_control_service_server_t * client = aics_find_service_for_attribute_handle(attribute_handle);
+    if (client == NULL){
         return 0;
     }
 
-    if (attribute_handle == aics->audio_input_state_value_handle){
+    if (attribute_handle == client->audio_input_state_value_handle){
         uint8_t value[4];
-        value[0] = (uint8_t)aics->info->audio_input_state.gain_setting_db;
-        value[1] = aics->info->audio_input_state.mute_mode;
-        value[2] = aics->info->audio_input_state.gain_mode;
-        value[3] = aics->audio_input_state_change_counter;
+        value[0] = (uint8_t)client->info->audio_input_state.gain_setting_db;
+        value[1] = client->info->audio_input_state.mute_mode;
+        value[2] = client->info->audio_input_state.gain_mode;
+        value[3] = client->audio_input_state_change_counter;
         return att_read_callback_handle_blob(value, sizeof(value), offset, buffer, buffer_size);
     }
 
 
-    if (attribute_handle == aics->gain_settings_properties_value_handle){
+    if (attribute_handle == client->gain_settings_properties_value_handle){
         uint8_t value[3];
 
-        value[0] = aics->info->gain_settings_properties.gain_settings_units;
-        value[1] = (uint8_t)aics->info->gain_settings_properties.gain_settings_minimum;
-        value[2] = (uint8_t)aics->info->gain_settings_properties.gain_settings_maximum;
+        value[0] = client->info->gain_settings_properties.gain_settings_units;
+        value[1] = (uint8_t)client->info->gain_settings_properties.gain_settings_minimum;
+        value[2] = (uint8_t)client->info->gain_settings_properties.gain_settings_maximum;
         return att_read_callback_handle_blob(value, sizeof(value), offset, buffer, buffer_size);
     }
 
-    if (attribute_handle == aics->audio_input_type_value_handle){
-        return att_read_callback_handle_byte((uint8_t)aics->info->audio_input_type, offset, buffer, buffer_size);
+    if (attribute_handle == client->audio_input_type_value_handle){
+        return att_read_callback_handle_byte((uint8_t)client->info->audio_input_type, offset, buffer, buffer_size);
     }
 
-    if (attribute_handle == aics->audio_input_status_value_handle){
-        return att_read_callback_handle_byte((uint8_t)aics->audio_input_status, offset, buffer, buffer_size);
+    if (attribute_handle == client->audio_input_status_value_handle){
+        return att_read_callback_handle_byte((uint8_t)client->audio_input_status, offset, buffer, buffer_size);
     }
 
-    if (attribute_handle == aics->audio_input_description_value_handle){
-        return att_read_callback_handle_blob((uint8_t *)aics->info->audio_input_description, aics->audio_input_description_len, offset, buffer, buffer_size);
+    if (attribute_handle == client->audio_input_description_value_handle){
+        return att_read_callback_handle_blob((uint8_t *)client->info->audio_input_description, client->audio_input_description_len, offset, buffer, buffer_size);
     }
     
     
-    if (attribute_handle == aics->audio_input_state_client_configuration_handle){
-        return att_read_callback_handle_little_endian_16(aics->audio_input_state_client_configuration, offset, buffer, buffer_size);
+    if (attribute_handle == client->audio_input_state_client_configuration_handle){
+        return att_read_callback_handle_little_endian_16(client->audio_input_state_client_configuration, offset, buffer, buffer_size);
     }
     
-    if (attribute_handle == aics->audio_input_status_client_configuration_handle){
-        return att_read_callback_handle_little_endian_16(aics->audio_input_status_client_configuration, offset, buffer, buffer_size);
+    if (attribute_handle == client->audio_input_status_client_configuration_handle){
+        return att_read_callback_handle_little_endian_16(client->audio_input_status_client_configuration, offset, buffer, buffer_size);
     }
     
-    if (attribute_handle == aics->audio_input_description_client_configuration_handle){
-        return att_read_callback_handle_little_endian_16(aics->audio_input_description_client_configuration, offset, buffer, buffer_size);
+    if (attribute_handle == client->audio_input_description_client_configuration_handle){
+        return att_read_callback_handle_little_endian_16(client->audio_input_description_client_configuration, offset, buffer, buffer_size);
     }
 
     return 0;
@@ -269,12 +269,12 @@ static int aics_write_callback(hci_con_handle_t con_handle, uint16_t attribute_h
     UNUSED(transaction_mode);
     UNUSED(offset);
     
-    audio_input_control_service_server_t * aics = aics_find_service_for_attribute_handle(attribute_handle);
-    if (aics == NULL){
+    audio_input_control_service_server_t * client = aics_find_service_for_attribute_handle(attribute_handle);
+    if (client == NULL){
         return 0;
     }
 
-    if (attribute_handle == aics->audio_input_control_value_handle){
+    if (attribute_handle == client->audio_input_control_value_handle){
         if (buffer_size == 0){
             return AICS_ERROR_CODE_OPCODE_NOT_SUPPORTED;
         }
@@ -285,7 +285,7 @@ static int aics_write_callback(hci_con_handle_t con_handle, uint16_t attribute_h
         }
         uint8_t change_counter = buffer[1];
 
-        if (change_counter != aics->audio_input_state_change_counter){
+        if (change_counter != client->audio_input_state_change_counter){
             return AICS_ERROR_CODE_INVALID_CHANGE_COUNTER;
         }
 
@@ -295,14 +295,14 @@ static int aics_write_callback(hci_con_handle_t con_handle, uint16_t attribute_h
                     return AICS_ERROR_CODE_VALUE_OUT_OF_RANGE;
                 }
                 
-                if (!audio_input_control_service_server_set_gain(aics, (int8_t)buffer[2])) {
+                if (!audio_input_control_service_server_set_gain(client, (int8_t)buffer[2])) {
                     return AICS_ERROR_CODE_VALUE_OUT_OF_RANGE;
                 }
 
-                switch (aics->info->audio_input_state.gain_mode){
+                switch (client->info->audio_input_state.gain_mode){
                     case AICS_GAIN_MODE_MANUAL_ONLY:
                     case AICS_GAIN_MODE_MANUAL:
-                        aics_server_emit_gain(aics);
+                        aics_server_emit_gain(client);
                         break;
                     default:
                         break;
@@ -310,13 +310,13 @@ static int aics_write_callback(hci_con_handle_t con_handle, uint16_t attribute_h
                 break;
 
             case AICS_OPCODE_UMUTE:
-                switch (aics->info->audio_input_state.mute_mode){
+                switch (client->info->audio_input_state.mute_mode){
                     case AICS_MUTE_MODE_DISABLED:
-                        aics_server_emit_mute_mode(aics);
+                        aics_server_emit_mute_mode(client);
                         return AICS_ERROR_CODE_MUTE_DISABLED;
                     case AICS_MUTE_MODE_MUTED:
-                        aics->info->audio_input_state.mute_mode = AICS_MUTE_MODE_NOT_MUTED;
-                        aics_server_emit_mute_mode(aics);
+                        client->info->audio_input_state.mute_mode = AICS_MUTE_MODE_NOT_MUTED;
+                        aics_server_emit_mute_mode(client);
                         break;
                     default:
                         break;
@@ -324,13 +324,13 @@ static int aics_write_callback(hci_con_handle_t con_handle, uint16_t attribute_h
                 break;
 
             case AICS_OPCODE_MUTE:
-                switch (aics->info->audio_input_state.mute_mode){
+                switch (client->info->audio_input_state.mute_mode){
                     case AICS_MUTE_MODE_DISABLED:
-                        aics_server_emit_mute_mode(aics);
+                        aics_server_emit_mute_mode(client);
                         return AICS_ERROR_CODE_MUTE_DISABLED;
                     case AICS_MUTE_MODE_NOT_MUTED:
-                        aics->info->audio_input_state.mute_mode = AICS_MUTE_MODE_MUTED;
-                        aics_server_emit_mute_mode(aics);
+                        client->info->audio_input_state.mute_mode = AICS_MUTE_MODE_MUTED;
+                        aics_server_emit_mute_mode(client);
                         break;
                     default:
                         break;
@@ -338,10 +338,10 @@ static int aics_write_callback(hci_con_handle_t con_handle, uint16_t attribute_h
                 break;
 
             case AICS_OPCODE_SET_MANUAL_GAIN_MODE:
-                switch (aics->info->audio_input_state.gain_mode){
+                switch (client->info->audio_input_state.gain_mode){
                     case AICS_GAIN_MODE_AUTOMATIC:
-                        aics->info->audio_input_state.gain_mode = AICS_GAIN_MODE_MANUAL;
-                        aics_server_emit_gain_mode(aics);
+                        client->info->audio_input_state.gain_mode = AICS_GAIN_MODE_MANUAL;
+                        aics_server_emit_gain_mode(client);
                         break;
                     default:
                         return AICS_ERROR_CODE_GAIN_MODE_CHANGE_NOT_ALLOWED;
@@ -349,10 +349,10 @@ static int aics_write_callback(hci_con_handle_t con_handle, uint16_t attribute_h
                 break;
 
             case AICS_OPCODE_SET_AUTOMATIC_GAIN_MODE:
-                switch (aics->info->audio_input_state.gain_mode){
+                switch (client->info->audio_input_state.gain_mode){
                     case AICS_GAIN_MODE_MANUAL:
-                        aics->info->audio_input_state.gain_mode = AICS_GAIN_MODE_AUTOMATIC;
-                        aics_server_emit_gain_mode(aics);
+                        client->info->audio_input_state.gain_mode = AICS_GAIN_MODE_AUTOMATIC;
+                        aics_server_emit_gain_mode(client);
                         break;
                     default:
                         return AICS_ERROR_CODE_GAIN_MODE_CHANGE_NOT_ALLOWED;
@@ -363,30 +363,30 @@ static int aics_write_callback(hci_con_handle_t con_handle, uint16_t attribute_h
                 return AICS_ERROR_CODE_OPCODE_NOT_SUPPORTED;
         }
 
-        audio_input_control_service_update_change_counter(aics);
-        audio_input_control_service_server_set_callback(aics, AICS_TASK_SEND_AUDIO_INPUT_STATE);
+        audio_input_control_service_update_change_counter(client);
+        audio_input_control_service_server_set_callback(client, AICS_TASK_SEND_AUDIO_INPUT_STATE);
     }
 
-    if (attribute_handle == aics->audio_input_description_value_handle){
-        btstack_strcpy(aics->info->audio_input_description, AICS_MAX_AUDIO_INPUT_DESCRIPTION_LENGTH, (char *)buffer);
-        aics->audio_input_description_len = (uint8_t) strlen(aics->info->audio_input_description);
-        aics_emit_audio_input_description(aics);
-        audio_input_control_service_server_set_callback(aics, AICS_TASK_SEND_AUDIO_INPUT_DESCRIPTION);
+    if (attribute_handle == client->audio_input_description_value_handle){
+        btstack_strcpy(client->info->audio_input_description, AICS_MAX_AUDIO_INPUT_DESCRIPTION_LENGTH, (char *)buffer);
+        client->audio_input_description_len = (uint8_t) strlen(client->info->audio_input_description);
+        aics_emit_audio_input_description(client);
+        audio_input_control_service_server_set_callback(client, AICS_TASK_SEND_AUDIO_INPUT_DESCRIPTION);
     }
 
-    if (attribute_handle == aics->audio_input_state_client_configuration_handle){
-        aics->audio_input_state_client_configuration = little_endian_read_16(buffer, 0);
-        aics_set_con_handle(aics, con_handle, aics->audio_input_state_client_configuration);
+    if (attribute_handle == client->audio_input_state_client_configuration_handle){
+        client->audio_input_state_client_configuration = little_endian_read_16(buffer, 0);
+        aics_set_con_handle(client, con_handle, client->audio_input_state_client_configuration);
     }
     
-    if (attribute_handle == aics->audio_input_status_client_configuration_handle){
-        aics->audio_input_status_client_configuration = little_endian_read_16(buffer, 0);
-        aics_set_con_handle(aics, con_handle, aics->audio_input_status_client_configuration);
+    if (attribute_handle == client->audio_input_status_client_configuration_handle){
+        client->audio_input_status_client_configuration = little_endian_read_16(buffer, 0);
+        aics_set_con_handle(client, con_handle, client->audio_input_status_client_configuration);
     }
     
-    if (attribute_handle == aics->audio_input_description_client_configuration_handle){
-        aics->audio_input_description_client_configuration = little_endian_read_16(buffer, 0);
-        aics_set_con_handle(aics, con_handle, aics->audio_input_description_client_configuration);
+    if (attribute_handle == client->audio_input_description_client_configuration_handle){
+        client->audio_input_description_client_configuration = little_endian_read_16(buffer, 0);
+        aics_set_con_handle(client, con_handle, client->audio_input_description_client_configuration);
     }
 
     return 0;
@@ -409,9 +409,9 @@ static void aics_packet_handler(uint8_t packet_type, uint16_t channel, uint8_t *
             btstack_linked_list_iterator_init(&it, &aics_services);
 
             while (btstack_linked_list_iterator_has_next(&it)){
-                audio_input_control_service_server_t * item = (audio_input_control_service_server_t*) btstack_linked_list_iterator_next(&it);
-                if (item->con_handle != con_handle) continue;
-                item->con_handle = HCI_CON_HANDLE_INVALID;
+                audio_input_control_service_server_t * connection = (audio_input_control_service_server_t*) btstack_linked_list_iterator_next(&it);
+                if (connection->con_handle != con_handle) continue;
+                connection->con_handle = HCI_CON_HANDLE_INVALID;
             }
             break;
         default:
