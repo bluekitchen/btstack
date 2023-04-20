8624321456e9efe3c2bf8df663c9b49d78417b2f
Matthias Ringwald <matthias@ringwald.ch>
2023-04-20 14:24:05 +0200

    chipset/bcm: print local version information from ROM

--- chipset/bcm/btstack_chipset_bcm_download_firmware.c
+++ chipset/bcm/btstack_chipset_bcm_download_firmware.c
@@ -65,19 +65,56 @@ static uint16_t response_buffer_len;
 static uint8_t command_buffer[260];
 
 static const int hci_command_complete_len = 7;
-static const uint8_t hci_reset_cmd[] = { 0x03, 0x0c, 0x00 };
+static const int hci_command_complete_read_local_version = 15;
+static const uint8_t hci_read_local_version_cmd[] = { 0x01, 0x10, 0x00};
+static const uint8_t hci_reset_cmd[] =              { 0x03, 0x0c, 0x00 };
 static const uint8_t hci_command_complete_reset[] = { 0x04, 0x0e, 0x04, 0x01, 0x03, 0x0c, 0x00};
 
 static void (*download_complete)(int result);
 static uint32_t baudrate;
 
-static void bcm_send_prepared_command(void){
-    int size = 1 + 3 + command_buffer[3];
-    command_buffer[0] = 1;
-    hci_dump_packet(HCI_COMMAND_DATA_PACKET, 0, &command_buffer[1], size-1);
-    uart_driver->receive_block(&response_buffer[0], hci_command_complete_len);
-    uart_driver->send_block(command_buffer, size);
+static inline void bcm_hci_dump_event(void){
+    uint16_t event_len = 2 + response_buffer[2];
+    hci_dump_packet(HCI_EVENT_PACKET, 0, &response_buffer[1], event_len);
 }
+
+static void bcm_send_prepared_command(void){
+    command_buffer[0] = 1;
+    uint16_t command_len = 3 + command_buffer[3];
+    hci_dump_packet(HCI_COMMAND_DATA_PACKET, 0, &command_buffer[1], command_len);
+    uart_driver->send_block(command_buffer, command_len + 1);
+}
+
+// Send / Receive HCI Read Local Version Information
+
+static void bcm_receive_command_command_complete_read_local_version(void){
+
+    // TODO: check version / detect controller
+
+    const uint8_t * packet = &response_buffer[1];
+    printf("ROM version information:\n");
+    uint16_t hci_version    = packet[6];
+    uint16_t hci_revision   = little_endian_read_16(packet, 7);
+    uint16_t lmp_version    = packet[9];
+    uint16_t manufacturer   = little_endian_read_16(packet, 10);
+    uint16_t lmp_subversion = little_endian_read_16(packet, 12);
+    printf("- HCI Version    0x%04x\n", hci_version);
+    printf("- HCI Revision   0x%04x\n", hci_revision);
+    printf("- LMP Version    0x%04x\n", lmp_version);
+    printf("- LMP Subversion 0x%04x\n", lmp_subversion);
+    printf("- Manufacturer 0x%04x\n", manufacturer);
+    bcm_w4_command_complete();
+}
+
+static void bcm_send_read_local_version(void){
+    log_info("bcm: send HCI Read Local Version Information");
+    uart_driver->set_block_received(&bcm_receive_command_command_complete_read_local_version);
+    uart_driver->receive_block(response_buffer, hci_command_complete_read_local_version);
+    memcpy(&command_buffer[1], hci_read_local_version_cmd, sizeof(hci_read_local_version_cmd));
+    bcm_send_prepared_command();
+}
+
+// Send / Receive HCI Reset
 
 // Although the Controller just has been reset by the user, there might still be HCI data in the UART driver
 // which we'll ignore in the receive function
@@ -87,7 +124,8 @@ static void bcm_receive_command_complete_reset(void){
     if (response_buffer_len == hci_command_complete_len){
         // try to match command complete for HCI Reset
         if (memcmp(response_buffer, hci_command_complete_reset, hci_command_complete_len) == 0){
-            bcm_w4_command_complete();
+            bcm_hci_dump_event();
+            bcm_send_read_local_version();
             return;
         }
         memmove(&response_buffer[0], &response_buffer[1], response_buffer_len - 1);
@@ -101,16 +139,14 @@ static void bcm_send_hci_reset(void){
     response_buffer_len = 0;
     uart_driver->set_block_received(&bcm_receive_command_complete_reset);
     uart_driver->receive_block(&response_buffer[response_buffer_len], 1);
-
-    command_buffer[0] = 1;
     memcpy(&command_buffer[1], hci_reset_cmd, sizeof(hci_reset_cmd));
-    uint16_t size = sizeof(hci_reset_cmd);
-    hci_dump_packet(HCI_COMMAND_DATA_PACKET, 0, &command_buffer[1], size);
-    uart_driver->send_block(command_buffer, size + 1);
+    bcm_send_prepared_command();
 }
 
+// Other
+
 static void bcm_send_hci_baudrate(void){
-    hci_dump_packet(HCI_EVENT_PACKET, 0, &response_buffer[1], hci_command_complete_len-1);
+    bcm_hci_dump_event();
     chipset->set_baudrate_command(baudrate, &command_buffer[1]);
     uart_driver->set_block_received(&bcm_set_local_baudrate);
     uart_driver->receive_block(&response_buffer[0], hci_command_complete_len);
@@ -119,14 +155,14 @@ static void bcm_send_hci_baudrate(void){
 }
 
 static void bcm_set_local_baudrate(void){
-    hci_dump_packet(HCI_EVENT_PACKET, 0, &response_buffer[1], hci_command_complete_len-1);
+    bcm_hci_dump_event();
     uart_driver->set_baudrate(baudrate);
     uart_driver->set_block_received(&bcm_w4_command_complete);
     bcm_send_next_init_script_command();
 }
 
 static void bcm_w4_command_complete(void){
-    hci_dump_packet(HCI_EVENT_PACKET, 0, &response_buffer[1], hci_command_complete_len-1);
+    bcm_hci_dump_event();
     uart_driver->set_block_received(&bcm_w4_command_complete);
     bcm_send_next_init_script_command();
 }
@@ -135,6 +171,7 @@ static void bcm_send_next_init_script_command(void){
     int res = chipset->next_command(&command_buffer[1]);
     switch (res){
         case BTSTACK_CHIPSET_VALID_COMMAND:
+            uart_driver->receive_block(&response_buffer[0], hci_command_complete_len);
             bcm_send_prepared_command();
             break;
         case BTSTACK_CHIPSET_DONE:
