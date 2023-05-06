88a563b8dc8e6c7c9d2889f84a5b784513799727
Matthias Ringwald <matthias@ringwald.ch>
2023-05-06 14:58:50 +0200

    chipset/nxp: fix iw612 download from nxp github

--- chipset/nxp/nxp.cmake
+++ chipset/nxp/nxp.cmake
@@ -9,11 +9,12 @@ file(DOWNLOAD ${NXP_8997_PATH}/${NXP_8997_FILE} ${CMAKE_CURRENT_BINARY_DIR}/${NX
 # IW416
 set(NXP_IW416_PATH https://github.com/nxp-imx/imx-firmware/raw/lf-6.1.1_1.0.0/nxp/FwImage_IW416_SD)
 set(NXP_IW416_FILE uartiw416_bt_v0.bin)
-message("NXP IW416: Download ${NXP_IW416_FILE}")
+message("NXP IW416:   Download ${NXP_IW416_FILE}")
 file(DOWNLOAD ${NXP_IW416_PATH}/${NXP_IW416_FILE} ${CMAKE_CURRENT_BINARY_DIR}/${NXP_IW416_FILE})
 
 # IW612
+
 set(NXP_IW612_PATH https://github.com/nxp-imx/imx-firmware/raw/lf-6.1.1_1.0.0/nxp/FwImage_IW612_SD)
 set(NXP_IW612_FILE uartspi_n61x_v1.bin.se)
-message("NXP IW612: Download ${NXP_IW612_FILE}")
-file(DOWNLOAD ${NXP_IW612_PATH}/${NXP_IW612_PATH} ${CMAKE_CURRENT_BINARY_DIR}/${NXP_IW612_FILE})
+message("NXP IW612:   Download ${NXP_IW612_FILE}")
+file(DOWNLOAD ${NXP_IW612_PATH}/${NXP_IW612_FILE} ${CMAKE_CURRENT_BINARY_DIR}/${NXP_IW612_FILE})
