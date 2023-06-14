c7375dd04ca9b4e6445e307fa30d3d1018f226da
Matthias Ringwald <matthias@ringwald.ch>
2023-06-14 12:17:48 +0200

    zephyr: fix build

--- port/zephyr/src/main.c
+++ port/zephyr/src/main.c
@@ -24,7 +24,6 @@
 #include "btstack_debug.h"
 #include "btstack_event.h"
 #include "btstack_memory.h"
-#include "btstack_run_loop_zephyr.h"
 #include "hci.h"
 #include "hci_dump.h"
 #include "hci_dump_embedded_stdout.h"
