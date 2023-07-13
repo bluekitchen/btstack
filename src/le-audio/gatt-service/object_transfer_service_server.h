03a73bf0b3f2432ba018db223ad4b7f9f0fe507b
Milanka Ringwald <mila@ringwald.ch>
2023-07-13 09:41:28 +0200

    gatt-service/ots_server: manage object storage outside of lib

--- src/le-audio/gatt-service/object_transfer_service_server.h
+++ src/le-audio/gatt-service/object_transfer_service_server.h
@@ -55,6 +55,12 @@ extern "C" {
 
 typedef struct {
     oacp_result_code_t (*create)(hci_con_handle_t con_handle, uint32_t object_size, uint8_t gatt_uuid_size, uint8_t * gatt_uuid);
+
+    // View operations
+    olcp_result_code_t (*first)(hci_con_handle_t con_handle);
+    olcp_result_code_t (*last)(hci_con_handle_t con_handle);
+    olcp_result_code_t (*next)(hci_con_handle_t con_handle);
+    olcp_result_code_t (*previous)(hci_con_handle_t con_handle);
 } ots_operations_t;
 
 typedef struct {
@@ -102,19 +108,14 @@ typedef struct {
  * @brief Init Object Transfer Service Server with ATT DB
  */
 uint8_t object_transfer_service_server_init(uint32_t oacp_features, uint32_t olcp_features, 
-    uint8_t clients_num, ots_server_connection_t * clients, 
-    uint16_t objects_num, ots_object_t * objects,
+    uint8_t connections_num, ots_server_connection_t * connections, 
     const ots_operations_t * ots_operations);
 
 void object_transfer_service_server_register_packet_handler(btstack_packet_handler_t packet_handler);
 
-uint8_t object_transfer_service_server_create_object_with_type_uuid16(
-    ots_object_id_t * object_id, char * name, 
-    uint32_t properties, uint16_t type_uuid16, uint32_t allocated_size, 
-    btstack_utc_t * first_created, btstack_utc_t * last_modified);
+uint8_t object_transfer_service_server_set_current_object(hci_con_handle_t con_handle, ots_object_t * object);
+uint8_t object_transfer_service_server_update_current_object_name(hci_con_handle_t con_handle, char * name);
 
-uint8_t object_transfer_service_server_set_current_object(hci_con_handle_t con_handle, ots_object_id_t * luid);
-uint8_t object_transfer_service_server_update_current_object_name(hci_con_handle_t con_handle, ots_object_id_t * luid, char * name);
 uint8_t object_transfer_service_server_reset_current_object(hci_con_handle_t con_handle);
 uint8_t object_transfer_service_server_update_current_object_filter(hci_con_handle_t con_handle, uint8_t filter_index, ots_filter_t * filter);
 
