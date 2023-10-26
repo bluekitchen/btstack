22931ba2931647e5b31dbb4ae974586d536bd486
Ricardo Quesada <ricardoquesada@gmail.com>
2023-10-26 06:23:07 -0700

    test/hid_parser: add inconsistent HID Descriptor of Tank Mouse
    https://tank-mouse.com/

--- test/hid_parser/hid_parser_test.cpp
+++ test/hid_parser/hid_parser_test.cpp
@@ -50,7 +50,7 @@ const uint8_t mouse_descriptor_with_report_id[] = {
     0x05, 0x01, /*  Usage Page (Desktop),               */
     0x09, 0x02, /*  Usage (Mouse),                      */
     0xA1, 0x01, /*  Collection (Application),           */
-    
+
     0x85,  0x01,                    // Report ID 1
 
     0x09, 0x01, /*      Usage (Pointer),                */
@@ -126,7 +126,7 @@ const uint8_t hid_descriptor_keyboard_boot_mode[] = {
     0x29, 0xff,                    //   Usage Maxium (Reserved)
     0x81, 0x00,                    //   Input (Data, Array)
 
-    0xc0,                          // End collection  
+    0xc0,                          // End collection
 };
 
 const uint8_t combo_descriptor_with_report_ids[] = {
@@ -134,7 +134,7 @@ const uint8_t combo_descriptor_with_report_ids[] = {
     0x05, 0x01, /*  Usage Page (Desktop),               */
     0x09, 0x02, /*  Usage (Mouse),                      */
     0xA1, 0x01, /*  Collection (Application),           */
-    
+
     0x85, 0x01, // Report ID 1
 
     0x09, 0x01, /*      Usage (Pointer),                */
@@ -162,7 +162,7 @@ const uint8_t combo_descriptor_with_report_ids[] = {
     0xC0,       /*  End Collection                      */
 
     0xa1, 0x01,                    // Collection (Application)
-    
+
     0x85, 0x02, // Report ID 2
 
     // Modifier byte
@@ -210,6 +210,55 @@ const uint8_t combo_descriptor_with_report_ids[] = {
 
 };
 
+// https://tank-mouse.com/
+const uint8_t tank_mouse_descriptor[] = {
+    0x05, 0x01,        // Usage Page (Generic Desktop Ctrls)
+    0x09, 0x02,        // Usage (Mouse)
+    0xA1, 0x01,        // Collection (Application)
+    0x85, 0x03,        //   Report ID (3)
+    0x09, 0x01,        //   Usage (Pointer)
+    0xA1, 0x00,        //   Collection (Physical)
+    0x05, 0x09,        //     Usage Page (Button)
+    0x19, 0x01,        //     Usage Minimum (0x01)
+    0x29, 0x03,        //     Usage Maximum (0x03)
+    0x15, 0x00,        //     Logical Minimum (0)
+    0x25, 0x01,        //     Logical Maximum (1)
+    0x75, 0x01,        //     Report Size (1)
+    0x95, 0x05,        //     Report Count (5)
+    0x81, 0x02,        //     Input (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position)
+    0x75, 0x03,        //     Report Size (3)
+    0x95, 0x01,        //     Report Count (1)
+    0x81, 0x01,        //     Input (Const,Array,Abs,No Wrap,Linear,Preferred State,No Null Position)
+    0x05, 0x01,        //     Usage Page (Generic Desktop Ctrls)
+    0x09, 0x30,        //     Usage (X)
+    0x09, 0x31,        //     Usage (Y)
+    0x16, 0x01, 0x80,  //     Logical Minimum (-32767)
+    0x26, 0xFF, 0x7F,  //     Logical Maximum (32767)
+    0x75, 0x10,        //     Report Size (16)
+    0x95, 0x02,        //     Report Count (2)
+    0x81, 0x06,        //     Input (Data,Var,Rel,No Wrap,Linear,Preferred State,No Null Position)
+    0x09, 0x38,        //     Usage (Wheel)
+    0x15, 0x81,        //     Logical Minimum (-127)
+    0x25, 0x7F,        //     Logical Maximum (127)
+    0x75, 0x08,        //     Report Size (8)
+    0x95, 0x01,        //     Report Count (1)
+    0x81, 0x06,        //     Input (Data,Var,Rel,No Wrap,Linear,Preferred State,No Null Position)
+    0xC0,              //   End Collection
+    0xC0,              // End Collection
+    0x06, 0x01, 0xFF,  // Usage Page (Vendor Defined 0xFF01)
+    0x09, 0x01,        // Usage (0x01)
+    0xA1, 0x01,        // Collection (Application)
+    0x85, 0x05,        //   Report ID (5)
+    0x09, 0x05,        //   Usage (0x05)
+    0x15, 0x00,        //   Logical Minimum (0)
+    0x26, 0xFF, 0x00,  //   Logical Maximum (255)
+    0x75, 0x08,        //   Report Size (8)
+    0x95, 0x04,        //   Report Count (4)
+    0xB1, 0x02,        //   Feature (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position,Non-volatile)
+    0xC0,              // End Collection
+    // 89 bytes
+};
+
 const uint8_t mouse_report_without_id_positive_xy[]    = {       0x03, 0x02, 0x03 };
 const uint8_t mouse_report_without_id_negative_xy[]    = {       0x03, 0xFE, 0xFD };
 const uint8_t mouse_report_with_id_1[]    = { 0x01, 0x03, 0x02, 0x03 };
@@ -218,6 +267,8 @@ const uint8_t keyboard_report1[] = { 0x01, 0x00, 0x04, 0x05, 0x06, 0x00, 0x00, 0
 
 const uint8_t combo_report1[]    = { 0x01, 0x03, 0x02, 0x03 };
 const uint8_t combo_report2[]    = { 0x02, 0x01, 0x00,  0x04, 0x05, 0x06, 0x00, 0x00, 0x00 };
+
+const uint8_t tank_mouse_report[] = { 0x03, 0x03, 0xFD, 0xFF, 0xF6, 0xFF, 0x00};
 
 
 
@@ -265,6 +316,7 @@ TEST(HID, MouseWithoutReportIDSigned){
 
 TEST(HID, MouseWithReportID){
     static btstack_hid_parser_t hid_parser;
+    CHECK_EQUAL(3, btstack_hid_get_report_size_for_id(1, HID_REPORT_TYPE_INPUT, sizeof(mouse_descriptor_with_report_id), mouse_descriptor_with_report_id));
     btstack_hid_parser_init(&hid_parser, mouse_descriptor_with_report_id, sizeof(mouse_descriptor_with_report_id), HID_REPORT_TYPE_INPUT, mouse_report_with_id_1, sizeof(mouse_report_with_id_1));
     expect_field(&hid_parser, 9, 1, 1);
     expect_field(&hid_parser, 9, 2, 1);
@@ -326,6 +378,21 @@ TEST(HID, Combo2){
     expect_field(&hid_parser, 7, 0x00, 1);
     expect_field(&hid_parser, 7, 0x00, 1);
     CHECK_EQUAL(72, hid_parser.report_pos_in_bit);
+    CHECK_EQUAL(0, btstack_hid_parser_has_more(&hid_parser));
+}
+
+TEST(HID, TankMouse){
+    static btstack_hid_parser_t hid_parser;
+    CHECK_EQUAL(6, btstack_hid_get_report_size_for_id(3, HID_REPORT_TYPE_INPUT, sizeof(tank_mouse_descriptor), tank_mouse_descriptor));
+    btstack_hid_parser_init(&hid_parser, tank_mouse_descriptor, sizeof(tank_mouse_descriptor), HID_REPORT_TYPE_INPUT, tank_mouse_report, sizeof(tank_mouse_report));
+    expect_field(&hid_parser, 9, 1, 1);
+    expect_field(&hid_parser, 9, 2, 1);
+    expect_field(&hid_parser, 9, 3, 0);
+    // no usages for 4th and 5th report as usage min..max only allows for 3 button
+    expect_field(&hid_parser, 1, 0x30, -3);
+    expect_field(&hid_parser, 1, 0x31, -10);
+    expect_field(&hid_parser, 1, 0x38, 0);
+    CHECK_EQUAL(56, hid_parser.report_pos_in_bit);
     CHECK_EQUAL(0, btstack_hid_parser_has_more(&hid_parser));
 }
 
