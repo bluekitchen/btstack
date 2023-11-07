22029648dbaf7b329b48db7cbda905ed784c5ee2
Matthias Ringwald <matthias@ringwald.ch>
2023-11-07 09:57:44 +0100

    freebsd-netgraph: FreeBSD port that accesses ng_hci node

---
+++ port/freebsd-netgraph/README.md
@@ -0,0 +1,44 @@
+# BTstack Port for FreeBSD Systems
+
+## Overview
+This port assumes that FreeBSD provides an ng_hci netgraph node for a connected Bluetooth Controller. 
+In most cases, these are Bluetooth USB dongles or built-in Bluetooth Controller connected via USB.
+
+For Bluetooth Controllers connected via UART, the POSX-H4 port might be a better option als 
+
+## Implementation details
+In FreeBSD 13.2, the hci node is connected to a l2cap node and a btsock_hci_raw node. In order to take control, this 
+port create a custom netgraph ng_socket node and connect to the 'acl' and 'raw' hooks of the hci node. The OS Bluetooth
+functionality will be interrupted.
+
+## Compilation
+
+BTstack's FeeeBSD port does not have additional dependencies. To compile the cmake project with make
+
+    mkdir build
+    cd build
+    cmake ..
+    make
+
+or using Ninja:
+
+    mkdir ninja
+    cd ninja
+    cmake ..
+    ninja
+
+## Running the examples
+
+As the port needs to reconfigure the Bluetooth netgraph node, it needs to run with root privileges.
+It tries to connect to 'ubt0hci' by default. If your Bluetooth Controller is different, you can select it with '-u node'
+On start, BTstack prints the path to the packet log and prints the information on the detected Buetooth Controller.
+
+	$ sudo gatt_counter
+	Packet Log: /tmp/hci_dump.pklg
+	BTstack counter 0001
+	BTstack up and running on 00:1A:7D:DA:71:13.
+
+## ToDO
+- drop privileges after startup
+- auto-detect ng_hci node
+- support for profiles that require SCO: HFP & HSP
---
+++ port/freebsd-netgraph/main.c
@@ -0,0 +1,275 @@
+/*
+ * Copyright (C) 2014 BlueKitchen GmbH
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the copyright holders nor the names of
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ * 4. Any redistribution, use, or modification is done solely for
+ *    personal benefit and not for any commercial purpose or for
+ *    monetary gain.
+ *
+ * THIS SOFTWARE IS PROVIDED BY BLUEKITCHEN GMBH AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL BLUEKITCHEN
+ * GMBH OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
+ * THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * Please inquire about commercial licensing options at 
+ * contact@bluekitchen-gmbh.com
+ *
+ */
+
+#define BTSTACK_FILE__ "main.c"
+
+// *****************************************************************************
+//
+// minimal setup for HCI code
+//
+// *****************************************************************************
+
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <signal.h>
+#include <unistd.h>
+#include <getopt.h>
+
+#include "btstack_config.h"
+
+#include "ble/le_device_db_tlv.h"
+#include "bluetooth_company_id.h"
+#include "btstack_audio.h"
+#include "btstack_debug.h"
+#include "btstack_event.h"
+#include "btstack_memory.h"
+#include "btstack_run_loop.h"
+#include "btstack_run_loop_posix.h"
+#include "btstack_signal.h"
+#include "btstack_stdin.h"
+#include "btstack_tlv_posix.h"
+#include "btstack_uart.h"
+#include "classic/btstack_link_key_db_tlv.h"
+#include "hci.h"
+#include "hci_dump.h"
+#include "hci_dump_posix_fs.h"
+#include "hci_dump_posix_stdout.h"
+#include "hci_transport_netgraph.h"
+
+
+#define TLV_DB_PATH_PREFIX "/tmp/btstack_"
+#define TLV_DB_PATH_POSTFIX ".tlv"
+static char tlv_db_path[100];
+static bool tlv_reset;
+static const btstack_tlv_t * tlv_impl;
+static btstack_tlv_posix_t   tlv_context;
+static bd_addr_t             local_addr;
+static hci_transport_config_netgraph_t config;
+
+// shutdown
+static bool shutdown_triggered;
+
+int btstack_main(int argc, const char * argv[]);
+static void local_version_information_handler(uint8_t * packet);
+
+static btstack_packet_callback_registration_t hci_event_callback_registration;
+
+static void packet_handler (uint8_t packet_type, uint16_t channel, uint8_t *packet, uint16_t size){
+    if (packet_type != HCI_EVENT_PACKET) return;
+    switch (hci_event_packet_get_type(packet)){
+        case BTSTACK_EVENT_STATE:
+            switch(btstack_event_state_get_state(packet)){
+                case HCI_STATE_WORKING:
+                    gap_local_bd_addr(local_addr);
+                    printf("BTstack up and running on %s.\n", bd_addr_to_str(local_addr));
+                    btstack_strcpy(tlv_db_path, sizeof(tlv_db_path), TLV_DB_PATH_PREFIX);
+                    btstack_strcat(tlv_db_path, sizeof(tlv_db_path), bd_addr_to_str_with_delimiter(local_addr, '-'));
+                    btstack_strcat(tlv_db_path, sizeof(tlv_db_path), TLV_DB_PATH_POSTFIX);
+                    printf("TLV path: %s", tlv_db_path);
+                    if (tlv_reset){
+                        int rc = unlink(tlv_db_path);
+                        if (rc == 0) {
+                            printf(", reset ok");
+                        } else {
+                            printf(", reset failed with result = %d", rc);
+                        }
+                    }
+                    printf("\n");
+                    tlv_impl = btstack_tlv_posix_init_instance(&tlv_context, tlv_db_path);
+                    btstack_tlv_set_instance(tlv_impl, &tlv_context);
+#ifdef ENABLE_CLASSIC
+                    hci_set_link_key_db(btstack_link_key_db_tlv_get_instance(tlv_impl, &tlv_context));
+#endif    
+#ifdef ENABLE_BLE
+                    le_device_db_tlv_configure(tlv_impl, &tlv_context);
+#endif
+                    break;
+                case HCI_STATE_OFF:
+                    btstack_tlv_posix_deinit(&tlv_context);
+                    if (!shutdown_triggered) break;
+                    // reset stdin
+                    btstack_stdin_reset();
+                    log_info("Good bye, see you.\n");
+                    exit(0);
+                    break;
+                default:
+                    break;                    
+            }
+            break;
+        case HCI_EVENT_COMMAND_COMPLETE:
+            if (hci_event_command_complete_get_command_opcode(packet) == HCI_OPCODE_HCI_READ_LOCAL_VERSION_INFORMATION){
+                local_version_information_handler(packet);
+            }
+            break;
+        default:
+            break;
+    }
+}
+
+static void trigger_shutdown(void){
+    printf("CTRL-C - SIGINT received, shutting down..\n");
+    log_info("sigint_handler: shutting down");
+    shutdown_triggered = true;
+    hci_power_control(HCI_POWER_OFF);
+}
+
+static int led_state = 0;
+void hal_led_toggle(void){
+    led_state = 1 - led_state;
+    printf("LED State %u\n", led_state);
+}
+
+static void local_version_information_handler(uint8_t * packet){
+    printf("Local version information:\n");
+    uint16_t hci_version    = packet[6];
+    uint16_t hci_revision   = little_endian_read_16(packet, 7);
+    uint16_t lmp_version    = packet[9];
+    uint16_t manufacturer   = little_endian_read_16(packet, 10);
+    uint16_t lmp_subversion = little_endian_read_16(packet, 12);
+    printf("- HCI Version    0x%04x\n", hci_version);
+    printf("- HCI Revision   0x%04x\n", hci_revision);
+    printf("- LMP Version    0x%04x\n", lmp_version);
+    printf("- LMP Subversion 0x%04x\n", lmp_subversion);
+    printf("- Manufacturer 0x%04x\n", manufacturer);
+}
+
+static char short_options[] = "hu:l:r";
+
+static struct option long_options[] = {
+        {"help",        no_argument,        NULL,   'h'},
+        {"logfile",    required_argument,  NULL,   'l'},
+        {"reset-tlv",    no_argument,       NULL,   'r'},
+        {"node",    required_argument,  NULL,   'u'},
+        {0, 0, 0, 0}
+};
+
+static char *help_options[] = {
+        "print (this) help.",
+        "set file to store debug output and HCI trace.",
+        "reset bonding information stored in TLV.",
+        "set name of netgraph HCI node. Default: ubt0hci",
+};
+
+static char *option_arg_name[] = {
+        "",
+        "LOGFILE",
+        "",
+        "NODE",
+};
+
+static void usage(const char *name){
+    unsigned int i;
+    printf( "usage:\n\t%s [options]\n", name );
+    printf("valid options:\n");
+    for( i=0; long_options[i].name != 0; i++) {
+        printf("--%-10s| -%c  %-10s\t\t%s\n", long_options[i].name, long_options[i].val, option_arg_name[i], help_options[i] );
+    }
+}
+
+int main(int argc, const char * argv[]){
+
+    const char * log_file_path = NULL;
+
+    // set default device path
+    config.device_name = "ubt0hci";
+
+    // parse command line parameters
+    while(true){
+        int c = getopt_long( argc, (char* const *)argv, short_options, long_options, NULL );
+        if (c < 0) {
+            break;
+        }
+        if (c == '?'){
+            break;
+        }
+        switch (c) {
+            case 'l':
+                log_file_path = optarg;
+                break;
+            case 'u':
+                config.device_name = optarg;
+                break;
+            case 'r':
+                tlv_reset = true;
+                break;
+            case 'h':
+            default:
+                usage(argv[0]);
+                return EXIT_FAILURE;
+        }
+    }
+    /// GET STARTED with BTstack ///
+	btstack_memory_init();
+    btstack_run_loop_init(btstack_run_loop_posix_get_instance());
+	    
+    // log into file using HCI_DUMP_PACKETLOGGER format
+    if (log_file_path == NULL){
+        log_file_path = "/tmp/hci_dump.pklg";
+    }
+
+    hci_dump_posix_fs_open(log_file_path, HCI_DUMP_PACKETLOGGER);
+    const hci_dump_t * hci_dump_impl = hci_dump_posix_fs_get_instance();
+    hci_dump_init(hci_dump_impl);
+    printf("Packet Log: %s\n", log_file_path);
+
+    // init HCI
+	const hci_transport_t * transport = hci_transport_netgraph_instance();
+    config.type = HCI_TRANSPORT_CONFIG_USB;
+	hci_init(transport, &config);
+
+#ifdef HAVE_PORTAUDIO
+    btstack_audio_sink_set_instance(btstack_audio_portaudio_sink_get_instance());
+    btstack_audio_source_set_instance(btstack_audio_portaudio_source_get_instance());
+#endif
+
+    // inform about BTstack state
+    hci_event_callback_registration.callback = &packet_handler;
+    hci_add_event_handler(&hci_event_callback_registration);
+
+    // register callback for CTRL-c
+    btstack_signal_register_callback(SIGINT, &trigger_shutdown);
+
+    // setup app
+    btstack_main(argc, argv);
+
+    // go
+    btstack_run_loop_execute();    
+
+    return 0;
+}
