0a12e51cb79306742ef2ad1c7c1b085711c8d517
Matthias Ringwald <matthias@ringwald.ch>
2023-12-01 11:00:05 +0100

    mcs_client: update connect param order

--- src/le-audio/gatt-service/media_control_service_client.h
+++ src/le-audio/gatt-service/media_control_service_client.h
@@ -100,28 +100,52 @@ typedef struct {
  */
 void media_control_service_client_init(void);
 
-/**
- * @brief Connect to Media Control Services of remote device. The client will automatically register for notifications. 
+ /**
+  * @brief Connect to Generic Media Control Service of remote device. The client will automatically register for notifications.
+  * The mute state is received via GATTSERVICE_SUBEVENT_MCS_CLIENT_MUTE event.
+  * The mute state can be 0 - off, 1 - on, 2 - disabeled and it is valid if the ATT status is equal to ATT_ERROR_SUCCESS,
+  * see ATT errors (see bluetooth.h) for other values.
+  *
+  * Event GATTSERVICE_SUBEVENT_MCS_CLIENT_CONNECTED is emitted with status ERROR_CODE_SUCCESS on success, otherwise
+  * GATT_CLIENT_IN_WRONG_STATE, ERROR_CODE_UNSUPPORTED_FEATURE_OR_PARAMETER_VALUE if no media control service is found, or ATT errors (see bluetooth.h).
+  *
+  * @param con_handle
+  * @param connection
+  * @param packet_handler
+  * @param characteristics       storage for characteristics
+  * @param characteristics_num >= MEDIA_CONTROL_SERVICE_CLIENT_NUM_CHARACTERISTICS
+  * @param mcs_cid
+  * @return status ERROR_CODE_SUCCESS on success, otherwise ERROR_CODE_COMMAND_DISALLOWED if there is already a client associated with con_handle, or BTSTACK_MEMORY_ALLOC_FAILED
+  */
+ uint8_t media_control_service_client_connect_generic_player(
+     hci_con_handle_t con_handle, btstack_packet_handler_t packet_handler,
+     mcs_client_connection_t * connection,
+     gatt_service_client_characteristic_t * characteristics, uint8_t characteristics_num,
+     uint16_t * mcs_cid);
+
+ /**
+ * @brief Connect to a Media Control Service instance of remote device. The client will automatically register for notifications.
  * The mute state is received via GATTSERVICE_SUBEVENT_MCS_CLIENT_MUTE event.
- * The mute state can be 0 - off, 1 - on, 2 - disabeled and it is valid if the ATTT status is equal to ATT_ERROR_SUCCESS, 
+ * The mute state can be 0 - off, 1 - on, 2 - disabeled and it is valid if the ATT status is equal to ATT_ERROR_SUCCESS,
  * see ATT errors (see bluetooth.h) for other values.
  *   
  * Event GATTSERVICE_SUBEVENT_MCS_CLIENT_CONNECTED is emitted with status ERROR_CODE_SUCCESS on success, otherwise
  * GATT_CLIENT_IN_WRONG_STATE, ERROR_CODE_UNSUPPORTED_FEATURE_OR_PARAMETER_VALUE if no media control service is found, or ATT errors (see bluetooth.h). 
  *
  * @param con_handle
- * @param connection
  * @param service_index         index o media player to connect to
+ * @param packet_handler
+ * @param connection
  * @param characteristics       storage for characteristics
  * @param characteristics_num >= MEDIA_CONTROL_SERVICE_CLIENT_NUM_CHARACTERISTICS
- * @param packet_handler
  * @param mcs_cid
  * @return status ERROR_CODE_SUCCESS on success, otherwise ERROR_CODE_COMMAND_DISALLOWED if there is already a client associated with con_handle, or BTSTACK_MEMORY_ALLOC_FAILED 
  */
 uint8_t media_control_service_client_connect_media_player(
-    hci_con_handle_t con_handle, mcs_client_connection_t * connection, uint8_t service_index,
+    hci_con_handle_t con_handle, uint8_t service_index, btstack_packet_handler_t packet_handler,
+    mcs_client_connection_t * connection,
     gatt_service_client_characteristic_t * characteristics, uint8_t characteristics_num,
-    btstack_packet_handler_t packet_handler, uint16_t * mcs_cid);
+    uint16_t * mcs_cid);
 
 /**
  * @brief Get the name of the media player application that is providing media tracks. 
