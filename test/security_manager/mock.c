d0f707ff117b6d1a94ac66ab24fc07f12e77bb02
Matthias Ringwald <matthias@ringwald.ch>
2024-01-15 18:12:06 +0100

    test/security_manager: fix build

--- test/security_manager/mock.c
+++ test/security_manager/mock.c
@@ -207,6 +207,16 @@ void hci_le_set_own_address_type(uint8_t own_address){
 void hci_le_random_address_set(const bd_addr_t addr){
 }
 
+bool hci_is_le_identity_address_type(bd_addr_type_t address_type) {
+    switch (address_type) {
+        case BD_ADDR_TYPE_LE_PUBLIC_IDENTITY:
+        case BD_ADDR_TYPE_LE_RANDOM_IDENTITY:
+            return true;
+        default:
+            return false;
+    }
+}
+
 void l2cap_request_can_send_fix_channel_now_event(hci_con_handle_t con_handle, uint16_t cid){
 	if (packet_buffer_len) return;
     uint8_t event[] = { L2CAP_EVENT_CAN_SEND_NOW, 2, 0, 0};
