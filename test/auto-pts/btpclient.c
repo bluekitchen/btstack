cbd18c2f72df373483ee0ad1fbdd0173a72a9588
Matthias Ringwald <matthias@ringwald.ch>
2024-02-19 19:16:57 +0100

    tset/auto-pts: trigger pairing on connect

--- test/auto-pts/btpclient.c
+++ test/auto-pts/btpclient.c
@@ -449,6 +449,10 @@ static void btstack_packet_handler (uint8_t packet_type, uint16_t channel, uint8
                             little_endian_store_16(buffer, 9, conn_latency);
                             little_endian_store_16(buffer, 11, supervision_timeout);
                             btp_send(BTP_SERVICE_ID_GAP, BTP_GAP_EV_DEVICE_CONNECTED, 0, sizeof(buffer), &buffer[0]);
+
+                            // trigger security
+                            sm_request_pairing(remote_handle);
+
                             break;
                         default:
                             break;
