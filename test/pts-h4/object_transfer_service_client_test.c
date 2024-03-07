a5eb0d19afe7d56dfa58cfde76517747fe63ccef
Milanka Ringwald <mila@ringwald.ch>
2024-03-07 16:56:03 +0100

    test/pts-h4: ots client test

---
+++ test/pts-h4/object_transfer_service_client_test.c
@@ -0,0 +1,318 @@
+/*
+ * Copyright (C) 2024 BlueKitchen GmbH
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the copyright holders nor the names of
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ * 4. Any redistribution, use, or modification is done solely for
+ *    personal benefit and not for any commercial purpose or for
+ *    monetary gain.
+ *
+ * THIS SOFTWARE IS PROVIDED BY BLUEKITCHEN GMBH AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL MATTHIAS
+ * RINGWALD OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
+ * THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * Please inquire about commercial licensing options at 
+ * contact@bluekitchen-gmbh.com
+ *
+ */
+
+#define BTSTACK_FILE__ "object_transfer_service_client_test.c"
+
+
+#include <inttypes.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <btstack_lc3.h>
+
+#include "btstack.h"
+
+typedef struct advertising_report {
+    uint8_t   type;
+    uint8_t   event_type;
+    uint8_t   address_type;
+    bd_addr_t address;
+    uint8_t   rssi;
+    uint8_t   length;
+    const uint8_t * data;
+} advertising_report_t;
+
+static enum {
+    APP_STATE_IDLE,
+    APP_STATE_W4_SCAN_RESULT,
+    APP_STATE_W4_CONNECT,
+    APP_STATE_CONNECTED
+} app_state;
+
+static ots_client_connection_t ots_connection;
+
+static advertising_report_t report;
+static btstack_packet_callback_registration_t sm_event_callback_registration;
+static hci_con_handle_t connection_handle;
+static uint16_t ots_cid;
+
+static bd_addr_t public_pts_address = {0xC0, 0x07, 0xE8, 0x41, 0x45, 0x91};
+static int       public_pts_address_type = 0;
+static bd_addr_t current_pts_address;
+static int       current_pts_address_type;
+
+
+static btstack_packet_callback_registration_t hci_event_callback_registration;
+
+static void show_usage(void);
+static void hci_event_handler(uint8_t packet_type, uint16_t channel, uint8_t *packet, uint16_t size);
+static void gatt_client_event_handler(uint8_t packet_type, uint16_t channel, uint8_t *packet, uint16_t size);
+
+static void object_transfer_service_client_setup(void){
+    // Init L2CAP
+    l2cap_init();
+
+    // GATT Client setup
+    gatt_client_init();
+    
+    object_transfer_service_client_init();
+
+    sm_init();
+    sm_set_io_capabilities(IO_CAPABILITY_NO_INPUT_NO_OUTPUT);
+    sm_set_authentication_requirements(SM_AUTHREQ_SECURE_CONNECTION | SM_AUTHREQ_BONDING);
+    sm_event_callback_registration.callback = &hci_event_handler;
+    sm_add_event_handler(&sm_event_callback_registration);
+
+    hci_event_callback_registration.callback = &hci_event_handler;
+    hci_add_event_handler(&hci_event_callback_registration);
+} 
+
+static void dump_advertising_report(uint8_t *packet){
+    bd_addr_t address;
+    gap_event_advertising_report_get_address(packet, address);
+
+    printf("    * adv. event: evt-type %u, addr-type %u, addr %s, rssi %u, length adv %u, data: ", 
+        gap_event_advertising_report_get_advertising_event_type(packet),
+        gap_event_advertising_report_get_address_type(packet), 
+        bd_addr_to_str(address), 
+        gap_event_advertising_report_get_rssi(packet), 
+        gap_event_advertising_report_get_data_length(packet));
+    printf_hexdump(gap_event_advertising_report_get_data(packet), gap_event_advertising_report_get_data_length(packet));
+    
+}
+
+static void hci_event_handler(uint8_t packet_type, uint16_t channel, uint8_t *packet, uint16_t size){
+    UNUSED(channel);
+    UNUSED(size);
+
+    bd_addr_t address;
+
+    if (packet_type != HCI_EVENT_PACKET){
+        return;  
+    }
+
+    bd_addr_t addr;
+    switch (hci_event_packet_get_type(packet)) {
+
+        case SM_EVENT_JUST_WORKS_REQUEST:
+            printf("Just Works requested\n");
+            sm_just_works_confirm(sm_event_just_works_request_get_handle(packet));
+            break;
+        case SM_EVENT_NUMERIC_COMPARISON_REQUEST:
+            printf("Confirming numeric comparison: %"PRIu32"\n", sm_event_numeric_comparison_request_get_passkey(packet));
+            sm_numeric_comparison_confirm(sm_event_passkey_display_number_get_handle(packet));
+            break;
+        case SM_EVENT_PASSKEY_DISPLAY_NUMBER:
+            printf("Display Passkey: %"PRIu32"\n", sm_event_passkey_display_number_get_passkey(packet));
+            break;
+        case SM_EVENT_IDENTITY_CREATED:
+            sm_event_identity_created_get_identity_address(packet, addr);
+            printf("Identity created: type %u address %s\n", sm_event_identity_created_get_identity_addr_type(packet), bd_addr_to_str(addr));
+            break;
+        case SM_EVENT_IDENTITY_RESOLVING_SUCCEEDED:
+            sm_event_identity_resolving_succeeded_get_identity_address(packet, addr);
+            printf("Identity resolved: type %u address %s\n", sm_event_identity_resolving_succeeded_get_identity_addr_type(packet), bd_addr_to_str(addr));
+            break;
+        case SM_EVENT_IDENTITY_RESOLVING_FAILED:
+            sm_event_identity_created_get_address(packet, addr);
+            printf("Identity resolving failed\n");
+            break;
+        case SM_EVENT_PAIRING_COMPLETE:
+            switch (sm_event_pairing_complete_get_status(packet)){
+                case ERROR_CODE_SUCCESS:
+                    printf("Pairing complete, success\n");
+                    break;
+                case ERROR_CODE_CONNECTION_TIMEOUT:
+                    printf("Pairing failed, timeout\n");
+                    break;
+                case ERROR_CODE_REMOTE_USER_TERMINATED_CONNECTION:
+                    printf("Pairing faileed, disconnected\n");
+                    break;
+                case ERROR_CODE_AUTHENTICATION_FAILURE:
+                    printf("Pairing failed, reason = %u\n", sm_event_pairing_complete_get_reason(packet));
+                    break;
+                default:
+                    break;
+            }
+            break;
+
+        case BTSTACK_EVENT_STATE:
+            // BTstack activated, get started
+            if (btstack_event_state_get_state(packet) != HCI_STATE_WORKING) break;
+            printf("BTstack activated, start scanning\n");
+            // app_state = APP_STATE_W4_SCAN_RESULT;
+            // gap_start_scan();
+            break;
+
+        case GAP_EVENT_ADVERTISING_REPORT:
+            if (app_state != APP_STATE_W4_SCAN_RESULT) return;
+
+            gap_event_advertising_report_get_address(packet, address);
+            dump_advertising_report(packet);
+
+            if (memcmp(address, public_pts_address, 6) != 0){
+                break;
+            }
+            // stop scanning, and connect to the device
+            app_state = APP_STATE_W4_CONNECT;
+            gap_stop_scan();
+            printf("Stop scan. Connect to device with addr %s.\n", bd_addr_to_str(report.address));
+            gap_connect(report.address,report.address_type);
+            break;
+
+        case HCI_EVENT_META_GAP:
+            // Wait for connection complete
+            if (hci_event_gap_meta_get_subevent_code(packet) !=  GAP_SUBEVENT_LE_CONNECTION_COMPLETE) break;
+            
+            // Get connection handle from event
+            connection_handle = gap_subevent_le_connection_complete_get_connection_handle(packet);
+            gap_stop_scan();
+            app_state = APP_STATE_CONNECTED;
+            printf("GAP connected.\n");
+            (void) object_transfer_service_client_connect(connection_handle, &gatt_client_event_handler,
+                                                             &ots_connection,
+                                                             0, 0xFFFF, 0,
+                                                             &ots_cid);
+            break;
+
+        case HCI_EVENT_DISCONNECTION_COMPLETE:
+            connection_handle = HCI_CON_HANDLE_INVALID;
+
+            object_transfer_service_client_disconnect(&ots_connection);
+            printf("Disconnected\n");
+            break;
+        default:
+            break;
+    }
+}
+
+static void gatt_client_event_handler(uint8_t packet_type, uint16_t channel, uint8_t *packet, uint16_t size) {
+    UNUSED(packet_type);
+    UNUSED(channel);
+    UNUSED(size);
+
+    uint8_t status;
+
+    if (hci_event_packet_get_type(packet) != HCI_EVENT_GATTSERVICE_META) {
+        return;
+    }
+
+    switch (hci_event_gattservice_meta_get_subevent_code(packet)) {
+        case GATTSERVICE_SUBEVENT_OTS_CLIENT_CONNECTED:
+            status = gattservice_subevent_ots_client_connected_get_att_status(packet);
+            switch (status) {
+                case ERROR_CODE_SUCCESS:
+                    printf("OTS Client Test: connected\n");
+                    break;
+                default:
+                    printf("OTS Client Test: connection failed, err 0x%02x.\n", status);
+                    gap_disconnect(connection_handle);
+                    break;
+            }
+            break;
+
+        case GATTSERVICE_SUBEVENT_OTS_CLIENT_WRITE_DONE:
+            status = gattservice_subevent_ots_client_write_done_get_att_status(packet);
+            if (status != ATT_ERROR_SUCCESS){
+                printf("OTS Client Test: Write failed, 0x%02x\n", status);
+            } else {
+                printf("OTS Client Test: Write successful\n");
+            }
+            break;
+
+        case GATTSERVICE_SUBEVENT_OTS_CLIENT_DISCONNECTED:
+            printf("OTS Client Test: disconnected\n");
+            break;
+
+        default:
+            break;
+    }
+}
+
+static void show_usage(void){
+    uint8_t iut_address_type;
+    bd_addr_t      iut_address;
+    gap_le_get_own_address(&iut_address_type, iut_address);
+
+    printf("--- OTS Client ---\n");
+    printf("c    - Connect to %s\n", bd_addr_to_str(current_pts_address));
+}
+
+static void stdin_process(char c){
+    uint8_t status = ERROR_CODE_SUCCESS;
+
+    switch (c){
+        case 'c':
+            printf("Connect to %s\n", bd_addr_to_str(current_pts_address));
+            app_state = APP_STATE_W4_CONNECT;
+            status = gap_connect(current_pts_address, current_pts_address_type);
+            break;
+
+        case '\n':
+        case '\r':
+            break;
+        default:
+            show_usage();
+            break;
+    }
+
+    if (status != ERROR_CODE_SUCCESS){
+        printf("OTS Client Test: command %c failed due to 0%02x\n", c, status);
+    }
+}
+
+int btstack_main(int argc, const char * argv[]);
+int btstack_main(int argc, const char * argv[]){
+    UNUSED(argc);
+    (void)argv;
+
+    btstack_stdin_setup(stdin_process);
+
+    object_transfer_service_client_setup();
+    memcpy(current_pts_address, public_pts_address, 6);
+    current_pts_address_type = public_pts_address_type;
+
+    app_state = APP_STATE_IDLE;
+
+    // turn on!
+    hci_power_control(HCI_POWER_ON);
+    return 0;
+}
+
+
+
