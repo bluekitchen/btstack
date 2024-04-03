ec066ac1849d27f4284bfa812aec37a5f7766e03
Matthias Ringwald <matthias@ringwald.ch>
2024-04-03 15:50:25 +0200

    posix-h4: configure for 5 audio channels

--- port/posix-h4/btstack_config.h
+++ port/posix-h4/btstack_config.h
@@ -49,6 +49,8 @@
 #define NVM_NUM_DEVICE_DB_ENTRIES      16
 #define NVM_NUM_LINK_KEYS              16
 
+#define MAX_NR_AUDIO_CHANNELS           5
+
 // Mesh Configuration
 #define ENABLE_MESH
 #define ENABLE_MESH_ADV_BEARER
