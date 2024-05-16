bluekitchen/btstack

commit 9fd274a1e93226c6b897affd93ad5315018d96e8

hci: re-start le connection with address in case of required stop to update lists

src/hci.c
@@ -3325,9 +3325,12 @@ static void hci_handle_le_connection_complete_event(const uint8_t * hci_event){
 		//  In either case, the event shall be sent with the error code Unknown Connection Identifier (0x02)."
 		if (status == ERROR_CODE_UNKNOWN_CONNECTION_IDENTIFIER){
 		    // reset state
-            hci_stack->le_connecting_state   = LE_CONNECTING_IDLE;
+            hci_stack->le_connecting_state = LE_CONNECTING_IDLE;
 			// get outgoing connection conn struct for direct connect
-			conn = gap_get_outgoing_le_connection();
+            if (hci_stack->le_connecting_request == LE_CONNECTING_DIRECT){
+                conn = gap_get_outgoing_le_connection();
+                conn->state = SEND_CREATE_CONNECTION;
+            }
 		}
 
 		// free connection if cancelled by user (request == IDLE)

commit 1f4510f50882959829b46b66e4acf55c4ea83ad1

hci: free memory for le connection on cancel (regression from 9fd274a)

src/hci.c
@@ -3394,9 +3394,10 @@ static void hci_handle_le_connection_complete_event(const uint8_t * hci_event){
             connection_was_cancelled = true;
 		    // reset state
             hci_stack->le_connecting_state = LE_CONNECTING_IDLE;
-			// get outgoing connection conn struct for direct connect
+            // get outgoing connection conn struct for direct connect
+            conn = gap_get_outgoing_le_connection();
+            // prepare restart if still active
             if (hci_stack->le_connecting_request == LE_CONNECTING_DIRECT){
-                conn = gap_get_outgoing_le_connection();
                 conn->state = SEND_CREATE_CONNECTION;
             }
 		}
@@ -3414,7 +3415,7 @@ static void hci_handle_le_connection_complete_event(const uint8_t * hci_event){
         // - connection cancelled by user
         // by this, no event is emitted for intermediate connection cancel required filterlist modification
         if ((connection_was_cancelled == false) || cancelled_by_user){
-            hci_emit_event(gap_event, sizeof(gap_event), 1);
+            hci_emit_btstack_event(gap_event, sizeof(gap_event), 1);
         }
         return;
 	}