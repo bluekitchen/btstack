e32e07aaeccd20cd66cf035257c3161ace383abd
Marcel Wappler <btstack@deyama.net>
2024-06-05 11:09:41 +0200

    defined bis masks for supported_message_types, corrected type to uin8_t bitmask in map_util_create_access_service_sdp_record to comply with BT MAP spec

--- example/map_server_demo.c
+++ example/map_server_demo.c
@@ -360,7 +360,9 @@ int btstack_main(int argc, const char * argv[]){
     // map_notification_client_init();
 
     // setup MAP Access Server
-    map_message_type_t supported_message_types = MAP_MESSAGE_TYPE_EMAIL;
+    uint8_t supported_message_types = MAP_SUPPORTED_MESSAGE_TYPE_EMAIL |
+                                                    MAP_SUPPORTED_MESSAGE_TYPE_SMS_GSM |
+                                                    MAP_SUPPORTED_MESSAGE_TYPE_SMS_CDMA;
     uint32_t supported_features = 0x1F;
     memset(map_message_access_service_buffer, 0, sizeof(map_message_access_service_buffer));
     map_util_create_access_service_sdp_record(map_message_access_service_buffer,
