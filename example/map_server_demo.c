f0944b2edc6cdcfd9c1b35f6b9c6284c9222ca15
Marcel Wappler <btstack@deyama.net>
2024-06-20 15:11:38 +0200

    MAP/MSE/MMB/BV-34-I: PASS (Verify that the MSE correctly responds to a request for a Conversation-Listing.)

--- example/map_server_demo.c
+++ example/map_server_demo.c
@@ -166,6 +166,7 @@ static struct test_config_s
 {.nr = 3, .descr = "MAP/MSE/MMB/BV-16-I 23"          , .type = &msg,  .obj_count = 1, .objects = { "EMAIL","EMAIL"                           }, },
 {.nr = 4, .descr = "MAP/MSE/MMB/BV-24-I <a><OK>"     , .type = &convo,.obj_count = 0, .objects = { "",""                                     }, },
 {.nr = 5, .descr = "MAP/MSE/MMB/BV-25-I <c><OK>"     , .type = &convo,.obj_count = 0, .objects = { "",""                                     }, },
+{.nr = 6, .descr = "MAP/MSE/MMB/BV-34-I"             , .type = &convo,.obj_count = 1, .objects = { "",""                                     }, },
 };
 
 struct test_config_s* config = &test_configs[0];
