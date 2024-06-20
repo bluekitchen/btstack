72fc0f45e1b26be60d0acfa0fa03eb074fa74706
Marcel Wappler <btstack@deyama.net>
2024-06-20 15:10:28 +0200

    map_server_demo: corrected test case desciptions on the console

--- example/map_server_demo.c
+++ example/map_server_demo.c
@@ -164,8 +164,8 @@ static struct test_config_s
 {.nr = 1, .descr = "MAP/MSE/MMB/BV-12-I"             , .type = &msg,  .obj_count = 1, .objects = { "EMAIL", "SMS_GSM","SMS_CDMA"             }, },
 {.nr = 2, .descr = "MAP/MSE/MMB/BV-15-I 18 20 22"    , .type = &msg,  .obj_count = 5, .objects = { "EMAIL","SMS_GSM","SMS_CDMA", "MMS", "IM" }, },
 {.nr = 3, .descr = "MAP/MSE/MMB/BV-16-I 23"          , .type = &msg,  .obj_count = 1, .objects = { "EMAIL","EMAIL"                           }, },
-{.nr = 4, .descr = "MAP/MSE/MMB/BV-16-I 24 <a><OK>"  , .type = &convo,.obj_count = 0, .objects = { "",""                                     }, },
-{.nr = 5, .descr = "MAP/MSE/MMB/BV-16-I 25 <c><OK>"  , .type = &convo,.obj_count = 0, .objects = { "",""                                     }, },
+{.nr = 4, .descr = "MAP/MSE/MMB/BV-24-I <a><OK>"     , .type = &convo,.obj_count = 0, .objects = { "",""                                     }, },
+{.nr = 5, .descr = "MAP/MSE/MMB/BV-25-I <c><OK>"     , .type = &convo,.obj_count = 0, .objects = { "",""                                     }, },
 };
 
 struct test_config_s* config = &test_configs[0];
