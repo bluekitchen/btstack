da11a2a588698bb70375af2c6d8ebbe9e1dbe3d6
Marcel Wappler <btstack@deyama.net>
2024-07-01 08:40:27 +0200

    map_access_server: MAP/MSE/GOEP/ROB/BV-02-C: PASS (2024-07-01) Verify that an IUT, that does not support Reliable Sessions, is able to reject an incoming request to create a Reliable Session.

--- port/windows-winusb/btstack_config.h
+++ port/windows-winusb/btstack_config.h
@@ -20,7 +20,7 @@
 #define ENABLE_CLASSIC
 #define ENABLE_CROSS_TRANSPORT_KEY_DERIVATION
 // traditionally OBEX is sent over RFCOM but this is an additonal layer, diable for better wireshark debugging
-//#define ENABLE_GOEP_L2CAP
+// #define ENABLE_GOEP_L2CAP // PTS test Case MAP/MSE/GOEP/ROB/BV-02-C needs this to be enabled to pass the test
 #define ENABLE_HFP_WIDE_BAND_SPEECH
 #define ENABLE_L2CAP_ENHANCED_RETRANSMISSION_MODE
 #define ENABLE_L2CAP_LE_CREDIT_BASED_FLOW_CONTROL_MODE
