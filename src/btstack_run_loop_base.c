7091533c99176993221c6e508c380437561b1b36
Dirk Helbig <DirkHelbig@gmx.de>
2024-07-23 21:06:45 +0200

    btstack_run_loop_base: fix warnings

--- src/btstack_run_loop_base.c
+++ src/btstack_run_loop_base.c
@@ -2,3 +2,7 @@
  *  btstack_run_loop_base.c was integrated into btstack_run_loop.c
  *  Deprecated - will be removed in future releases
  */
+
+// dummy function added to avoid warning about empty file
+void btstack_run_loop_base_dummy(void) {
+}
