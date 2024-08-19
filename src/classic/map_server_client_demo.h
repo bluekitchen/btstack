6e19651ed9fc6bff22b7f1d5f878f6c0e50aed20
Marcel Wappler <btstack@deyama.net>
2024-08-19 15:29:48 +0200

    map_server_demo/map_notification_client_demo: MAP/MSE/MMD/BV-05: PASS (2024-08-16) PTS 8.6.0 & L2CAP & MAP_NEW, Info: manual only. Sending event reports later needs to be implemented yet.

--- src/classic/map_server_client_demo.h
+++ src/classic/map_server_client_demo.h
@@ -52,6 +52,10 @@
 extern "C" {
 #endif
 
+/**
+* @brief prepares the notification part for TC MAP_MSE_MMD_BV_05
+*/
+void mac_select_tc_MAP_MSE_MMD_BV_05(void);
 
 /**
 * @brief creates the next mnc event report body object for PTS tests MAP/MSE/MMN/BV-*
