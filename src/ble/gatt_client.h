aa43543a707b66e8b00ddb5b292391fdac24a028
Matthias Ringwald <matthias@ringwald.ch>
2024-08-26 13:12:33 +0200

    gatt_client: add gatt_client_find_included_services_for_service_with_context

--- src/ble/gatt_client.h
+++ src/ble/gatt_client.h
@@ -484,7 +484,27 @@ uint8_t gatt_client_discover_primary_services_by_uuid128(btstack_packet_handler_
  */
 uint8_t gatt_client_find_included_services_for_service(btstack_packet_handler_t callback, hci_con_handle_t con_handle, gatt_client_service_t * service);
 
-/** 
+/**
+ * @brief Finds included services within the specified service.
+ * For each found included service a GATT_EVENT_INCLUDED_SERVICE_QUERY_RESULT event will be emitted.
+ * The GATT_EVENT_QUERY_COMPLETE event marks the end of discovery.
+ * Information about included service type (primary/secondary) can be retrieved either by sending
+ * an ATT find information request for the returned start group handle
+ * (returning the handle and the UUID for primary or secondary service) or by comparing the service
+ * to the list of all primary services.
+ * @param  callback
+ * @param  con_handle
+ * @param service_id    - context provided to callback in events
+ * @param connection_id - contest provided to callback in events
+ * @param  service_id
+ * @return status BTSTACK_MEMORY_ALLOC_FAILED, if no GATT client for con_handle is found
+ *                GATT_CLIENT_IN_WRONG_STATE , if GATT client is not ready
+ *                ERROR_CODE_SUCCESS         , if query is successfully registered
+ */
+uint8_t gatt_client_find_included_services_for_service_with_context(btstack_packet_handler_t callback, hci_con_handle_t con_handle,
+                                                                    gatt_client_service_t * service, uint16_t service_id, uint16_t connection_id);
+
+/**
  * @brief Discovers all characteristics within the specified service. 
  * For each found characteristic a GATT_EVENT_CHARACTERISTIC_QUERY_RESULT event will be emited. 
  * The GATT_EVENT_QUERY_COMPLETE event marks the end of discovery.
