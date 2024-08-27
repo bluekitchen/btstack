ef2990b74ccf715351e1eae2ee324d67dbe8424d
Matthias Ringwald <matthias@ringwald.ch>
2024-08-27 14:27:30 +0200

    gatt-service/vocs-client: use gatt client with context functions

--- src/le-audio/gatt-service/volume_offset_control_service_client.c
+++ src/le-audio/gatt-service/volume_offset_control_service_client.c
@@ -555,17 +555,17 @@ static void vocs_client_run_for_connection(void * context){
     switch (connection->state){
         case VOLUME_OFFSET_CONTROL_SERVICE_CLIENT_STATE_W2_QUERY_CHANGE_COUNTER:
             connection->state = VOLUME_OFFSET_CONTROL_SERVICE_CLIENT_STATE_W4_CHANGE_COUNTER_RESULT;
-            (void) gatt_client_read_value_of_characteristic_using_value_handle(
+            (void) gatt_client_read_value_of_characteristic_using_value_handle_with_context(
                     &vocs_client_handle_gatt_client_event, connection->basic_connection.con_handle,
-                    vocs_client_value_handle_for_index(connection));
+                    vocs_client_value_handle_for_index(connection), vocs_client.service_id, connection->basic_connection.cid);
             break;
 
         case VOLUME_OFFSET_CONTROL_SERVICE_CLIENT_STATE_W2_READ_CHARACTERISTIC_VALUE:
             connection->state = VOLUME_OFFSET_CONTROL_SERVICE_CLIENT_STATE_W4_READ_CHARACTERISTIC_VALUE_RESULT;
 
-            (void) gatt_client_read_value_of_characteristic_using_value_handle(
+            (void) gatt_client_read_value_of_characteristic_using_value_handle_with_context(
                 &vocs_client_handle_gatt_client_event, connection->basic_connection.con_handle,
-                vocs_client_value_handle_for_index(connection));
+                vocs_client_value_handle_for_index(connection), vocs_client.service_id, connection->basic_connection.cid);
             break;
 
         case VOLUME_OFFSET_CONTROL_SERVICE_CLIENT_STATE_W2_WRITE_CHARACTERISTIC_VALUE_WITHOUT_RESPONSE:
@@ -582,10 +582,10 @@ static void vocs_client_run_for_connection(void * context){
             connection->state = VOLUME_OFFSET_CONTROL_SERVICE_CLIENT_STATE_W4_WRITE_CHARACTERISTIC_VALUE_RESULT;
 
             value_length = vocs_client_serialize_characteristic_value_for_write(connection, &value);
-            (void) gatt_client_write_value_of_characteristic(
+            (void) gatt_client_write_value_of_characteristic_with_context(
                     &vocs_client_handle_gatt_client_event, connection->basic_connection.con_handle,
                     vocs_client_value_handle_for_index(connection),
-                    value_length, value);
+                    value_length, value, vocs_client.service_id, connection->basic_connection.cid);
             break;
         default:
             break;
