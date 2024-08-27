8c54c376c28c4fea538d8219a4665d9027724700
Matthias Ringwald <matthias@ringwald.ch>
2024-08-27 12:21:32 +0200

    gatt-service/lls-client: use gatt client with context functions

--- src/ble/gatt-service/link_loss_service_client.c
+++ src/ble/gatt-service/link_loss_service_client.c
@@ -313,19 +313,19 @@ static void lls_client_run_for_connection(void * context){
         case LINK_LOSS_SERVICE_CLIENT_STATE_W2_READ_CHARACTERISTIC_VALUE:
             connection->state = LINK_LOSS_SERVICE_CLIENT_STATE_W4_READ_CHARACTERISTIC_VALUE_RESULT;
 
-            (void) gatt_client_read_value_of_characteristic_using_value_handle(
+            (void) gatt_client_read_value_of_characteristic_using_value_handle_with_context(
                 &lls_client_handle_gatt_client_event, connection->basic_connection.cid,
-                lls_client_value_handle_for_index(connection));
+                lls_client_value_handle_for_index(connection), lls_client.service_id, connection->basic_connection.cid);
             break;
 
         case LINK_LOSS_SERVICE_CLIENT_STATE_W2_WRITE_CHARACTERISTIC_VALUE:
             connection->state = LINK_LOSS_SERVICE_CLIENT_STATE_W4_WRITE_CHARACTERISTIC_VALUE_RESULT;
 
             value_length = lls_client_serialize_characteristic_value_for_write(connection, &value);
-            (void) gatt_client_write_value_of_characteristic(
+            (void) gatt_client_write_value_of_characteristic_with_context(
                     &lls_client_handle_gatt_client_event, connection->basic_connection.cid,
                 lls_client_value_handle_for_index(connection),
-                value_length, value);
+                value_length, value, lls_client.service_id, connection->basic_connection.cid);
             
             break;
 
