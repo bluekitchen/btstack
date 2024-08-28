9110547f2c48dc3345b87712f9f033132ff8bcb4
Matthias Ringwald <matthias@ringwald.ch>
2024-08-28 09:46:12 +0200

    gatt-service/tbs-client: use connection id from notification/indication

--- src/le-audio/gatt-service/telephone_bearer_service_client.c
+++ src/le-audio/gatt-service/telephone_bearer_service_client.c
@@ -460,8 +460,7 @@ static void tbs_client_packet_handler_internal(uint8_t packet_type, uint16_t cha
     if (packet_type != HCI_EVENT_PACKET) return;
     gatt_service_client_connection_t * connection_helper;
     tbs_client_connection_t * connection;
-    hci_con_handle_t con_handle;
-    uint16_t value_handle;
+    uint16_t connection_id;
     uint8_t status;
 
     switch(hci_event_packet_get_type(packet)){
@@ -502,13 +501,12 @@ static void tbs_client_packet_handler_internal(uint8_t packet_type, uint16_t cha
             break;
 
         case GATT_EVENT_NOTIFICATION:
-            con_handle = (hci_con_handle_t)gatt_event_notification_get_handle(packet);
-            value_handle = gatt_event_notification_get_value_handle(packet);
-            connection_helper = gatt_service_client_get_connection_for_con_handle_and_attribute_handle(&tbs_client, con_handle, value_handle);
+            connection_id = gatt_event_notification_get_connection_id(packet);
+            connection_helper = gatt_service_client_get_connection_for_cid(&tbs_client, connection_id);
             btstack_assert(connection_helper != NULL);
 
-            tbs_client_emit_notify_event(connection_helper, value_handle, ATT_ERROR_SUCCESS,
-                    gatt_event_notification_get_value(packet),gatt_event_notification_get_value_length(packet));
+            tbs_client_emit_notify_event(connection_helper, gatt_event_notification_get_value_handle(packet), ATT_ERROR_SUCCESS,
+                                         gatt_event_notification_get_value(packet), gatt_event_notification_get_value_length(packet));
             break;
         default:
             break;
