c26f92b34dd5f5601503f0628c050997affe31fa
Matthias Ringwald <matthias@ringwald.ch>
2024-09-13 19:00:13 +0200

    gatt_client: set connection and service id for gatt_client_listen_for_service_characteristic_value_updates

--- src/ble/gatt_client.c
+++ src/ble/gatt_client.c
@@ -1359,7 +1359,7 @@ void gatt_client_listen_for_service_characteristic_value_updates(gatt_client_ser
     notification->end_group_handle = service->end_group_handle;
     notification->service_id = service_id;
     notification->connection_id = connection_id;
-    btstack_linked_list_add(&gatt_client_value_listeners, (btstack_linked_item_t*) notification);
+    btstack_linked_list_add(&gatt_client_service_value_listeners, (btstack_linked_item_t*) notification);
 }
 
 /**
