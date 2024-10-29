25d5427a345779b159b63c0d8b197d2dee40cf37
Matthias Ringwald <matthias@ringwald.ch>
2024-10-29 10:58:35 +0100

    tool/state_num: remove whitespace

--- tool/state_enums.sh
+++ tool/state_enums.sh
@@ -3,22 +3,22 @@ BTSTACK_ROOT=`dirname $0`'/..'
 
 # SM
 echo "Security Manager states"
-grep SM_ $BTSTACK_ROOT/src/hci.h | nl -v0
+grep SM_ $BTSTACK_ROOT/src/hci.h | sed 's/^[[:space:]]*//'  | nl -v0
 
 echo
 
 # RFCOMM
 echo "RFCOMM Multiplexer states"
-grep RFCOMM_MULTIPLEXER_ $BTSTACK_ROOT/src/classic/rfcomm.h | grep -v RFCOMM_MULTIPLEXER_EVENT | grep -v RFCOMM_MULTIPLEXER_STATE | nl
+grep RFCOMM_MULTIPLEXER_ $BTSTACK_ROOT/src/classic/rfcomm.h | grep -v RFCOMM_MULTIPLEXER_EVENT | grep -v RFCOMM_MULTIPLEXER_STATE | sed 's/^[[:space:]]*//'  | nl
 echo "RFCOMM Channel states"
-grep RFCOMM_CHANNEL_ $BTSTACK_ROOT/src/classic/rfcomm.h | grep -v RFCOMM_CHANNEL_STATE | grep -v "// state variables" | nl
+grep RFCOMM_CHANNEL_ $BTSTACK_ROOT/src/classic/rfcomm.h | grep -v RFCOMM_CHANNEL_STATE | grep -v "// state variables" | sed 's/^[[:space:]]*//'  | nl
 
 echo
 
 # HFP
 echo "HFP Commands/States"
-grep  HFP_CMD_ $BTSTACK_ROOT/src/classic/hfp.h | nl -v 0
+grep  HFP_CMD_ $BTSTACK_ROOT/src/classic/hfp.h | sed 's/^[[:space:]]*//'  | nl -v 0
 echo "HFP Errors"
-grep  HFP_CME_ $BTSTACK_ROOT/src/classic/hfp.h | nl -v 0
+grep  HFP_CME_ $BTSTACK_ROOT/src/classic/hfp.h | sed 's/^[[:space:]]*//'  | nl -v 0
 
 echo
