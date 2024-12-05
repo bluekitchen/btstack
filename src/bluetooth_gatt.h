7f5d8e5f11e4f050a2628944569bacd15f3dbd6a
Matthias Ringwald <matthias@ringwald.ch>
2024-12-05 16:50:12 +0100

    bluetooth_gatt: fix spelling of ORG_BLUETOOTH_CHARACTERISTIC_BATTERY_CRITCAL_STATUS

--- src/bluetooth_gatt.h
+++ src/bluetooth_gatt.h
@@ -451,7 +451,7 @@
 #define ORG_BLUETOOTH_CHARACTERISTIC_HEARING_AID_FEATURES                                                             0x2BDA // Hearing Aid Features
 #define ORG_BLUETOOTH_CHARACTERISTIC_HEARING_AID_PRESET_CONTROL_POINT                                                 0x2BDB // Hearing Aid Preset Control Point
 #define ORG_BLUETOOTH_CHARACTERISTIC_ACTIVE_PRESET_INDEX                                                              0x2BDC // Active Preset Index
-#define ORG_BLUETOOTH_CHARACTERISTIC_BATTERY_CRITCAL_STATUS                                                           0x2BE9 // Battery Critical Status
+#define ORG_BLUETOOTH_CHARACTERISTIC_BATTERY_CRITICAL_STATUS                                                           0x2BE9 // Battery Critical Status
 #define ORG_BLUETOOTH_CHARACTERISTIC_BATTERY_HEALTH_STATUS                                                            0x2BEA // Battery Health Status
 #define ORG_BLUETOOTH_CHARACTERISTIC_BATTERY_HEALTH_INFORMATION                                                       0x2BEB // Battery Health Information
 #define ORG_BLUETOOTH_CHARACTERISTIC_BATTERY_INFORMATION                                                              0x2BEC // Battery Health Information
