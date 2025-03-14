69fd0c4125fc6cbbeb7d4ec03c8d497ba886621d
Matthias Ringwald <matthias@ringwald.ch>
2025-03-14 16:38:25 +0100

    hfp: reset context extended_audio_gateway_error

--- src/classic/hfp.c
+++ src/classic/hfp.c
@@ -607,7 +607,6 @@ void hfp_reset_context_flags(hfp_connection_t * hfp_connection){
     hfp_connection->change_status_update_for_individual_ag_indicators = 0;
 
     hfp_connection->enable_extended_audio_gateway_error_report = 0;
-    hfp_connection->extended_audio_gateway_error = 0;
 
     // establish codecs hfp_connection
     hfp_connection->suggested_codec = 0;
@@ -643,11 +642,11 @@ static hfp_connection_t * create_hfp_connection_context(void){
     hfp_connection->hf_call_status      = HFP_CALL_STATUS_NO_HELD_OR_ACTIVE_CALLS;
     hfp_connection->hf_callsetup_status = HFP_CALLSETUP_STATUS_NO_CALL_SETUP_IN_PROGRESS;
     hfp_connection->hf_callheld_status  = HFP_CALLHELD_STATUS_NO_CALLS_HELD;
+    hfp_connection->operator_name_changed = 0;
 
     // AG only
     hfp_connection->send_error = 0;
-
-    hfp_connection->operator_name_changed = 0;
+    hfp_connection->extended_audio_gateway_error = 0;
 
     hfp_reset_context_flags(hfp_connection);
 
