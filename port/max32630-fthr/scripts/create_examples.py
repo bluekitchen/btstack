8638fb5ee3699b050e16356002e488ca331fc34d
Tuomas Lahtinen <tuomas123lahtinen@gmail.com>
2025-04-06 14:33:49 +0300

    Remove the cc256x init script fetching from create_examples.py
    It is not going to work gracefully in the separate builds.

--- port/max32630-fthr/scripts/create_examples.py
+++ port/max32630-fthr/scripts/create_examples.py
@@ -32,19 +32,6 @@ script_path = os.path.abspath(os.path.dirname(sys.argv[0])) + '/../'
 # get btstack root
 btstack_root = script_path + '../../'
 
-## pick correct init script based on your hardware
-# - init script for CC2564B
-cc256x_init_script = 'bluetooth_init_cc2564B_1.8_BT_Spec_4.1.c'
-
-subprocess.call("make -f ../Makefile -C src " + cc256x_init_script, shell=True)
-
-# fetch init script
-# print("Creating init script %s" % cc256x_init_script)
-# make_template = 'make -f {BTSTACK_ROOT}chipset/cc256x/Makefile.inc -C {SCRIPT_PATH}src/ {INIT_SCRIPT} BTSTACK_ROOT={BTSTACK_ROOT}'
-# make_command = make_template.format(BTSTACK_ROOT=btstack_root, SCRIPT_PATH=script_path, INIT_SCRIPT=cc256x_init_script)
-# print(make_command)
-# subprocess.call(make_command)
-
 # path to examples
 examples_embedded = btstack_root + 'example/'
 
