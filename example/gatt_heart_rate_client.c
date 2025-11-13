cf425728ffdf1b471add12ad7a9bdf08476a5ab5
Matthias Ringwald <matthias@ringwald.ch>
2025-11-13 11:41:38 +0100

    example/gatt_heart_rate_client: rename application states

--- example/gatt_heart_rate_client.c
+++ example/gatt_heart_rate_client.c
@@ -57,22 +57,21 @@ static void handle_gatt_client_event(uint8_t packet_type, uint16_t channel, uint
 static void gatt_heart_rate_client_request_to_send(void);
 
 typedef enum {
-    TC_OFF,
-    TC_IDLE,
-    TC_W4_SCAN_RESULT,
-    TC_W4_CONNECT,
-    TC_W2_QUERY_SERVICE,
-    TC_W4_SERVICE_RESULT,
-    TC_W2_QUERY_HEART_RATE_MEASUREMENT_CHARACTERISTIC,
-    TC_W4_HEART_RATE_MEASUREMENT_CHARACTERISTIC,
-    TC_W2_ENABLE_NOTIFICATIONS,
-    TC_W4_ENABLE_NOTIFICATIONS_COMPLETE,
-    TC_W2_QUERY_SENSOR_LOCATION_CHARACTERISTIC,
-    TC_W4_SENSOR_LOCATION_CHARACTERISTIC,
-    TC_W2_QUERY_SENSOR_LOCATION,
-    TC_W4_SENSOR_LOCATION,
-    TC_CONNECTED
-} gc_state_t;
+    APP_STATE_IDLE,
+    APP_STATE_W4_SCAN_RESULT,
+    APP_STATE_W4_CONNECT,
+    APP_STATE_W2_QUERY_SERVICE,
+    APP_STATE_W4_SERVICE_RESULT,
+    APP_STATE_W2_QUERY_HEART_RATE_MEASUREMENT_CHARACTERISTIC,
+    APP_STATE_W4_HEART_RATE_MEASUREMENT_CHARACTERISTIC,
+    APP_STATE_W2_ENABLE_NOTIFICATIONS,
+    APP_STATE_W4_ENABLE_NOTIFICATIONS_COMPLETE,
+    APP_STATE_W2_QUERY_SENSOR_LOCATION_CHARACTERISTIC,
+    APP_STATE_W4_SENSOR_LOCATION_CHARACTERISTIC,
+    APP_STATE_W2_QUERY_SENSOR_LOCATION,
+    APP_STATE_W4_SENSOR_LOCATION,
+    APP_STATE_CONNECTED
+} app_state_t;
 
 static const char * sensor_contact_string[] = {
     "not supported",
@@ -89,7 +88,7 @@ static bd_addr_t      le_streamer_addr;
 static bd_addr_type_t le_streamer_addr_type;
 
 static hci_con_handle_t connection_handle;
-static btstack_context_callback_registration_t heart_rate_service_write_request;
+static btstack_context_callback_registration_t heart_rate_service_gatt_query_request;
 
 static gatt_client_service_t        heart_rate_service;
 static gatt_client_characteristic_t body_sensor_location_characteristic;
@@ -100,7 +99,7 @@ static uint8_t body_sensor_location;
 static gatt_client_notification_t notification_listener;
 static int listener_registered;
 
-static gc_state_t state = TC_OFF;
+static app_state_t state = APP_STATE_IDLE;
 static btstack_packet_callback_registration_t hci_event_callback_registration;
 
 
@@ -146,7 +145,7 @@ static void handle_gatt_client_event(uint8_t packet_type, uint16_t channel, uint
     uint8_t  att_status;
 
     switch(state){
-        case TC_W4_SERVICE_RESULT:
+        case APP_STATE_W4_SERVICE_RESULT:
             switch(hci_event_packet_get_type(packet)){
                 case GATT_EVENT_SERVICE_QUERY_RESULT:
                     // store service (we expect only one)
@@ -159,7 +158,7 @@ static void handle_gatt_client_event(uint8_t packet_type, uint16_t channel, uint
                         gap_disconnect(connection_handle);
                         break;  
                     } 
-                    state = TC_W2_QUERY_HEART_RATE_MEASUREMENT_CHARACTERISTIC;
+                    state = APP_STATE_W2_QUERY_HEART_RATE_MEASUREMENT_CHARACTERISTIC;
                     gatt_heart_rate_client_request_to_send();
                     break;
                 default:
@@ -167,7 +166,7 @@ static void handle_gatt_client_event(uint8_t packet_type, uint16_t channel, uint
             }
             break;
 
-        case TC_W4_HEART_RATE_MEASUREMENT_CHARACTERISTIC:
+        case APP_STATE_W4_HEART_RATE_MEASUREMENT_CHARACTERISTIC:
             switch(hci_event_packet_get_type(packet)){
                 case GATT_EVENT_CHARACTERISTIC_QUERY_RESULT:
                     gatt_event_characteristic_query_result_get_characteristic(packet, &heart_rate_measurement_characteristic);
@@ -184,7 +183,7 @@ static void handle_gatt_client_event(uint8_t packet_type, uint16_t channel, uint
                     gatt_client_listen_for_characteristic_value_updates(&notification_listener, handle_gatt_client_event, connection_handle, &heart_rate_measurement_characteristic);
                     // enable notifications
                     printf("Enable Notify on Heart Rate Measurements characteristic.\n");
-                    state = TC_W2_ENABLE_NOTIFICATIONS;
+                    state = APP_STATE_W2_ENABLE_NOTIFICATIONS;
                     gatt_heart_rate_client_request_to_send();
                     break;
                 default:
@@ -192,11 +191,11 @@ static void handle_gatt_client_event(uint8_t packet_type, uint16_t channel, uint
             }
             break;
 
-        case TC_W4_ENABLE_NOTIFICATIONS_COMPLETE:
+        case APP_STATE_W4_ENABLE_NOTIFICATIONS_COMPLETE:
             switch(hci_event_packet_get_type(packet)){
                 case GATT_EVENT_QUERY_COMPLETE:
                     printf("Notifications enabled, ATT status 0x%02x\n", gatt_event_query_complete_get_att_status(packet));
-                    state = TC_W2_QUERY_SENSOR_LOCATION_CHARACTERISTIC;
+                    state = APP_STATE_W2_QUERY_SENSOR_LOCATION_CHARACTERISTIC;
                     gatt_heart_rate_client_request_to_send();
                     break;
                 default:
@@ -205,7 +204,7 @@ static void handle_gatt_client_event(uint8_t packet_type, uint16_t channel, uint
             break;
 
 
-        case TC_W4_SENSOR_LOCATION_CHARACTERISTIC:
+        case APP_STATE_W4_SENSOR_LOCATION_CHARACTERISTIC:
             switch(hci_event_packet_get_type(packet)){
                 case GATT_EVENT_CHARACTERISTIC_QUERY_RESULT:
                     gatt_event_characteristic_query_result_get_characteristic(packet, &body_sensor_location_characteristic);
@@ -213,15 +212,15 @@ static void handle_gatt_client_event(uint8_t packet_type, uint16_t channel, uint
                 case GATT_EVENT_QUERY_COMPLETE:
                     if (gatt_event_query_complete_get_att_status(packet) != ATT_ERROR_SUCCESS){
                         printf("CHARACTERISTIC_QUERY_RESULT - ATT Error 0x%02x.\n", packet[4]);
-                        state = TC_CONNECTED;
+                        state = APP_STATE_CONNECTED;
                         break;  
                     } 
                     if (body_sensor_location_characteristic.value_handle == 0){
                         printf("Sensor Location characteristic not available.\n");
-                        state = TC_CONNECTED;
+                        state = APP_STATE_CONNECTED;
                         break;
                     }
-                    state = TC_W2_QUERY_SENSOR_LOCATION;
+                    state = APP_STATE_W2_QUERY_SENSOR_LOCATION;
                     gatt_heart_rate_client_request_to_send();
                     break;
                 default:
@@ -230,21 +229,21 @@ static void handle_gatt_client_event(uint8_t packet_type, uint16_t channel, uint
             break;
 
 
-        case TC_W4_SENSOR_LOCATION:
+        case APP_STATE_W4_SENSOR_LOCATION:
             switch(hci_event_packet_get_type(packet)){
                 case GATT_EVENT_CHARACTERISTIC_VALUE_QUERY_RESULT:
                     body_sensor_location = gatt_event_characteristic_value_query_result_get_value(packet)[0];
                     printf("Sensor Location: %u\n", body_sensor_location);
                     break;
                 case GATT_EVENT_QUERY_COMPLETE:
-                    state = TC_CONNECTED;
+                    state = APP_STATE_CONNECTED;
                     break;
                 default:
                     break;
             }
             break;
 
-        case TC_CONNECTED:
+        case APP_STATE_CONNECTED:
             switch(hci_event_packet_get_type(packet)){
                 case GATT_EVENT_NOTIFICATION:
                     if (gatt_event_notification_get_value(packet)[0] & 1){
@@ -273,11 +272,11 @@ static void handle_gatt_client_event(uint8_t packet_type, uint16_t channel, uint
 static void gatt_heart_rate_client_start(void){
     if (cmdline_addr_found){
         printf("Connect to %s\n", bd_addr_to_str(cmdline_addr));
-        state = TC_W4_CONNECT;
+        state = APP_STATE_W4_CONNECT;
         gap_connect(cmdline_addr, 0);
     } else {
         printf("Start scanning!\n");
-        state = TC_W4_SCAN_RESULT;
+        state = APP_STATE_W4_SCAN_RESULT;
         gap_set_scan_parameters(0,0x0030, 0x0030);
         gap_start_scan();
     }
@@ -287,30 +286,30 @@ static void gatt_heart_rate_client_send_next_query(void * context){
     UNUSED(context);
 
     switch (state){
-        case TC_W2_QUERY_SERVICE:
-            state = TC_W4_SERVICE_RESULT;
+        case APP_STATE_W2_QUERY_SERVICE:
+            state = APP_STATE_W4_SERVICE_RESULT;
             printf("Search for Heart Rate MService.\n");
             gatt_client_discover_primary_services_by_uuid16(handle_gatt_client_event, connection_handle, ORG_BLUETOOTH_SERVICE_HEART_RATE);
             break;
-        case TC_W2_QUERY_HEART_RATE_MEASUREMENT_CHARACTERISTIC:
-            state = TC_W4_HEART_RATE_MEASUREMENT_CHARACTERISTIC;
+        case APP_STATE_W2_QUERY_HEART_RATE_MEASUREMENT_CHARACTERISTIC:
+            state = APP_STATE_W4_HEART_RATE_MEASUREMENT_CHARACTERISTIC;
             printf("Search for Heart Rate Measurement characteristic.\n");
             gatt_client_discover_characteristics_for_service_by_uuid16(handle_gatt_client_event, connection_handle, &heart_rate_service, ORG_BLUETOOTH_CHARACTERISTIC_HEART_RATE_MEASUREMENT);
             break;
-        case TC_W2_ENABLE_NOTIFICATIONS:
+        case APP_STATE_W2_ENABLE_NOTIFICATIONS:
             printf("Enable Notify on Heart Rate Measurements characteristic.\n");
-            state = TC_W4_ENABLE_NOTIFICATIONS_COMPLETE;
+            state = APP_STATE_W4_ENABLE_NOTIFICATIONS_COMPLETE;
             gatt_client_write_client_characteristic_configuration(handle_gatt_client_event, connection_handle,
                                                                   &heart_rate_measurement_characteristic, GATT_CLIENT_CHARACTERISTICS_CONFIGURATION_NOTIFICATION);
             break;
-        case TC_W2_QUERY_SENSOR_LOCATION_CHARACTERISTIC:
-            state = TC_W4_SENSOR_LOCATION_CHARACTERISTIC;
+        case APP_STATE_W2_QUERY_SENSOR_LOCATION_CHARACTERISTIC:
+            state = APP_STATE_W4_SENSOR_LOCATION_CHARACTERISTIC;
             printf("Search for Sensor Location characteristic.\n");
             gatt_client_discover_characteristics_for_service_by_uuid16(handle_gatt_client_event, connection_handle, &heart_rate_service, ORG_BLUETOOTH_CHARACTERISTIC_BODY_SENSOR_LOCATION);
             break;
-        case TC_W2_QUERY_SENSOR_LOCATION:
+        case APP_STATE_W2_QUERY_SENSOR_LOCATION:
             printf("Read Body Sensor Location.\n");
-            state = TC_W4_SENSOR_LOCATION;
+            state = APP_STATE_W4_SENSOR_LOCATION;
             gatt_client_read_value_of_characteristic(handle_gatt_client_event, connection_handle, &body_sensor_location_characteristic);
             break;
         default:
@@ -320,8 +319,8 @@ static void gatt_heart_rate_client_send_next_query(void * context){
 }
 
 static void gatt_heart_rate_client_request_to_send(void){
-    heart_rate_service_write_request.callback = &gatt_heart_rate_client_send_next_query;
-    gatt_client_request_to_send_gatt_query(&heart_rate_service_write_request, connection_handle);
+    heart_rate_service_gatt_query_request.callback = &gatt_heart_rate_client_send_next_query;
+    gatt_client_request_to_send_gatt_query(&heart_rate_service_gatt_query_request, connection_handle);
 }
 
 static void hci_event_handler(uint8_t packet_type, uint16_t channel, uint8_t *packet, uint16_t size){
@@ -337,19 +336,17 @@ static void hci_event_handler(uint8_t packet_type, uint16_t channel, uint8_t *pa
             // BTstack activated, get started
             if (btstack_event_state_get_state(packet) == HCI_STATE_WORKING) {
                 gatt_heart_rate_client_start();
-            } else {
-                state = TC_OFF;
             }
             break;
         case GAP_EVENT_ADVERTISING_REPORT:
-            if (state != TC_W4_SCAN_RESULT) return;
+            if (state != APP_STATE_W4_SCAN_RESULT) return;
             // check name in advertisement
             if (!advertisement_report_contains_uuid16(ORG_BLUETOOTH_SERVICE_HEART_RATE, packet)) return;
             // store address and type
             gap_event_advertising_report_get_address(packet, le_streamer_addr);
             le_streamer_addr_type = gap_event_advertising_report_get_address_type(packet);
             // stop scanning, and connect to the device
-            state = TC_W4_CONNECT;
+            state = APP_STATE_W4_CONNECT;
             gap_stop_scan();
             printf("Stop scan. Connect to device with addr %s.\n", bd_addr_to_str(le_streamer_addr));
             gap_connect(le_streamer_addr,le_streamer_addr_type);
@@ -357,7 +354,7 @@ static void hci_event_handler(uint8_t packet_type, uint16_t channel, uint8_t *pa
         case HCI_EVENT_META_GAP:
             // wait for connection complete
             if (hci_event_gap_meta_get_subevent_code(packet) !=  GAP_SUBEVENT_LE_CONNECTION_COMPLETE) break;
-            if (state != TC_W4_CONNECT) return;
+            if (state != APP_STATE_W4_CONNECT) return;
             connection_handle = gap_subevent_le_connection_complete_get_connection_handle(packet);
             // print connection parameters (without using float operations)
             conn_interval = gap_subevent_le_connection_complete_get_conn_interval(packet);
@@ -366,7 +363,7 @@ static void hci_event_handler(uint8_t packet_type, uint16_t channel, uint8_t *pa
             // initialize gatt client context with handle, and add it to the list of active clients
             // query primary services
             printf("Search for Heart Rate service.\n");
-            state = TC_W2_QUERY_SERVICE;
+            state = APP_STATE_W2_QUERY_SERVICE;
             gatt_heart_rate_client_request_to_send();
             break;
         case HCI_EVENT_DISCONNECTION_COMPLETE:
@@ -381,7 +378,6 @@ static void hci_event_handler(uint8_t packet_type, uint16_t channel, uint8_t *pa
                 return;
             }
             printf("Disconnected %s\n", bd_addr_to_str(le_streamer_addr));
-            if (state == TC_OFF) break;
             gatt_heart_rate_client_start();
             break;
         default:
