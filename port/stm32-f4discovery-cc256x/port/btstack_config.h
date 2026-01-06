05fcf6221dbaf42a7de9c83665c39acd0a391efe
Matthias Ringwald <matthias@ringwald.ch>
2026-01-06 18:37:16 +0100

    Revert "stm32-f4discovery-cc256x: add HAVE_BTSTACK_AUDIO_EFFECTIVE_SAMPLERATE"
    This reverts commit fc27c41c6707d8dd61e80029518b524225a85f7a.

--- port/stm32-f4discovery-cc256x/port/btstack_config.h
+++ port/stm32-f4discovery-cc256x/port/btstack_config.h
@@ -14,7 +14,6 @@
 #define HAVE_HAL_AUDIO
 #define HAVE_HAL_AUDIO_SINK_STEREO_ONLY
 #define HAVE_HAL_AUDIO_SINK_VOLUME_CONTROL
-#define HAVE_BTSTACK_AUDIO_EFFECTIVE_SAMPLERATE
 
 // BTstack features that can be enabled
 #define ENABLE_BLE
