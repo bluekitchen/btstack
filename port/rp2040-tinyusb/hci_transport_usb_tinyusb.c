b1d79d9af3e8aa2920910c7f8ec1b9fae0031b70
Matthias Ringwald <matthias@ringwald.ch>
2026-09-03 14:52:57 +0200

    rp2040-tinyusb: detect usb endpoints

--- port/rp2040-tinyusb/hci_transport_usb_tinyusb.c
+++ port/rp2040-tinyusb/hci_transport_usb_tinyusb.c
@@ -2,6 +2,8 @@
 
 #include "hci_transport_usb_tinyusb.h"
 
+#include <string.h>
+
 #include "btstack_debug.h"
 #include "btstack_run_loop.h"
 #include "tusb.h"
@@ -13,10 +15,21 @@
 #define HCI_USB_SUBCLASS 0x01
 #define HCI_USB_PROTOCOL 0x01
 
+typedef struct {
+    uint8_t event_in_addr;
+    uint8_t acl_in_addr;
+    uint8_t acl_out_addr;
+    uint8_t hci_interface_number;
+    tusb_desc_endpoint_t event_in_desc;
+    tusb_desc_endpoint_t acl_in_desc;
+    tusb_desc_endpoint_t acl_out_desc;
+} tinyusb_hci_controller_t;
+
 static btstack_timer_source_t tinyusb_timer;
 static void (*packet_handler)(uint8_t packet_type, uint8_t *packet, uint16_t size);
 static tusb_desc_device_t tinyusb_device_descriptor;
 CFG_TUH_MEM_SECTION CFG_TUH_MEM_ALIGN static uint8_t tinyusb_configuration_descriptor[CFG_TUH_ENUMERATION_BUFSIZE];
+static tinyusb_hci_controller_t tinyusb_hci_controller;
 static uint8_t tinyusb_candidate_daddr = HCI_USB_INVALID_ADDRESS;
 static uint8_t tinyusb_hci_daddr = HCI_USB_INVALID_ADDRESS;
 static bool tinyusb_initialized;
@@ -38,25 +51,76 @@ static void hci_transport_usb_tinyusb_init(const void *transport_config) {
     tinyusb_transport_open = false;
 }
 
-static bool tinyusb_configuration_has_hci_interface(const tusb_desc_configuration_t *configuration) {
+static void tinyusb_dump_hci_controller_configuration(void) {
+    log_info("HCI USB configuration:");
+    log_info("  Command: 0x00 (interface %u)", tinyusb_hci_controller.hci_interface_number);
+    log_info("  Event:   0x%02x (transfer size %u)", tinyusb_hci_controller.event_in_addr,
+             tu_edpt_packet_size(&tinyusb_hci_controller.event_in_desc));
+    log_info("  ACL Out: 0x%02x (transfer size %u)", tinyusb_hci_controller.acl_out_addr,
+             tu_edpt_packet_size(&tinyusb_hci_controller.acl_out_desc));
+    log_info("  ACL  In: 0x%02x (transfer size %u)", tinyusb_hci_controller.acl_in_addr,
+             tu_edpt_packet_size(&tinyusb_hci_controller.acl_in_desc));
+}
+
+static void tinyusb_find_hci_endpoints(const tusb_desc_configuration_t *configuration) {
     const uint8_t *descriptor_end = (const uint8_t *) configuration + tu_le16toh(configuration->wTotalLength);
     const uint8_t *descriptor = tu_desc_next(configuration);
+    uint8_t interface_number = HCI_USB_INVALID_ADDRESS;
+    bool hci_interface = false;
 
+    memset(&tinyusb_hci_controller, 0, sizeof(tinyusb_hci_controller));
+    tinyusb_hci_controller.hci_interface_number = HCI_USB_INVALID_ADDRESS;
     while (descriptor < descriptor_end) {
         if (tu_desc_len(descriptor) == 0) {
             break;
         }
-        if (tu_desc_type(descriptor) == TUSB_DESC_INTERFACE) {
-            const tusb_desc_interface_t *interface = (const tusb_desc_interface_t *) descriptor;
-            if (interface->bAlternateSetting == 0 && interface->bInterfaceClass == HCI_USB_CLASS &&
-                interface->bInterfaceSubClass == HCI_USB_SUBCLASS &&
-                interface->bInterfaceProtocol == HCI_USB_PROTOCOL) {
-                return true;
+
+        switch (tu_desc_type(descriptor)) {
+            case TUSB_DESC_INTERFACE: {
+                const tusb_desc_interface_t *interface = (const tusb_desc_interface_t *) descriptor;
+                interface_number = interface->bInterfaceNumber;
+                hci_interface = interface->bAlternateSetting == 0 && interface->bInterfaceClass == HCI_USB_CLASS &&
+                                interface->bInterfaceSubClass == HCI_USB_SUBCLASS &&
+                                interface->bInterfaceProtocol == HCI_USB_PROTOCOL;
+                if (hci_interface) {
+                    tinyusb_hci_controller.hci_interface_number = interface_number;
+                }
+                break;
             }
+
+            case TUSB_DESC_ENDPOINT: {
+                const tusb_desc_endpoint_t *endpoint = (const tusb_desc_endpoint_t *) descriptor;
+                if (!hci_interface || interface_number != tinyusb_hci_controller.hci_interface_number) {
+                    break;
+                }
+
+                if (tu_edpt_dir(endpoint->bEndpointAddress) == TUSB_DIR_IN &&
+                    endpoint->bmAttributes.xfer == TUSB_XFER_INTERRUPT) {
+                    tinyusb_hci_controller.event_in_addr = endpoint->bEndpointAddress;
+                    tinyusb_hci_controller.event_in_desc = *endpoint;
+                } else if (tu_edpt_dir(endpoint->bEndpointAddress) == TUSB_DIR_IN &&
+                           endpoint->bmAttributes.xfer == TUSB_XFER_BULK) {
+                    tinyusb_hci_controller.acl_in_addr = endpoint->bEndpointAddress;
+                    tinyusb_hci_controller.acl_in_desc = *endpoint;
+                } else if (tu_edpt_dir(endpoint->bEndpointAddress) == TUSB_DIR_OUT &&
+                           endpoint->bmAttributes.xfer == TUSB_XFER_BULK) {
+                    tinyusb_hci_controller.acl_out_addr = endpoint->bEndpointAddress;
+                    tinyusb_hci_controller.acl_out_desc = *endpoint;
+                }
+                break;
+            }
+
+            default:
+                break;
         }
         descriptor = tu_desc_next(descriptor);
     }
-    return false;
+}
+
+static bool tinyusb_hci_controller_usable(void) {
+    return tinyusb_hci_controller.hci_interface_number != HCI_USB_INVALID_ADDRESS &&
+           tinyusb_hci_controller.event_in_addr != 0 && tinyusb_hci_controller.acl_in_addr != 0 &&
+           tinyusb_hci_controller.acl_out_addr != 0;
 }
 
 static void tinyusb_handle_device_descriptor(tuh_xfer_t *xfer) {
@@ -83,10 +147,12 @@ static void tinyusb_handle_device_descriptor(tuh_xfer_t *xfer) {
         return;
     }
 
-    if (tinyusb_configuration_has_hci_interface((const tusb_desc_configuration_t *) tinyusb_configuration_descriptor)) {
+    tinyusb_find_hci_endpoints((const tusb_desc_configuration_t *) tinyusb_configuration_descriptor);
+    if (tinyusb_hci_controller_usable()) {
         tinyusb_hci_daddr = xfer->daddr;
         log_info("Bluetooth HCI controller detected at device %u (%04x:%04x)", xfer->daddr,
                  tu_le16toh(tinyusb_device_descriptor.idVendor), tu_le16toh(tinyusb_device_descriptor.idProduct));
+        tinyusb_dump_hci_controller_configuration();
     } else {
         log_info("Device %u (%04x:%04x) is not a Bluetooth HCI controller", xfer->daddr,
                  tu_le16toh(tinyusb_device_descriptor.idVendor), tu_le16toh(tinyusb_device_descriptor.idProduct));
